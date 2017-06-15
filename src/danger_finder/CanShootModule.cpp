9f22d837bcf28b9c63db3a231604f9104a12d014
DennisdeW <dennisdw@outlook.com>
2017-06-15 11:10:45 +0200

    Fixes for OrientationModule and CanShootModule

--- src/danger_finder/CanShootModule.cpp
+++ src/danger_finder/CanShootModule.cpp
@@ -1,7 +1,11 @@
 #include "roboteam_world/danger_finder/CanShootModule.h"
 #include "roboteam_utils/world_analysis.h"
 #include "roboteam_utils/Vector2.h"
+#include "roboteam_utils/Position.h"
+#include "roboteam_utils/Section.h"
 #include "roboteam_utils/LastWorld.h"
+#include "roboteam_utils/Math.h"
+#include "roboteam_msgs/GeometryFieldSize.h"
 
 namespace rtt {
 namespace df {
@@ -10,13 +14,29 @@ REGISTER_MODULE("CanShoot", CanShootModule)
 
 CanShootModule::CanShootModule() : DangerModule("CanShoot") {}
 
+bool facingGoal(Position pos) {
+	auto geom = LastWorld::get_field();
+    Section goalSection {
+    	-geom.field_length / 2,  geom.goal_width / 2,
+		-geom.field_length / 2, -geom.goal_width / 2
+    };
+    Vector2 longVec { 100, 0 };
+    longVec = longVec.rotate(pos.rot);
+    longVec = longVec + pos.location();
+    Vector2 isect = goalSection.intersection({pos.x, pos.y, longVec.x, longVec.y});
+    ROS_INFO("goal: (%f, %f) -> (%f, %f), pos: (%f, %f), long: (%f, %f) isect: (%f, %f)",
+    		goalSection.a.x, goalSection.a.y, goalSection.b.x, goalSection.b.y,
+			pos.x, pos.y, longVec.x, longVec.y, isect.x, isect.y);
+    return goalSection.pointOnLine(isect);
+}
+
 PartialResult CanShootModule::calculate(const roboteam_msgs::WorldRobot& bot, const roboteam_msgs::World& world) {
 	Vector2 botPos(bot.pos);
 	Vector2 goalPos = LastWorld::get_our_goal_center();
 
 	auto obstacles = getObstaclesBetweenPoints(botPos, goalPos);
-	if (obstacles.size() > 0 && bot_has_ball(bot, world.ball)) {
-		return { myConfig().doubles["canShootDanger"], DANGER_CAN_SHOOT };
+	if (obstacles.size() == 0 && bot_has_ball(bot, world.ball) && facingGoal({bot.pos.x, bot.pos.y, bot.angle})) {
+		return { myConfig().ints["canShootDanger"], DANGER_CAN_SHOOT };
 	}
 	return PartialResult();
 }
