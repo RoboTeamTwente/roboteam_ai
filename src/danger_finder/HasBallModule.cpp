dbe3bdf0c8d976f2c1f818b1487710fd5c6d875e
jim3101 <jimhoekstra2@gmail.com>
2017-06-21 11:55:29 +0200

    small changes

--- src/danger_finder/HasBallModule.cpp
+++ src/danger_finder/HasBallModule.cpp
@@ -9,7 +9,12 @@ REGISTER_MODULE("HasBall", HasBallModule);
 HasBallModule::HasBallModule() : DangerModule("HasBall") {}
 
 PartialResult HasBallModule::calculate(const roboteam_msgs::WorldRobot& bot, const roboteam_msgs::World& world) {
+	// std::cout << "calling bot_has_ball function...";
 	bool hasBall = bot_has_ball(bot, world.ball);
+	
+	// if (hasBall) {
+	// 	ROS_INFO_STREAM("robot " << bot.id << " has the ball");
+	// }
 	return { hasBall ? myConfig().doubles["hasBallDanger"] : 0, hasBall ? DANGER_HAS_BALL : 0 };
 }
 
