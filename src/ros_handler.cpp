b640dc47f37ac630dc59bf5f34b212d0f5bcadbe
baris <b.imre@student.utwente.nl>
2018-09-27 11:13:15 +0200

    Remove tracker

--- src/ros_handler.cpp
+++ src/ros_handler.cpp
@@ -16,11 +16,7 @@ namespace rtt {
 
         // Advertise the reset service.
         reset_srv = nh.advertiseService(SERVICE_WORLD_RESET, &RosHandler::reset_callback, this);
-        
-        // Advertise the tracker service.
-        tracker_srv = nh.advertiseService(SERVICE_OPPONENT_TRACKER, &RosHandler::tracker_callback, this);
-        tracker.add_module(new SpeedTracker());
-        tracker.add_module(new AccelerationTracker());
+
     }
 
     /// Callback function for /vision_detection in ros_handler
@@ -31,7 +27,6 @@ namespace rtt {
             // Filtered world! Special case that shit
             if (auto worldOpt = fworld->consumeMsg()) {
                 world_pub.publish(*worldOpt);
-                tracker.update(*worldOpt);
             }
         } else {
             // Generic world approach
@@ -39,7 +34,6 @@ namespace rtt {
             roboteam_msgs::World world_msg = world->as_message();
 
             world_pub.publish(world_msg);
-            tracker.update(world_msg);
         }
     }
 
@@ -51,36 +45,6 @@ namespace rtt {
 
         return true;
     }
-    
-    bool RosHandler::tracker_callback(roboteam_msgs::Tracker::Request& req, roboteam_msgs::Tracker::Response& res) {
-        
-        const TrackerModule* module = tracker.get_module(req.type);
-        if (module == nullptr) {
-            res.success = false;
-            return false;
-        }
-        
-        TrackerResult track_result = module->calculate_for({req.id, req.our_team});
-        res.success = track_result.success;
-        switch (track_result.type) {
-            case TrackedValueType::LONG: res.long_val = boost::get<long>(track_result.value); break;
-            case TrackedValueType::DOUBLE: res.double_val = boost::get<double>(track_result.value); break;
-            case TrackedValueType::BOOL: res.bool_val = boost::get<bool>(track_result.value); break;
-            case TrackedValueType::STRING: res.string_val = boost::get<std::string>(track_result.value); break;
-            case TrackedValueType::VEC3:
-                Position pos = boost::get<Position>(track_result.value);
-                res.vector_val.x = pos.x;
-                res.vector_val.y = pos.y;
-                res.vector_val.z = pos.rot; 
-                break;
-        }
-        
-        return true;
-    }
 
-    TrackerResult* RosHandler::track(const std::string& type, const TeamRobot& bot) const {
-        const TrackerModule* module = tracker.get_module(type);
-        return module == nullptr ? nullptr : new TrackerResult(module->calculate_for(bot));
-    }
 
 }
