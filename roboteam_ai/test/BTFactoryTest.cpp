f0be8230a9a88a298bd0771f0fca98160c757773
baris <b.imre@student.utwente.nl>
2018-10-08 16:42:13 +0200

    Add test for relative path will finish tomorrow

--- roboteam_ai/test/BTFactoryTest.cpp
+++ roboteam_ai/test/BTFactoryTest.cpp
@@ -9,6 +9,17 @@
 #include "../src/bt/Leaf.hpp"
 
 
+
+#include <stdio.h>  /* defines FILENAME_MAX */
+#ifdef WINDOWS
+#include <direct.h>
+    #define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+
 TEST(BT, BTTest) {
 
     // ===Let's build a BT manually!===
@@ -17,6 +28,17 @@ TEST(BT, BTTest) {
     bt::BehaviorTree manualTree;
 
     bt::Sequence rootSeq;
+
+    char cCurrentPath[FILENAME_MAX];
+
+    if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+    {
+        std::cerr << "gh" << std::endl;
+    }
+
+    cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
+
+    printf ("The current working directory is %s", cCurrentPath);
 
     // Add root to the tree
 
