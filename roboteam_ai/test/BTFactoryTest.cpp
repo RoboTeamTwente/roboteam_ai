d4828912909e913e5ae0d8e31cbd6c656b40fc76
baris <b.imre@student.utwente.nl>
2018-11-05 15:02:04 +0100

    Fix and limit BTFactoryTest

--- roboteam_ai/test/BTFactoryTest.cpp
+++ roboteam_ai/test/BTFactoryTest.cpp
@@ -41,10 +41,10 @@ TEST(BT, BasicFactoryTest) {
     BTFactory dummyFactory = BTFactory::getFactory();
 
     //Define file to test with and update the project to match it.
-    std::string testProjectA = "sampleTest";
+    std::string testProjectA = "bigjson";
     std::string testTreeA = "2d276c37-4ffc-4209-bc32-9fd2405a2ad6";
     std::string testTreeB = "77bc7cab-fe50-4754-9cd4-b80ef75cecf1";
-    int testProjectSize = 26; // The amount of trees in the project
+    int testProjectSize = 5; // The amount of trees in the project
     //try updating a single Tree
     dummyFactory.updateTree(testProjectA, testTreeA);
 
@@ -73,68 +73,7 @@ TEST(BT, BasicFactoryTest) {
     ASSERT_EQ(treeMapA.find(testTreeA)->first, testTreeA);
     ASSERT_EQ(treeMapA.find(testTreeB)->first, testTreeB);
 
-
-
-    //initialize the BTFactory. This updates all the projects in it
-    //dummyFactory.init();
 }
 
-//TEST(BT, UpdateDuringRuntime) {
-//    BTFactory dummyFactory = BTFactory::getFactory();
-//    std::string testProjectA = "sampleTest";
-//    std::string testTreeA = "2d276c37-4ffc-4209-bc32-9fd2405a2ad6";
-//    std::string testTreeB = "d16f9751-a781-4fee-8570-7d88a207aef0";
-//    //Initial update of the project
-//    dummyFactory.updateProject(testProjectA);
-//
-//
-//    // Now we will test if we edit a tree during runtime, and then update it, that it is actually updated.
-//    JsonReader pathReader;
-//    // Fields we are editing and their original values
-//    std::string fieldToEdit = "root";
-//    std::string newValueA = "f541478e-e705-46e7-8801-155dd28bf4eb";
-//    std::string originalA = "5462f6e1-8a5d-4d2e-82c4-8739ac16ca04";
-//
-//    std::string newValueB = "28b9ae61-7734-4492-8a43-12ed385249b2";
-//    std::string originalB = "05290a9f-6795-431d-8999-140af520edef";
-//    pathReader.editJSON(testProjectA, testTreeA, fieldToEdit, originalA);
-//    pathReader.editJSON(testProjectA, testTreeB, fieldToEdit, originalB);
-//
-//    //first we test if the root is equal to the original for both
-//    std::map<std::string, bt::BehaviorTree> testMap = dummyFactory.getProject(testProjectA);
-//    bt::BehaviorTree treeA = testMap[testTreeA];
-//    bt::BehaviorTree treeB = testMap[testTreeB];
-//
-//    // The tree of A has no null pointer already
-//
-//    ASSERT_NE(treeA.GetRoot(), nullptr);
-//    //Now we change both values in the .json file
-//    pathReader.editJSON(testProjectA, testTreeA, fieldToEdit, newValueA);
-//    pathReader.editJSON(testProjectA, testTreeB, fieldToEdit, newValueB);
-//    // Update ONLY treeA
-//    dummyFactory.updateTree(testProjectA, testTreeA);
-//
-//    testMap = dummyFactory.getProject(testProjectA);
-//    bt::BehaviorTree treeAchanged = testMap[testTreeA];
-//    bt::BehaviorTree treeBchanged = testMap[testTreeB];
-//
-//    // The pointer in A changed but the pointer of treeB did not change.
-//    ASSERT_EQ(treeAchanged.GetRoot(), nullptr);
-//    ASSERT_EQ(treeBchanged.GetRoot(), treeB.GetRoot());
-//
-//    dummyFactory.updateProject(testProjectA);
-//
-//    testMap = dummyFactory.getProject(testProjectA);
-//    bt::BehaviorTree treeAchangedtwice = testMap[testTreeA];
-//    bt::BehaviorTree treeBchangedtwice = testMap[testTreeB];
-//
-//    ASSERT_EQ(treeAchangedtwice.GetRoot(), nullptr);
-//    ASSERT_NE(treeBchangedtwice.GetRoot(), treeBchanged.GetRoot());
-//    ASSERT_NE(treeBchangedtwice.GetRoot(), nullptr);
-//    //Change back the .json file and check this
-//    pathReader.editJSON(testProjectA, testTreeA, fieldToEdit, originalA);
-//    pathReader.editJSON(testProjectA, testTreeB, fieldToEdit, originalB);
-//
-//}
 
 
