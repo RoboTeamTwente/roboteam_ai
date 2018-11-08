5f54946cf083503daad6fafc6b97d0590878cae8
mrlukasbos <mrlukasbos@gmail.com>
2018-11-08 09:48:21 +0100

    Further updates to strategynode

--- roboteam_ai/src/roboteam_ai.cpp
+++ roboteam_ai/src/roboteam_ai.cpp
@@ -1,8 +1,8 @@
 #include "ros/ros.h"
-#include "io/StrategyIOManager.h"
 #include "DangerFinder/DangerFinder.h"
 #include "io/IOManager.h"
 #include "treeinterp/TreeInterpreter.h"
+#include "utilities/RefStateManager.hpp"
 
 namespace df = rtt::ai::dangerfinder;
 namespace io = rtt::ai::io;
@@ -11,37 +11,45 @@ namespace ai = rtt::ai;
 roboteam_msgs::World worldMsg;
 
 int main(int argc, char* argv[]) {
+    // Init ROS node
     ros::init(argc, argv, "StrategyNode");
 
     // init IOManager and subscribe to all topics immediately
     io::IOManager IOManager(true);
-    ros::Rate rate(10);
-
     roboteam_msgs::World worldMsg;
     roboteam_msgs::GeometryData geometryMsg;
 
-    /*
-     * what we will do:
-     *
-     * We will loop through frames.
-     * each frame we will decide on a strategy (tick through strat tree)
-     * based on that strategy we will tick through role tree
-     *
-     */
+    bt::BehaviorTree strategy;
+
+    // start looping
+    // set the framerate to 50 Hz
+    ros::Rate rate(50);
 
     while (ros::ok()) {
         ros::spinOnce();
 
+        // make ROS worldstate and geometry data globally accessible
         worldMsg = IOManager.getWorldState();
         geometryMsg = IOManager.getGeometryData();
         ai::World::set_world(worldMsg);
         ai::Field::set_field(geometryMsg.field);
 
-        bt::BehaviorTree strategyTree = TreeInterpreter::getInstance().getTreeWithID("ai_project", "id of the tree");
-        strategyTree.Tick();
+        strategy = TreeInterpreter::getInstance().getTreeWithID("ai_project", "id of the tree");
+        bt::Node::Status status = strategy.Update();
 
-
+        if (status != bt::Node::Status::Running) {
+            auto statusStr = bt::statusToString(status);
+            // return failure, success or invalid
+            ROS_DEBUG_STREAM_NAMED("Roboteam_ai", "Strategy result: " << statusStr.c_str() << "Shutting down...\n");
+            break;
+        }
         rate.sleep();
     }
+
+    // Terminate if needed
+    if (strategy.getStatus() == bt::Node::Status::Running) {
+        strategy.Terminate(bt::Node::Status::Running);
+    }
+
     return 0;
 }
