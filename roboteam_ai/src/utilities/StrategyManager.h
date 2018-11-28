2017cc42cdac3bf4f277c3e7b655639e01538bbe
mrlukasbos <mrlukasbos@gmail.com>
2018-11-28 10:45:31 +0100

    uncomment strategies in strategymanager

--- roboteam_ai/src/utilities/StrategyManager.h
+++ roboteam_ai/src/utilities/StrategyManager.h
@@ -35,33 +35,33 @@ private:
 
     std::vector<StrategyMap> strategyMaps = {
             {RefGameState::NORMAL_START,         "bigjson/NormalPlay"},
-        {RefGameState::FORCED_START,         "bigjson/009472f6-0d76-4db6-8161-a536bf497f89"}
-//        {RefGameState::HALT,                 "rtt_dennis/HaltStrategy"},
-//        {RefGameState::STOP,                 "rtt_anouk/StopStrat"},
-//        {RefGameState::TIMEOUT_US,           "rtt_anouk/StopStrat"},
-//        {RefGameState::TIMEOUT_THEM,         "rtt_anouk/StopStrat"},
-//        {RefGameState::GOAL_US,              "rtt_anouk/StopStrat"},
-//        {RefGameState::GOAL_THEM,            "rtt_anouk/StopStrat"},
-//        {RefGameState::BALL_PLACEMENT_US,    "rtt_anouk/BallPlacement_Strat"},
-//        {RefGameState::BALL_PLACEMENT_THEM,  "rtt_anouk/BallPlacementThemStrat"},
-//
-//        //  Strategies with a follow up strategy
-//        {RefGameState::PREPARE_KICKOFF_US,   "rtt_emiel/PrepareKickoffUsStrategy", RefGameState::DO_KICKOFF},
-//        {RefGameState::PREPARE_KICKOFF_THEM, "rtt_emiel/PrepareKickoffThemStrategy", RefGameState::DEFEND_KICKOFF},
-//        {RefGameState::PREPARE_PENALTY_US,   "rtt_emiel/PreparePenaltyUsStrategy", RefGameState::DEFEND_PENALTY},
-//        {RefGameState::PREPARE_PENALTY_THEM, "rtt_emiel/PreparePenaltyThemStrategy", RefGameState::DO_PENALTY},
-//
-//        {RefGameState::DIRECT_FREE_US,       "rtt_jim/NormalPlay"},
-//        {RefGameState::DIRECT_FREE_THEM,     "rtt_anouk/PrepareDirectThem"},
-//        {RefGameState::INDIRECT_FREE_US,     "rtt_emiel/IndirectUsStrategy"},
-//        {RefGameState::INDIRECT_FREE_THEM,   "rtt_anouk/PrepareDirectThem"},
-//
-//        // these are called after PREPARE_
-//        // these custom strategies need special attention
-//        {RefGameState::DO_KICKOFF,           "rtt_bob/KickoffWithChipStrategy"},
-//        {RefGameState::DEFEND_KICKOFF,       "rtt_jim/KickOffDefenseStrat"},
-//        {RefGameState::DEFEND_PENALTY,       "rtt_emiel/PreparePenaltyThemStrategy"},
-//        {RefGameState::DO_PENALTY,           "rtt_jim/TakePenalty"}
+        {RefGameState::FORCED_START,         "bigjson/009472f6-0d76-4db6-8161-a536bf497f89"},
+        {RefGameState::HALT,                 "rtt_dennis/HaltStrategy"},
+        {RefGameState::STOP,                 "rtt_anouk/StopStrat"},
+        {RefGameState::TIMEOUT_US,           "rtt_anouk/StopStrat"},
+        {RefGameState::TIMEOUT_THEM,         "rtt_anouk/StopStrat"},
+        {RefGameState::GOAL_US,              "rtt_anouk/StopStrat"},
+        {RefGameState::GOAL_THEM,            "rtt_anouk/StopStrat"},
+        {RefGameState::BALL_PLACEMENT_US,    "rtt_anouk/BallPlacement_Strat"},
+        {RefGameState::BALL_PLACEMENT_THEM,  "rtt_anouk/BallPlacementThemStrat"},
+
+        //  Strategies with a follow up strategy
+        {RefGameState::PREPARE_KICKOFF_US,   "rtt_emiel/PrepareKickoffUsStrategy", RefGameState::DO_KICKOFF},
+        {RefGameState::PREPARE_KICKOFF_THEM, "rtt_emiel/PrepareKickoffThemStrategy", RefGameState::DEFEND_KICKOFF},
+        {RefGameState::PREPARE_PENALTY_US,   "rtt_emiel/PreparePenaltyUsStrategy", RefGameState::DEFEND_PENALTY},
+        {RefGameState::PREPARE_PENALTY_THEM, "rtt_emiel/PreparePenaltyThemStrategy", RefGameState::DO_PENALTY},
+
+        {RefGameState::DIRECT_FREE_US,       "rtt_jim/NormalPlay"},
+        {RefGameState::DIRECT_FREE_THEM,     "rtt_anouk/PrepareDirectThem"},
+        {RefGameState::INDIRECT_FREE_US,     "rtt_emiel/IndirectUsStrategy"},
+        {RefGameState::INDIRECT_FREE_THEM,   "rtt_anouk/PrepareDirectThem"},
+
+        // these are called after PREPARE_
+        // these custom strategies need special attention
+        {RefGameState::DO_KICKOFF,           "rtt_bob/KickoffWithChipStrategy"},
+        {RefGameState::DEFEND_KICKOFF,       "rtt_jim/KickOffDefenseStrat"},
+        {RefGameState::DEFEND_PENALTY,       "rtt_emiel/PreparePenaltyThemStrategy"},
+        {RefGameState::DO_PENALTY,           "rtt_jim/TakePenalty"}
     };
 };
 
