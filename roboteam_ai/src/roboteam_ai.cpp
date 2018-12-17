72480eb5624a046a37f82ddaa4f4da131a36090f
barisimre <b.imre@student.utwente.nl>
2018-12-17 10:57:28 +0100

    Fix proper terminate

--- roboteam_ai/src/roboteam_ai.cpp
+++ roboteam_ai/src/roboteam_ai.cpp
@@ -67,12 +67,12 @@ void runBehaviourTrees() {
 
         strategy = factory.getTree(BTFactory::getCurrentTree());
 
-
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+//
+//        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
         Status status = strategy->tick();
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+//        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+//
+//        std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
    //     std::cout << "Tick took:  " << time_span.count()*1000 << " ms." << std::endl;
 
         switch (status) {
