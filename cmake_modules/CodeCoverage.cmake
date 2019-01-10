79243936b7e92ce0d4e427705b906d0ec8b7fc55
Lukas Bos <l.bos@roboteamtwente.nl>
2019-01-10 16:06:35 +0100

    use cleaned lcov file

--- cmake_modules/CodeCoverage.cmake
+++ cmake_modules/CodeCoverage.cmake
@@ -162,12 +162,12 @@ function(SETUP_TARGET_FOR_COVERAGE_LCOV)
             COMMAND ${Coverage_EXECUTABLE} ${Coverage_EXECUTABLE_ARGS}
 
             # Capturing lcov counters and generating report
-            COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --directory . --capture --remove ${COVERAGE_LCOV_EXCLUDES} --output-file ${Coverage_NAME}.info
+            COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --directory . --capture --output-file ${Coverage_NAME}.info
             # add baseline counters
             COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --output-file ${Coverage_NAME}.total
             COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --remove ${Coverage_NAME}.total ${COVERAGE_LCOV_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
             COMMAND ${GENHTML_PATH} -o ${Coverage_NAME} ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
-            COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.base ${Coverage_NAME}.total ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
+            COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.base ${Coverage_NAME}.total
 
             WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
             DEPENDS ${Coverage_DEPENDENCIES}
