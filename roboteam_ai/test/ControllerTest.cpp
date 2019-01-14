191078073fbc452c56c8ae5db1bc98a5267ef028
Lukas Bos <l.bos@roboteamtwente.nl>
2019-01-14 10:33:06 +0100

    test pid and pir

--- roboteam_ai/test/ControllerTest.cpp
+++ roboteam_ai/test/ControllerTest.cpp
@@ -12,13 +12,38 @@ TEST(ControllerTest, it_calculates_proper_pid) {
     Controller c;
     ASSERT_EQ(c.controlPID(100), 0);
 
-    c = Controller(100, 200, 300);
-    ASSERT_EQ(c.controlP(12), 1200);
-    ASSERT_EQ(c.controlI(12), 0);
-    ASSERT_EQ(c.controlD(12), 0);
-    ASSERT_EQ(c.controlPID(100), 1200);
+    c = Controller(10, 20, 30);
+    double expectedP = 120;
+    ASSERT_EQ(c.controlP(12), expectedP);
+    double expectedI = (1.0/rtt::ai::constants::tickRate) * 12 * 20;
+    ASSERT_EQ(c.controlI(12), expectedI);
+    double expectedD = 30 * (12 / (1.0/rtt::ai::constants::tickRate));
+    ASSERT_EQ(c.controlD(12), expectedD);
 
+    // clear controller otherwise timeDiff makes the values different
+    c = Controller(10, 20, 30);
+    ASSERT_EQ(c.controlPID(12), expectedP + expectedI + expectedD);
 }
+
+TEST(ControllerTest, it_calculates_proper_pir) {
+    Controller c;
+    ASSERT_EQ(c.controlPIR(100, 100), 0);
+
+    c = Controller(10, 20, 30);
+    double expectedP = 120;
+    ASSERT_EQ(c.controlP(12), expectedP);
+    double expectedI = (1.0/rtt::ai::constants::tickRate) * 12 * 20;
+    ASSERT_EQ(c.controlI(12), expectedI);
+    double expectedR = 30 * 12 * -1;
+    ASSERT_EQ(c.controlR(12), expectedR);
+
+
+    // clear controller otherwise timeDiff makes the values different
+    c = Controller(10, 20, 30);
+    ASSERT_EQ(c.controlPIR(12, 12), expectedP + expectedI + expectedR);
 }
-}
-}
+
+
+} // control
+} // ai
+} // rtt
