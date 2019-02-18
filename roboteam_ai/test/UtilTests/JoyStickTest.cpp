e7a2f4b6eaf85cf736ab7077e9797b78d7c239c0
barisimre <b.imre@student.utwente.nl>
2019-02-18 11:02:36 +0100

    Add test class :(

---
+++ roboteam_ai/test/UtilTests/JoyStickTest.cpp
@@ -0,0 +1,17 @@
+//
+// Created by baris on 15-2-19.
+//
+
+#include <gtest/gtest.h>
+#include "roboteam_ai/src/io/IOManager.h"
+
+namespace {
+
+TEST(JoyStickTest, joy) {
+
+    EXPECT_TRUE(true);
+
+}
+
+
+}
