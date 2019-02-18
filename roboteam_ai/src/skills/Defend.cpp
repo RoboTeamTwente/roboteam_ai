c9f1af5945e54953545cd6f35014d614e85bdc65
Thijs Luttikhuis <41472608+ThijsLuttikhuis@users.noreply.github.com>
2019-02-18 16:56:37 +0100

    Feature/ObstacleAvoidance (#138)
    * Clean up gtpLuTh code
    Add comments
    
    * Fix requested change
    
    * Fix requested change
    
    * Fix requested change
    
    * Fix requested change
    
    * Refactor CGTPClean
    
    * Improved pathfinding goToPosClean, calculation is longer though
    
    * Implemented robotcommands/movement in gtpclean
    
    * Fix compile time warnings
    Start Force-model within CGTPC
    
    * NewLine?
    
    * fix merge
    
    * Add force field when other robot is close
    
    * Fix compile warnings
    
    * Stufffff
    
    * Add debug variables for gotoposclean
    
    * Add seperate PID for position and velocity control in GTP
    
    * fix test
    
    * fix test v2.0.0.0
    
    * fix test v2.0.0.2.a

--- roboteam_ai/src/skills/Defend.cpp
+++ roboteam_ai/src/skills/Defend.cpp
@@ -14,7 +14,7 @@ void Defend::onInitialize() {
 
 bt::Node::Status Defend::onUpdate() {
     Vector2 targetLocation = coach::Coach::getDefensivePosition(robot->id);
-    auto velocities = gtp.goToPos(robot, targetLocation, control::GoToType::luTh);
+    auto velocities = gtp.goToPos(robot, targetLocation, control::GoToType::clean);
 
     roboteam_msgs::RobotCommand cmd;
     cmd.id = robot->id;
