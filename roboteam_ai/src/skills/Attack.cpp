e234b245ba3419116a2741239fbb70f631280596
barisimre <b.imre@student.utwente.nl>
2019-02-20 11:37:56 +0100

    Fix demo

--- roboteam_ai/src/skills/Attack.cpp
+++ roboteam_ai/src/skills/Attack.cpp
@@ -39,7 +39,7 @@ bt::Node::Status Attack::onUpdate() {
         targetPos = ball;
         command.use_angle = 1;
         command.w = static_cast<float>(((Vector2) {- 1.0, - 1.0}*deltaBall).angle());
-        if (World::ourBotHasBall(robot->id)) {
+        if (World::ourBotHasBall(robot->id, Constants::MAX_KICK_RANGE())) {
             command.kicker = 1;
             command.kicker_vel = static_cast<float>(rtt::ai::Constants::MAX_KICK_POWER());
             command.kicker_forced = 1;
