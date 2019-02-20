2c0586bc2a334aac4316bc048bc9cbd26837d595
rolfvdhulst <rolfvdhulst@gmail.com>
2019-02-20 11:10:30 +0100

    initial header creation

--- roboteam_ai/src/utilities/DefensiveCoach.cpp
+++ roboteam_ai/src/utilities/DefensiveCoach.cpp
@@ -3,9 +3,17 @@
 //
 
 #include "DefensiveCoach.h"
+
+/// This is a class that returns the positions we want our defenders to be at for all defenders
 namespace rtt{
 namespace ai{
 namespace coach{
+
+/// returns a vector with robot ID's and pass likelyhood
+std::vector<std::pair<int,double>> DefensiveCoach::possiblePassTargets() {
+    std::vector<std::pair<int,double>> passTargets;
+    //first find if one of their bots has the ball and if so which
+}
 
 }
 }
--- roboteam_ai/src/utilities/DefensiveCoach.h
+++ roboteam_ai/src/utilities/DefensiveCoach.h
@@ -5,13 +5,24 @@
 #ifndef ROBOTEAM_AI_DEFENSIVECOACH_H
 #define ROBOTEAM_AI_DEFENSIVECOACH_H
 #include "roboteam_utils/Vector2.h"
-
+#include "../utilities/World.h"
 namespace rtt{
 namespace ai{
 namespace coach{
 class DefensiveCoach {
     private:
-        std::priority_queue<
+        //BLOCKPOS actively covers the goal up from the ball or robots who can receive the ball and score
+        //PASSIVEINTERCEPTPOS tries to go to a position where in case of a pass to another robot it can intercept it in case of a pass
+        //ACTIVEINTERCEPTPOS is actively blocking intercepts to other robots
+        enum PosType{BLOCKPOS, PASSIVEINTERCEPTPOS};
+        std::map<int,Vector2> robotWithPos;
+        Vector2 computeBlockPos();
+        Vector2 computePassiveInterceptPos();
+        Vector2 computeActiveInterceptPos();
+        //pass detection
+        bool theyCanPass();
+        std::vector<std::pair<int,double>> possiblePassTargets();
+
     public:
 };
 
