0945fc08a5acbff65268687b563cadf1d0bedaaf
rolfvdhulst <rolfvdhulst@gmail.com>
2019-02-20 10:06:29 +0100

    GetBall Overshoot testing

---
+++ roboteam_ai/src/utilities/DefensiveCoach.cpp
@@ -0,0 +1,12 @@
+//
+// Created by rolf on 18-2-19.
+//
+
+#include "DefensiveCoach.h"
+namespace rtt{
+namespace ai{
+namespace coach{
+
+}
+}
+}
---
+++ roboteam_ai/src/utilities/DefensiveCoach.h
@@ -0,0 +1,23 @@
+//
+// Created by rolf on 18-2-19.
+//
+
+#ifndef ROBOTEAM_AI_DEFENSIVECOACH_H
+#define ROBOTEAM_AI_DEFENSIVECOACH_H
+#include "roboteam_utils/Vector2.h"
+
+namespace rtt{
+namespace ai{
+namespace coach{
+class DefensiveCoach {
+    private:
+        std::priority_queue<
+    public:
+};
+
+}
+}
+}
+
+
+#endif //ROBOTEAM_AI_DEFENSIVECOACH_H
