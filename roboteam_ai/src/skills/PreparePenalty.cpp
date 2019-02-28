5471e3bcf2c4d45af6db36a70c8ab3040d838185
barisimre <b.imre@student.utwente.nl>
2019-02-28 12:10:02 +0100

    Add prepare penalty

---
+++ roboteam_ai/src/skills/PreparePenalty.cpp
@@ -0,0 +1,12 @@
+//
+// Created by baris on 27-2-19.
+//
+
+#include "PreparePenalty.h"
+#include "Skill.h"
+
+namespace rtt {
+namespace ai {
+
+}
+}
---
+++ roboteam_ai/src/skills/PreparePenalty.h
@@ -0,0 +1,27 @@
+//
+// Created by baris on 27-2-19.
+//
+
+#ifndef ROBOTEAM_AI_PREPAREPENALTY_H
+#define ROBOTEAM_AI_PREPAREPENALTY_H
+
+#include "Skill.h"
+
+namespace rtt {
+namespace ai {
+
+class PreparePenalty : public Skill {
+
+        /**
+         * Things to prepare for a penalty
+         * 1. Keeper calculation
+         * 2. Geneva
+         * 3. Orientation
+         * 4. Then wait
+         */
+
+};
+}
+}
+
+#endif //ROBOTEAM_AI_PREPAREPENALTY_H
