6373519d3dc621434b9c6ad5a00f322d5df42ff9
rolfvdhulst <rolfvdhulst@gmail.com>
2019-03-18 09:58:42 +0100

    change model, works quite alright now in most situations.

--- roboteam_ai/src/utilities/BallModel.cpp
+++ roboteam_ai/src/utilities/BallModel.cpp
@@ -4,8 +4,8 @@
 
 #include <roboteam_ai/src/control/ControlUtils.h>
 #include "BallModel.h"
-namespace rtt{
-namespace ai{
+namespace rtt {
+namespace ai {
 /// what do we want:
 /// Inelastic collision detection. Kick detection. Chip detection and prediction (maybe even height estimation).
 /// Time prediction (deceleration)
@@ -14,56 +14,74 @@ bool BallModel::ballInAir;
 double BallModel::lastKickVel;
 bool BallModel::collidesNow;
 bool BallModel::kickedNow;
-int BallModel::ballStraightTicks=0;
+int BallModel::ballStraightTicks = 0;
 void BallModel::updateBallModel(roboteam_msgs::WorldBall newBall) {
     // check for collisions
-    Vector2 lastVel=Vector2(currentBall.vel);
-    Vector2 newVel= Vector2(newBall.vel);
+    Vector2 lastVel = Vector2(currentBall.vel);
+    Vector2 newVel = Vector2(newBall.vel);
     //TODO: this model need to be tuned for real life / the noise levels need to be tested esp
-    double maxDecelleration=5.0; // m/s^2
-    double maxAngleDeviation=5.0/180.0*M_PI;
-    double threshold=0.1;// m/s
-    double baseNoise=0.2;// m/s
-    double slope=0.2 ; // m/s / m/s
-    double velNoiseMax;
-    if (lastVel.length()<threshold){
-        velNoiseMax=baseNoise;
+    double maxDecelleration = 5.0; // m/s^2
+    double linearDeviation, orthogonalDeviation;
+    //noise rectangle; rectangle within the velocity state diagram where the velocity is still considered 'good'
+    double linearTreshhold=0.1;
+    double baseLinear=0.1;
+    double linearSlope=0.1;
+    double orthogonaltreshHold=0.1;
+    double baseOrthogo=0.1;
+    double orthogoSlope=0.01;
+    double linearLen=0;
+    if (lastVel.length()!=0&&newVel.length()!=0) {
+        Vector2 projection = newVel.project2(lastVel);
+        linearLen = projection.length();
     }
-    else {
-        velNoiseMax=baseNoise+slope*(lastVel.length()-threshold);
+    double hypotLen=newVel.length();
+    double orthogoLen=0;
+    if (hypotLen>=linearLen) {
+        orthogoLen = sqrt(hypotLen*hypotLen - linearLen*linearLen);
     }
 
-    if ((lastVel-newVel).length()>velNoiseMax){
-        // decelleration in the direction; just ball rolling
-        double angDif=control::ControlUtils::angleDifference(lastVel.angle(),newVel.angle());
-        double minLen=lastVel.length()-maxDecelleration*1/60-velNoiseMax;
-        if(angDif<=maxAngleDeviation&&newVel.length()>minLen){
-            collidesNow=false;
-            ballStraightTicks++;
-            kickedNow=false;
-        }
-        else{
-            std::cout<<angDif<< " : "<<maxAngleDeviation<< "|| " << newVel.length() <<" :" <<minLen<<std::endl;
-            ballStraightTicks=0;
-            // kicked or collided
-            if (newVel.length()>(lastVel.length()+velNoiseMax)){
-                kickedNow=true;
-                std::cout<<"KICKED"<<std::endl;
-
-            }
-            else{
-                collidesNow=true;
-                std::cout<<"COLLIDED"<<std::endl;
-            }
-        }
+    if (linearLen<linearTreshhold){
+        linearDeviation=baseLinear;
     }
     else{
-        collidesNow=false;
-        ballStraightTicks++;
-        kickedNow=false;
+        linearDeviation=baseLinear+linearSlope*(linearLen-linearTreshhold);
     }
-    currentBall=newBall;
-    std::cout<<ballStraightTicks<<std::endl;
+    if (orthogoLen<orthogonaltreshHold){
+        orthogonalDeviation=baseOrthogo;
+    }
+    else{
+        orthogonalDeviation=baseOrthogo+orthogoSlope*(orthogoLen-orthogonaltreshHold);
+    }
+
+    // defining the rectangle
+    Vector2 centerLow = lastVel - lastVel.stretchToLength(linearDeviation + maxDecelleration/60);
+    Vector2 centerHigh = lastVel + lastVel.stretchToLength(linearDeviation);
+    Vector2 orthogoVec = Vector2(orthogonalDeviation, 0).rotate(lastVel.angle() + M_PI_2);
+    Vector2 point1 = centerLow + orthogoVec;
+    Vector2 point2 = centerLow - orthogoVec;
+    Vector2 point3 = centerHigh - orthogoVec;
+    Vector2 point4 = centerHigh + orthogoVec;
+
+    if (!control::ControlUtils::pointInRectangle(newVel,point1,point2,point3,point4)) {
+        // decelleration in the direction; just ball rolling
+
+        ballStraightTicks = 0;
+        // kicked or collided
+        if (newVel.length() > (lastVel.length()+linearDeviation)) {
+            kickedNow = true;
+            std::cout << "KICKED" << std::endl;
+        }
+        else {
+            collidesNow = true;
+            std::cout << "COLLIDED" << std::endl;
+        }
+    }
+    else {
+        collidesNow = false;
+        ballStraightTicks ++;
+        kickedNow = false;
+    }
+    currentBall = newBall;
 }
 bool BallModel::ballCollided() {
     return collidesNow;
