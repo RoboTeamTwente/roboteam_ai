c2cb95da701527a9456db0a82dee76d8f56ca7b2
Lukas Bos <mrlukasbos@gmail.com>
2019-03-19 10:32:27 +0100

    Update ApplicationManager.cpp

--- roboteam_ai/src/ApplicationManager.cpp
+++ roboteam_ai/src/ApplicationManager.cpp
@@ -23,7 +23,7 @@ void ApplicationManager::setup() {
 }
 
 void ApplicationManager::loop() {
-    ros::Rate rate(60);
+    ros::Rate rate(ai::Constants::TICK_RATE());
     double longestTick = 0.0;
     double timeTaken;
     int nTicksTaken = 0;
