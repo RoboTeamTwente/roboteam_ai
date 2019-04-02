3301d1c186d89a3cc7248d6b4975634147a47d31
barisimre <b.imre@student.utwente.nl>
2019-04-02 15:35:10 +0200

    Add fromation caoch

---
+++ roboteam_ai/src/coach/FormationCoach.h
@@ -0,0 +1,24 @@
+//
+// Created by baris on 2-4-19.
+//
+
+#ifndef ROBOTEAM_AI_FORMATIONCOACH_H
+#define ROBOTEAM_AI_FORMATIONCOACH_H
+namespace rtt {
+namespace ai {
+namespace coach {
+
+class FormationCoach {
+
+
+
+};
+
+
+
+
+}
+}
+}
+
+#endif //ROBOTEAM_AI_FORMATIONCOACH_H
