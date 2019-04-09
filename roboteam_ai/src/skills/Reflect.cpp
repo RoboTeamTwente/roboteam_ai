ee4a6603cd21d4d317b5255c0d3564b5996bb7ba
robzelluf <robzelluf@hotmail.com>
2019-04-09 17:45:20 +0200

    Begin reflect skill

--- roboteam_ai/src/skills/Reflect.cpp
+++ roboteam_ai/src/skills/Reflect.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Reflect.h"
+
+namespace rtt{
+namespace ai{
+
+Reflect::Reflect(string name, bt::Blackboard::Ptr blackboard)
+    :Skill(std::move(name), std::move(blackboard)) {
+}
+
+}
+}
