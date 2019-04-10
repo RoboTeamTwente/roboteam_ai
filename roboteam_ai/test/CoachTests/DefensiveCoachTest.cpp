d2c301666c9b1a3b0f2933bcc8ad6e27014d5b1d
rolfvdhulst <rolfvdhulst@gmail.com>
2019-04-10 10:31:18 +0200

    fix tests

--- roboteam_ai/test/CoachTests/DefensiveCoachTest.cpp
+++ roboteam_ai/test/CoachTests/DefensiveCoachTest.cpp
@@ -13,11 +13,11 @@ TEST(DefensiveCoachTest,it_calculates_blockPoint) {
     rtt::Vector2 pointPos(0, 0);
     auto segment = std::make_pair(startPos, endPos);
     double colllisionRadius=w::Constants::ROBOT_RADIUS()+w::Constants::BALL_RADIUS();
-    auto blockPoint = cc::g_defensiveCoach.getBlockPoint(segment, pointPos,colllisionRadius);
+    auto blockPoint= cc::g_defensivePositionCoach.getBlockPoint(segment, pointPos,colllisionRadius);
     ASSERT_FLOAT_EQ(blockPoint.angle(), M_PI_4);
     ASSERT_FLOAT_EQ(blockPoint.x, colllisionRadius);
     ASSERT_FLOAT_EQ(blockPoint.y, colllisionRadius);
     rtt::Vector2 middlePoint(3,3),checkPoint(3,1.5);
-    auto blockPoint2 = cc::g_defensiveCoach.getBlockPoint(std::make_pair(middlePoint,endPos), pointPos,colllisionRadius);
+    auto blockPoint2 = cc::g_defensivePositionCoach.getBlockPoint(std::make_pair(middlePoint,endPos), pointPos,colllisionRadius);
     ASSERT_FLOAT_EQ(blockPoint2.angle(),checkPoint.angle());
 }
