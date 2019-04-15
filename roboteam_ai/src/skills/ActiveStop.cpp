eacf1d47e684c8c1bd4c95cf2e24b2265afd44a5
barisimre <b.imre@student.utwente.nl>
2019-04-15 16:39:40 +0200

    More things

---
+++ roboteam_ai/src/skills/ActiveStop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roboteam on 15-4-19.
+//
+
+#include "ActiveStop.h"
---
+++ roboteam_ai/src/skills/ActiveStop.h
@@ -0,0 +1,12 @@
+//
+// Created by roboteam on 15-4-19.
+//
+
+#ifndef ROBOTEAM_AI_ACTIVESTOP_H
+#define ROBOTEAM_AI_ACTIVESTOP_H
+
+class ActiveStop {
+
+};
+
+#endif //ROBOTEAM_AI_ACTIVESTOP_H
