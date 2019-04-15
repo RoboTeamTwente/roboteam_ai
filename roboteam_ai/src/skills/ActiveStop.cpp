fecb73e4aca31e888156c37d47c6b8c0196d2c32
barisimre <b.imre@student.utwente.nl>
2019-04-15 16:46:30 +0200

    Add active stop

--- roboteam_ai/src/skills/ActiveStop.cpp
+++ roboteam_ai/src/skills/ActiveStop.cpp
@@ -1,5 +1,64 @@
 //
-// Created by roboteam on 15-4-19.
+// Created by baris on 8-4-19.
 //
 
 #include "ActiveStop.h"
+namespace rtt{
+namespace ai {
+
+int ActiveStop::attack = -1;
+
+ActiveStop::ActiveStop(string name, bt::Blackboard::Ptr blackboard)
+        :Skill(name, blackboard) {
+}
+void ActiveStop::onInitialize() {
+    goToPos.setAvoidBall(0.8);
+    if (attack == -1) {
+        attack = robot->id;
+        attacker = true;
+    }
+}
+
+Skill::Status ActiveStop::onUpdate() {
+    if (attacker)
+        targetPos = getOffensiveActivePoint();
+    else
+        targetPos = getDefensiveActivePoint();
+
+    command.w = static_cast<float>((targetPos - robot->pos).angle());
+    Vector2 velocityRaw = goToPos.getPosVelAngle(robot, targetPos).vel;
+    Vector2 velocity = control::ControlUtils::velocityLimiter(velocityRaw, 1.2);
+    command.x_vel = static_cast<float>(velocity.x);
+    command.y_vel = static_cast<float>(velocity.y);
+    publishRobotCommand();
+    return Status::Running;
+
+
+}
+
+void ActiveStop::onTerminate(Skill::Status s) {
+    attack = -1;
+}
+
+Vector2 ActiveStop::getOffensiveActivePoint() {
+
+    Vector2 penaltyPos = rtt::ai::world::field->getPenaltyPoint(false);
+    Vector2 ballPos = rtt::ai::world::world->getBall()->pos;
+
+    Vector2 offset = (penaltyPos - ballPos).stretchToLength(0.666); // ssl rule + some buffer + rtt spirit
+    return ballPos + offset;
+
+}
+
+Vector2 ActiveStop::getDefensiveActivePoint() {
+
+    Vector2 penaltyPos = rtt::ai::world::field->getPenaltyPoint(false);
+    Vector2 ballPos = rtt::ai::world::world->getBall()->pos;
+
+    Vector2 offset = (penaltyPos - ballPos).stretchToLength(0.666); // ssl rule + some buffer + rtt spirit
+    return ballPos + offset;
+
+}
+
+}
+}
--- roboteam_ai/src/skills/ActiveStop.h
+++ roboteam_ai/src/skills/ActiveStop.h
@@ -1,12 +1,31 @@
 //
-// Created by roboteam on 15-4-19.
+// Created by baris on 8-4-19.
 //
 
 #ifndef ROBOTEAM_AI_ACTIVESTOP_H
 #define ROBOTEAM_AI_ACTIVESTOP_H
+#include "Skill.h"
+#include <roboteam_ai/src/control/positionControllers/NumTreePosControl.h>
 
-class ActiveStop {
+namespace rtt{
+namespace ai {
+
+
+class ActiveStop : public Skill {
+    public:
+        explicit ActiveStop(string name, bt::Blackboard::Ptr blackboard);
+        void onInitialize() override;
+        Status onUpdate() override;
+        void onTerminate(Status s) override;
+    private:
+        Vector2 targetPos;
+        control::NumTreePosControl goToPos;
+        static int attack;
+        bool attacker = false;
+        static Vector2 getOffensiveActivePoint();
+        static Vector2 getDefensiveActivePoint();
 
 };
-
+}
+}
 #endif //ROBOTEAM_AI_ACTIVESTOP_H
