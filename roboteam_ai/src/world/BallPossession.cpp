e46c483c62872f2b0c1ec51fff6017761811f79b
barisimre <b.imre@student.utwente.nl>
2019-04-16 16:02:57 +0200

    Fix ball placement

--- roboteam_ai/src/world/BallPossession.cpp
+++ roboteam_ai/src/world/BallPossession.cpp
@@ -32,7 +32,7 @@ void BallPossession::recomputeState() {
         state = LOOSEBALL;
     }
     // in the other cases we stay in the same state (so we do nothing)
-    std::cout << stateAsString() << std::endl;
+//    std::cout << stateAsString() << std::endl;
 }
 
 void BallPossession::updateTicks() {
