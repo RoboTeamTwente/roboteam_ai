5991dedef867f4fe66445e47b2e8b8e312d72bc6
mrlukasbos <mrlukasbos@gmail.com>
2019-04-16 14:29:31 +0200

    some cleaning up

--- roboteam_ai/src/world/BallPossession.cpp
+++ roboteam_ai/src/world/BallPossession.cpp
@@ -3,48 +3,51 @@
 //
 
 #include "BallPossession.h"
+
 namespace rtt {
 namespace ai {
+
 BallPossession bpTrackerObj;
-BallPossession* bpTracker= &bpTrackerObj;
+BallPossession *bpTracker = &bpTrackerObj;
+
 void BallPossession::update() {
     updateTicks();
     recomputeState();
 }
 
 void BallPossession::recomputeState() {
-    double closeTimeTreshHold=0.05;//s
-    double farTimeTreshHold=1.5;//s
-    bool weAreClose= closeToUsTime>closeTimeTreshHold;
-    bool theyAreClose=closeToThemTime>closeTimeTreshHold;
-    bool weAreFar=farFromUsTime>farTimeTreshHold;
-    bool theyAreFar=farFromThemTime>farTimeTreshHold;
-    if (weAreClose&&!theyAreClose){
-        state=OURBALL;
-    }
-    else if(theyAreClose&&!weAreClose){
-        state=THEIRBALL;
-    }
-    else if(weAreClose&&theyAreClose){
-        state=CONTENDEDBALL;
-    }
-    else if (weAreFar&&theyAreFar){
-        state=LOOSEBALL;
+
+    bool weAreClose = closeToUsTime > CLOSE_TIME_TRESHOLD;
+    bool theyAreClose = closeToThemTime > CLOSE_TIME_TRESHOLD;
+    bool weAreFar = farFromUsTime > FAR_TIME_TRESHOLD;
+    bool theyAreFar = farFromThemTime > FAR_TIME_TRESHOLD;
+
+    if (weAreClose && !theyAreClose) {
+        state = OURBALL;
+    } else if (theyAreClose && !weAreClose) {
+        state = THEIRBALL;
+    } else if (weAreClose && theyAreClose) {
+        state = CONTENDEDBALL;
+    } else if (weAreFar && theyAreFar) {
+        state = LOOSEBALL;
     }
     // in the other cases we stay in the same state (so we do nothing)
-    std::cout<<stateAsString()<<std::endl;
-}
-void BallPossession::updateTicks() {
-    world::WorldData wd=world::world->getWorld();
-    double timeDiff = world::world->timeDifference();
-    // if a team is close or far to the ball increment the timers, otherwise reset them
-    closeToUsTime = teamCloseToBall(wd,true) ? closeToUsTime + timeDiff : 0.0;
-    closeToThemTime = teamCloseToBall(wd,false) ? closeToThemTime + timeDiff : 0.0;
-    farFromUsTime = teamFarFromBall(wd,true) ? farFromUsTime + timeDiff : 0.0;
-    farFromThemTime = teamFarFromBall(wd,false) ? farFromThemTime + timeDiff : 0.0;
+    std::cout << stateAsString() << std::endl;
 }
 
-bool BallPossession::teamCloseToBall(const world::WorldData& world, bool ourTeam) {
+void BallPossession::updateTicks() {
+    world::WorldData wd = world::world->getWorld();
+    double timeDiff = world::world->timeDifference();
+
+    // if a team is close or far to the ball increment the timers, otherwise reset them
+    closeToUsTime = teamCloseToBall(wd, true) ? closeToUsTime + timeDiff : 0.0;
+    closeToThemTime = teamCloseToBall(wd, false) ? closeToThemTime + timeDiff : 0.0;
+    farFromUsTime = teamFarFromBall(wd, true) ? farFromUsTime + timeDiff : 0.0;
+    farFromThemTime = teamFarFromBall(wd, false) ? farFromThemTime + timeDiff : 0.0;
+}
+
+/// return true if given team is relatively close to ball
+bool BallPossession::teamCloseToBall(const world::WorldData &world, bool ourTeam) {
     double closeTreshHoldDist = Constants::MAX_BALL_RANGE();
     std::vector<world::Robot> robots = ourTeam ? world.us : world.them;
     for (auto robot : robots) {
@@ -54,26 +57,36 @@ bool BallPossession::teamCloseToBall(const world::WorldData& world, bool ourTeam
     }
     return false;
 }
-bool BallPossession::teamFarFromBall(const world::WorldData& world, bool ourTeam) {
+
+/// return true if given team is relatively far from ball
+bool BallPossession::teamFarFromBall(const world::WorldData &world, bool ourTeam) {
     double farThreshHoldDist = 0.4;
     std::vector<world::Robot> robots = ourTeam ? world.us : world.them;
-    for (const auto& robot :robots) {
+    for (const auto &robot :robots) {
         if ((robot.pos - world.ball.pos).length() < farThreshHoldDist) {
             return false;
         }
     }
     return true;
 }
+
 BallPossession::Possession BallPossession::getPossession() {
     return state;
 }
+
+// convert ballpossession states to strings
 std::string BallPossession::stateAsString() {
-    switch(state){
-    case OURBALL: return "OURBALL";
-    case THEIRBALL: return "THEIRBALL";
-    case CONTENDEDBALL: return "CONTENDEDBALL";
-    case LOOSEBALL: return "LOOSE";
+    switch (state) {
+        case OURBALL:
+            return "OURBALL";
+        case THEIRBALL:
+            return "THEIRBALL";
+        case CONTENDEDBALL:
+            return "CONTENDEDBALL";
+        case LOOSEBALL:
+            return "LOOSE";
     }
 }
-}
-}
+
+} // ai
+} // rtt
--- roboteam_ai/src/world/BallPossession.h
+++ roboteam_ai/src/world/BallPossession.h
@@ -6,28 +6,41 @@
 #define ROBOTEAM_AI_BALLPOSSESSION_H
 
 #include "World.h"
+
 namespace rtt {
 namespace ai {
+
 class BallPossession {
-    public:
-        enum Possession {LOOSEBALL, OURBALL, THEIRBALL, CONTENDEDBALL };
-    private:
-        Possession state=LOOSEBALL;
-        double closeToUsTime=0.0,closeToThemTime=0.0,farFromUsTime=0.0,farFromThemTime=0.0;
-        void updateTicks();
-        bool teamCloseToBall(const world::WorldData& world, bool ourTeam);
-        bool teamFarFromBall(const world::WorldData& world, bool ourTeam);
+public:
+    enum Possession {
+        LOOSEBALL,
+        OURBALL,
+        THEIRBALL,
+        CONTENDEDBALL
+    };
+    void update();
+    Possession getPossession();
+    void recomputeState();
+    std::string stateAsString();
 
-    public:
-        void update();
-        Possession getPossession();
-        void recomputeState();
-        std::string stateAsString();
+private:
+    const double CLOSE_TIME_TRESHOLD = 0.05;//s
+    const double FAR_TIME_TRESHOLD = 1.5;//s
 
+    Possession state = LOOSEBALL;
+    double closeToUsTime = 0.0;
+    double closeToThemTime = 0.0;
+    double farFromUsTime = 0.0;
+    double farFromThemTime = 0.0;
+    void updateTicks();
+    bool teamCloseToBall(const world::WorldData& world, bool ourTeam);
+    bool teamFarFromBall(const world::WorldData& world, bool ourTeam);
 };
+
 extern BallPossession bpTrackerObj;
 extern BallPossession* bpTracker;
-}
-}
+
+} // ai
+} // rtt
 
 #endif //ROBOTEAM_AI_BALLPOSSESSION_H
