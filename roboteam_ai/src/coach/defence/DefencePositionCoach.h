24432ec625465691b8c6222567bf5a44d3ce2335
rolfvdhulst <rolfvdhulst@gmail.com>
2019-04-24 12:41:56 +0200

    fix requested changes and refactor

--- roboteam_ai/src/coach/defence/DefencePositionCoach.h
+++ roboteam_ai/src/coach/defence/DefencePositionCoach.h
@@ -24,15 +24,12 @@ class DefencePositionCoach {
           world::Robot toRobot();
           bool validPosition(const world::WorldData &world);
         };
-        struct BlockPassBot : DefenderBot {
-          int blockToID;
-        };
         Vector2 getMostDangerousPos(const world::WorldData &world);
         std::vector<DefenderBot> decidePositions(int amount);
 
         DefenderBot createBlockBall(const Line &blockLine);
         DefenderBot createBlockToGoal(const PossiblePass &pass, double aggressionFactor, const Line &blockLine);
-        BlockPassBot createBlockPass(PossiblePass &pass, const Vector2 &blockPoint);
+        DefenderBot createBlockPass(PossiblePass &pass, const Vector2 &blockPoint);
         DefenderBot createBlockOnLine(const PossiblePass &pass, const Vector2 &blockPos);
 
         std::shared_ptr<Line> blockToGoalLine(const PossiblePass &pass, const world::WorldData &simulatedWorld);
@@ -49,7 +46,14 @@ class DefencePositionCoach {
         Vector2 getPosOnLine(const Line &line, double aggressionFactor);
         double getOrientation(const Line &line);
         Vector2 findPositionForBlockBall(const Line &line);
+
     private:
+        const double defenceLineMargin=0.15; //min distance the points are from defence area. Should atleast be robotradius large.
+        const double calculationCollisionRad=0.15; // distance at which our own robots are considered to be colliding in our calculation (prevents robots from stacking up too much)
+
+        world::WorldData simulatedWorld;
+        std::vector<DefenderBot> defenders;
+
         std::vector<PossiblePass> createPassesSortedByDanger(const world::WorldData &world);
         std::vector<PossiblePass> sortPassesByDanger(std::vector<std::pair<PossiblePass, double>> &passesWithDanger);
         std::vector<std::pair<PossiblePass, double>> createPassesAndDanger(const world::WorldData &world);
@@ -59,6 +63,12 @@ class DefencePositionCoach {
         bool validNewPosition(const Vector2& position,const world::WorldData& world);
         std::shared_ptr<double> pickNewPosition(const Line& line ,const world::WorldData& world);
         std::shared_ptr<Vector2> pickNewPosition(PossiblePass pass, const world::WorldData& world);
+
+        world::WorldData setupSimulatedWorld();
+        void blockMostDangerousPos();
+        bool blockPass(PossiblePass pass);
+        void addDefender(DefenderBot defender);
+
 
 };
 extern DefencePositionCoach g_defensivePositionCoach;
