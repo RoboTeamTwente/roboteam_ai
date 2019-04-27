b401cf1ac023b5bb9b43f92f3a2a855cce95ca4e
barisimre <b.imre@student.utwente.nl>
2019-04-27 14:44:27 +0200

    Take passing into account in ShouldHandleBall

--- roboteam_ai/src/conditions/ShouldHandleBall.cpp
+++ roboteam_ai/src/conditions/ShouldHandleBall.cpp
@@ -16,13 +16,27 @@ std::string ShouldHandleBall::node_name() { return "ShouldHandleBall"; }
 
 ShouldHandleBall::Status ShouldHandleBall::onUpdate() {
     if (coach::g_pass.getRobotBeingPassedTo() != -1) {
-        return Status::Failure;
+        if (coach::g_pass.getRobotPassing() == robot->id) {
+            return Status::Success;
+        } else {
+            return Status::Failure;
+        }
     }
 
     if (coach::getBallCoach->getBallGetterID() == robot->id) {
         return Status::Success;
     }
+
     return Status::Failure;
+}
+void ShouldHandleBall::onTerminate(Condition::Status s) {
+    Condition::onTerminate(s);
+
+    if (s == Status::Failure) {
+        if (coach::g_pass.getRobotPassing() == robot->id) {
+            coach::g_pass.resetPass();
+        }
+    }
 }
 
 }//ai
--- roboteam_ai/src/conditions/ShouldHandleBall.h
+++ roboteam_ai/src/conditions/ShouldHandleBall.h
@@ -14,6 +14,7 @@ class ShouldHandleBall : public Condition {
     public:
         explicit ShouldHandleBall(std::string name = "ShouldHandleBall", bt::Blackboard::Ptr blackboard = nullptr);
         Status onUpdate() override;
+        void onTerminate(Status s) override;
         std::string node_name() override;
 };
 }
