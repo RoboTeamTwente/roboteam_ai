f64e78de6b0d668114f4b5cf753fd5e13b57796e
RobZelluf <robzelluf@hotmail.com>
2019-04-29 13:13:21 +0200

    Create check for passing takes too long in main loop (#359)
    * Create check for passing takes too long in main loop
    
    * yada

--- roboteam_ai/src/ApplicationManager.cpp
+++ roboteam_ai/src/ApplicationManager.cpp
@@ -119,6 +119,7 @@ void ApplicationManager::runOneLoopCycle() {
 
         rtt::ai::coach::getBallCoach->update();
         rtt::ai::coach::g_DefenceDealer.updateDefenderLocations();
+        rtt::ai::coach::g_pass.updatePassProgression();
         Status status = strategy->tick();
         this->notifyTreeStatus(status);
 
