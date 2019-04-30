4cab09544e904ff456cef5db45bea0f05b152d9d
barisimre <b.imre@student.utwente.nl>
2019-04-30 14:55:58 +0200

    Change constatnt

--- roboteam_ai/src/conditions/BallIsMoving.cpp
+++ roboteam_ai/src/conditions/BallIsMoving.cpp
@@ -1,6 +1,7 @@
 
 /*
  * returns SUCCESS if the ball is moving
+ * USED FOR CHECKING GAME STATE CHANGES DONT USE IF YOU DONT KNOW WHEN TO
  */
 
 #include "../utilities/Constants.h"
@@ -17,8 +18,9 @@ bt::Node::Status BallIsMoving::onUpdate() {
     Vector2 ballVel=ball->vel;
 
     bool ballIsLayingStill = ballVel.length() < Constants::BALL_STILL_VEL();
+    auto refCommand = static_cast<RefGameState>(rtt::ai::Referee::getRefereeData().command.command);
 
-    if (ballIsLayingStill){
+    if (ballIsLayingStill || refCommand != RefGameState::NORMAL_START){
         return Status::Failure;
     }
     return Status::Success;
