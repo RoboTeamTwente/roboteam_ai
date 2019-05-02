655c4960464c2b0cf84e74cd0920ef3ddeead207
rolfvdhulst <rolfvdhulst@gmail.com>
2019-05-02 12:26:57 +0200

    more terminate fixes

--- roboteam_ai/src/skills/Receive.cpp
+++ roboteam_ai/src/skills/Receive.cpp
@@ -50,6 +50,8 @@ Receive::Status Receive::onUpdate() {
 }
 
 void Receive::onTerminate(Status s) {
+    readyToPassSet = false;
+    currentProgress = POSITIONING;
     if (passFailed() || coach::g_pass.getRobotBeingPassedTo() != robot->id || s == Status::Success) {
         coach::g_pass.resetPass(robot->id);
     }
