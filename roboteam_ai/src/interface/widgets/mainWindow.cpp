03fa25a1ee8dca3a0b8da1ea062480a3649e20ab
mrlukasbos <mrlukasbos@gmail.com>
2019-05-07 14:12:28 +0200

    clean up mainwindow

--- roboteam_ai/src/interface/widgets/mainWindow.cpp
+++ roboteam_ai/src/interface/widgets/mainWindow.cpp
@@ -16,8 +16,7 @@ namespace rtt {
 namespace ai {
 namespace interface {
 
-MainWindow::MainWindow(QWidget* parent)
-        :QMainWindow(parent) {
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     setMinimumWidth(800);
     setMinimumHeight(600);
 
@@ -27,43 +26,39 @@ MainWindow::MainWindow(QWidget* parent)
     horizontalLayout = new QHBoxLayout();
     vLayout = new QVBoxLayout();
 
-
-
+    // the main controls widget for the most crucial buttons
+    // changing strategies, goalie id, etc.
     auto mainControlsWidget = new MainControlsWidget(this);
     vLayout->addWidget(mainControlsWidget);
 
-    robotsWidget = new RobotsWidget(this);
+    // create widgets hidden under tabs
     treeWidget = new TreeVisualizerWidget(this);
     keeperTreeWidget = new TreeVisualizerWidget(this);
-
     auto visualizationSettingsWidget = new VisualizationSettingsWidget(visualizer, this);
     auto pidWidget = new PidsWidget();
-
+    robotsWidget = new RobotsWidget(this);
 
     // add the tab widget
     auto tabWidget = new QTabWidget;
     tabWidget->addTab(treeWidget, tr("Behaviour trees"));
+    tabWidget->addTab(keeperTreeWidget, tr("Keeper"));
     tabWidget->addTab(visualizationSettingsWidget, tr("Visualisation Settings"));
-
     tabWidget->addTab(pidWidget, tr("PID"));
     tabWidget->addTab(robotsWidget, tr("Robots"));
-    tabWidget->addTab(keeperTreeWidget, tr("Keeper"));
-
     vLayout->addWidget(tabWidget);
 
-    // set up the large widgets
+    // set up the general layout structure
+    // visualizer on the left, sidebar (with maincontrols and tabs) on the right.
     auto splitter = new QSplitter(); // the splitter is an horizontal view that allows to be changed by the user
     splitter->addWidget(visualizer);
     auto sideBarWidget = new QWidget;
     sideBarWidget->setLayout(vLayout);
     splitter->addWidget(sideBarWidget);
     splitter->setSizes({600, 200});
-
     horizontalLayout->addWidget(splitter);
     mainLayout->addLayout(horizontalLayout);
 
-
-    // apply layout
+    // apply layout to the window
     setCentralWidget(new QWidget);
     centralWidget()->setLayout(mainLayout);
 
@@ -77,9 +72,9 @@ MainWindow::MainWindow(QWidget* parent)
     connect(mainControlsWidget, SIGNAL(treeHasChanged()), keeperTreeWidget, SLOT(invalidateTree()));
 
     // start the UI update cycles
-    // these are slower
+    // these are slower than the tick rate
     auto * robotsTimer = new QTimer(this);
-     connect(robotsTimer, SIGNAL(timeout()), this, SLOT(updateTreeWidget()));
+    connect(robotsTimer, SIGNAL(timeout()), this, SLOT(updateTreeWidget()));
     connect(robotsTimer, SIGNAL(timeout()), this, SLOT(updateKeeperTreeWidget()));
     connect(robotsTimer, SIGNAL(timeout()), this, SLOT(updateRobotsWidget())); // we need to pass the visualizer so thats why a seperate function is used
     connect(robotsTimer, SIGNAL(timeout()), mainControlsWidget, SLOT(updatePause()));
@@ -90,27 +85,24 @@ MainWindow::MainWindow(QWidget* parent)
 /// Set up a checkbox and add it to the layout
 void MainWindow::configureCheckBox(QString title, QLayout * layout, const QObject* receiver, const char* method,
         bool defaultState) {
-
     auto checkbox = new QCheckBox(title);
     checkbox->setChecked(defaultState);
     layout->addWidget(checkbox);
     QObject::connect(checkbox, SIGNAL(clicked(bool)), receiver, method);
 }
 
-
+// when updating the robotswidget it needs the current visualizer state
 void MainWindow::updateRobotsWidget() {
     if (world::world->weHaveRobots())
         robotsWidget->updateContents(visualizer);
 }
 
-void MainWindow::setShowDebugValueInTerminal(bool showDebug) {
-    Output::setShowDebugValues(showDebug);
-}
-
+// update the tree widget with the newest strategy tree
 void MainWindow::updateTreeWidget() {
     this->treeWidget->updateContents(BTFactory::getTree(BTFactory::getCurrentTree()));
 }
 
+// update the keeper widget with the newest keeper tree
 void MainWindow::updateKeeperTreeWidget() {
    this->keeperTreeWidget->updateContents(BTFactory::getKeeperTree());
 }
