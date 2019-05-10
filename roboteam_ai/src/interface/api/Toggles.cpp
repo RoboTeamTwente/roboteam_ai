04db688e1531dfe5705e56e05a9508cd13895a6e
mrlukasbos <mrlukasbos@gmail.com>
2019-05-10 10:54:59 +0200

    formatting

--- roboteam_ai/src/interface/api/Toggles.cpp
+++ roboteam_ai/src/interface/api/Toggles.cpp
@@ -5,13 +5,13 @@ namespace ai {
 namespace interface {
 
 std::vector<Toggle> Toggles::toggles = {
-        {Visual::DEBUG,       showType::ALL_ROBOTS,      "Show debug values"},
-        {Visual::PATHFINDING, showType::SELECTED_ROBOTS, "Show final paths"},
-        {Visual::PATHFINDING_DEBUG, showType::NO_ROBOTS, "Show pathfinding tried paths"},
-        {Visual::KEEPER,      showType::NO_ROBOTS,       "Show keeper points"},
-        {Visual::INTERCEPT,   showType::ALL_ROBOTS,      "Show Interceptions"},
-        {Visual::DEFENSE,     showType::ALL_ROBOTS,      "Show defensive location"},
-        {Visual::SHOTLINES,   showType::ALL_ROBOTS,      "Show desired shotlines"},
+        {Visual::DEBUG,             showType::ALL_ROBOTS,       "Show debug values"},
+        {Visual::PATHFINDING,       showType::SELECTED_ROBOTS,  "Show final paths"},
+        {Visual::PATHFINDING_DEBUG, showType::NO_ROBOTS,        "Show pathfinding tried paths"},
+        {Visual::KEEPER,            showType::NO_ROBOTS,        "Show keeper points"},
+        {Visual::INTERCEPT,         showType::ALL_ROBOTS,       "Show Interceptions"},
+        {Visual::DEFENSE,           showType::ALL_ROBOTS,       "Show defensive location"},
+        {Visual::SHOTLINES,         showType::ALL_ROBOTS,       "Show desired shotlines"},
 };
 
 } // interface
