9c4d5c7fec30bdd9d5352247ff25bb9910406b0a
mrlukasbos <mrlukasbos@gmail.com>
2019-05-10 11:49:49 +0200

    remove double semicolons in constants.h

--- roboteam_ai/src/utilities/Constants.h
+++ roboteam_ai/src/utilities/Constants.h
@@ -22,7 +22,7 @@ public:
     static bool SHOW_TICK_TIME_TAKEN();
     static bool SHOW_NUMTREE_TIME_TAKEN();
     static bool SHOW_NUMTREE_DEBUG_INFO();
-    static bool SHOW_FULL_NUMTREE_DEBUG_INFO();;
+    static bool SHOW_FULL_NUMTREE_DEBUG_INFO();
 
 
     /// ROBOT AND RELATED ///
@@ -44,79 +44,79 @@ public:
     static double ROBOT_RADIUS(); // TODO: Need to test if world_state agrees with this definition of the centre of the robot
     static double ROBOT_RADIUS_MAX();
     static double FRONT_LENGTH();
-    static double DRIBBLER_ANGLE_OFFSET();;  // If the angle 0 is the centre of the robot, then -DRIBBLER_ANGLE_OFFSET() points to the left and DRIBBLER_ANGLE_OFFSET() to the right.
-    static double CENTRE_TO_FRONT();;
-    static double BALL_RADIUS();;
-    static int TICK_RATE();;
-    static double CLOSE_TO_BORDER_DISTANCE();;
-    static int GAME_ANALYSIS_TICK_RATE();;
+    static double DRIBBLER_ANGLE_OFFSET();  // If the angle 0 is the centre of the robot, then -DRIBBLER_ANGLE_OFFSET() points to the left and DRIBBLER_ANGLE_OFFSET() to the right.
+    static double CENTRE_TO_FRONT();
+    static double BALL_RADIUS();
+    static int TICK_RATE();
+    static double CLOSE_TO_BORDER_DISTANCE();
+    static int GAME_ANALYSIS_TICK_RATE();
 
     /// GENERAL SKILLS ///
-    static double DEFAULT_KICK_POWER();; // max kick power() { return  100
-    static double MAX_KICK_POWER();; //TODO: CHECK
-    static double MAX_POWER_KICK_DISTANCE();;
-    static int MAX_KICK_CYCLES();;
-    static double OUT_OF_FIELD_MARGIN();;
-    static double MAX_BALL_BOUNCE_RANGE();;
-    static double MAX_BALL_RANGE();; // Could maybe be even less? Is a LOT lower in real life, think max 0.05 m.
-    static double HAS_BALL_ANGLE();;
-    static double MAX_KICK_RANGE();;
+    static double DEFAULT_KICK_POWER(); // max kick power() { return  100
+    static double MAX_KICK_POWER(); //TODO: CHECK
+    static double MAX_POWER_KICK_DISTANCE();
+    static int MAX_KICK_CYCLES();
+    static double OUT_OF_FIELD_MARGIN();
+    static double MAX_BALL_BOUNCE_RANGE();
+    static double MAX_BALL_RANGE(); // Could maybe be even less? Is a LOT lower in real life, think max 0.05 m.
+    static double HAS_BALL_ANGLE();
+    static double MAX_KICK_RANGE();
 
-    static double MAX_INTERCEPT_TIME();;    // Seconds. Intercept terminates  after this time.
-    static double BALL_STILL_VEL();;    // If the ball has velocity lower than this in defense area, keeper starts getting it
-    static double MIN_DISTANCE_FOR_FORCE();;
-    static double GOTOPOS_ERROR_MARGIN();;
-    static double DEFAULT_BALLCOLLISION_RADIUS();;
+    static double MAX_INTERCEPT_TIME();    // Seconds. Intercept terminates  after this time.
+    static double BALL_STILL_VEL();    // If the ball has velocity lower than this in defense area, keeper starts getting it
+    static double MIN_DISTANCE_FOR_FORCE();
+    static double GOTOPOS_ERROR_MARGIN();
+    static double DEFAULT_BALLCOLLISION_RADIUS();
 
     /// KEEPER ///
-    static double KEEPER_POST_MARGIN();;//m
-    static double KEEPER_CENTREGOAL_MARGIN();;//m
+    static double KEEPER_POST_MARGIN();//m
+    static double KEEPER_CENTREGOAL_MARGIN();//m
     static double KEEPER_PENALTY_LINE_MARGIN();//m
 
     /// INTERFACE ///
-    static int ROBOT_DRAWING_SIZE();;
-    static int BALL_DRAWING_SIZE();;
-    static int TACTIC_COLOR_DRAWING_SIZE();;
-    static int WINDOW_FIELD_MARGIN();;
+    static int ROBOT_DRAWING_SIZE();
+    static int BALL_DRAWING_SIZE();
+    static int TACTIC_COLOR_DRAWING_SIZE();
+    static int WINDOW_FIELD_MARGIN();
 
-    static int KEEPER_HELP_DRAW_SIZE();;
-    static int INTERCEPT_DRAW_VECTOR_SIZE();;
+    static int KEEPER_HELP_DRAW_SIZE();
+    static int INTERCEPT_DRAW_VECTOR_SIZE();
 
-    static double BP_MOVE_BACK_DIST();;
-    static double BP_MOVE_TOWARDS_DIST();;
+    static double BP_MOVE_BACK_DIST();
+    static double BP_MOVE_TOWARDS_DIST();
 
     /// SETTINGS ///
-    static bool STD_SHOW_ROLES();;
-    static bool STD_SHOW_TACTICS();;
-    static bool STD_SHOW_TACTICS_COLORS();;
-    static bool STD_SHOW_VELOCITIES();;
-    static bool STD_SHOW_ANGLES();;
-    static bool STD_SHOW_PATHS_ALL();;
-    static bool STD_SHOW_PATHS_CURRENT();;
-    static bool STD_SHOW_BALL_PLACEMENT_MARKER();;
-    static bool STD_SHOW_DEBUG_VALUES();;
-    static bool STD_USE_REFEREE();;
-    static bool STD_TIMEOUT_TO_TOP();;
+    static bool STD_SHOW_ROLES();
+    static bool STD_SHOW_TACTICS();
+    static bool STD_SHOW_TACTICS_COLORS();
+    static bool STD_SHOW_VELOCITIES();
+    static bool STD_SHOW_ANGLES();
+    static bool STD_SHOW_PATHS_ALL();
+    static bool STD_SHOW_PATHS_CURRENT();
+    static bool STD_SHOW_BALL_PLACEMENT_MARKER();
+    static bool STD_SHOW_DEBUG_VALUES();
+    static bool STD_USE_REFEREE();
+    static bool STD_TIMEOUT_TO_TOP();
 
     static std::map<int, bool> ROBOTS_WITH_WORKING_GENEVA();
 
 
     static bool ROBOT_HAS_WORKING_GENEVA(int id);
 
-    static QColor FIELD_COLOR();;
-    static QColor FIELD_LINE_COLOR();;
-    static QColor ROBOT_COLOR_BLUE();; // Blue
-    static QColor ROBOT_COLOR_YELLOW();; // Yellow
-    static QColor BALL_COLOR();; // Orange
-    static QColor TEXT_COLOR();;
-    static QColor SELECTED_ROBOT_COLOR();;
+    static QColor FIELD_COLOR();
+    static QColor FIELD_LINE_COLOR();
+    static QColor ROBOT_COLOR_BLUE(); // Blue
+    static QColor ROBOT_COLOR_YELLOW(); // Yellow
+    static QColor BALL_COLOR(); // Orange
+    static QColor TEXT_COLOR();
+    static QColor SELECTED_ROBOT_COLOR();
 
-    static std::vector<QColor> TACTIC_COLORS();;
+    static std::vector<QColor> TACTIC_COLORS();
 
     // Default PID values for the gotoposses/interface
-    static pidVals standardNumTreePID();;
-    static pidVals standardForcePID();;
-    static pidVals standardBasicPID();;
+    static pidVals standardNumTreePID();
+    static pidVals standardForcePID();
+    static pidVals standardBasicPID();
 
     
 private:
