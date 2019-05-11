e9dfc1b5bc540d17ce0285de9ed741b985df554f
mrlukasbos <mrlukasbos@gmail.com>
2019-05-11 20:00:10 +0200

    more work on new ref structure

--- roboteam_ai/src/utilities/StrategyManager.h
+++ roboteam_ai/src/utilities/StrategyManager.h
@@ -1,7 +1,7 @@
 /*
  * Created by mrlukasbos on 9-11-18.
  *
- * Looks at the refereeData and returns the name of the desired strategy
+ * Set the refgame state according to referee commands.
  */
 
 #ifndef ROBOTEAM_AI_STRATEGYMANAGER_H
@@ -16,24 +16,20 @@
 namespace rtt {
 namespace ai {
 
-
 class StrategyManager {
 public:
     explicit StrategyManager() = default;
-    StrategyMap getCurrentStrategy(roboteam_msgs::RefereeCommand currentRefCmd);
-    std::string getCurrentKeeperTreeName(roboteam_msgs::RefereeCommand currentRefCmd);
+    const RefGameState &getCurrentRefGameState() const;
+    void setCurrentRefGameState(RefCommand command);
 
 private:
-    RefCommand prevCmd = RefCommand::HALT;
-    StrategyMap currentStrategyMap = {RefCommand::HALT, "halt_strategy"};
-    StrategyMap currentKeeperMap = {RefCommand::HALT, "keeper_halt_tactic"};
-    StrategyMap getStrategyMapForRefGameState(RefCommand commandId);
-    StrategyMap getKeeperMapForRefGameState(RefCommand commandId);
+    RefGameState currentRefGameState;
 
     // Basic rulesets for rule compliance
-    const RuleSet DEFAULT_RULESET   = {8.0, 1.5, 6.5, false, true, true};
-    const RuleSet HALT_RULESET      = {0.0, 0.0, 0.0, true, true, true};
-    const RuleSet STOP_RULESET      = {1.5, 0.0, 0.0, true, true, false};
+    const RuleSet DEFAULT_RULESET           = {8.0, 1.5, 6.5, false, true, true};
+    const RuleSet HALT_RULESET              = {0.0, 0.0, 0.0, true, true, true};
+    const RuleSet STOP_RULESET              = {1.5, 0.0, 0.0, true, true, false};
+    const RuleSet BALL_PLACEMENT_RULESET    = {1.5, 0.0, 0.0, true, true, true};
 
     std::map<RefCommand, RefGameState> gameStates = {
         {RefCommand::NORMAL_START,          RefGameState("normal_play_strategy",               "keeper_default_tactic",        DEFAULT_RULESET)},
@@ -44,8 +40,8 @@ private:
         {RefCommand::TIMEOUT_THEM,          RefGameState("halt_strategy",                      "keeper_halt_tactic",           DEFAULT_RULESET)},
         {RefCommand::GOAL_US,               RefGameState("kickoff_them_formation_strategy",    "keeper_formation_tactic",      DEFAULT_RULESET)},
         {RefCommand::GOAL_THEM,             RefGameState("kickoff_us_formation_strategy",      "keeper_formation_tactic",      DEFAULT_RULESET)},
-        {RefCommand::BALL_PLACEMENT_US,     RefGameState("ball_placement_us_strategy",         "keeper_avoid_tactic",          DEFAULT_RULESET)},
-        {RefCommand::BALL_PLACEMENT_THEM,   RefGameState("kickoff_them_formation_strategy",    "keeper_avoid_tactic",          DEFAULT_RULESET)},
+        {RefCommand::BALL_PLACEMENT_US,     RefGameState("ball_placement_us_strategy",         "keeper_avoid_tactic",          BALL_PLACEMENT_RULESET)},
+        {RefCommand::BALL_PLACEMENT_THEM,   RefGameState("kickoff_them_formation_strategy",    "keeper_avoid_tactic",          BALL_PLACEMENT_RULESET)},
         {RefCommand::PREPARE_KICKOFF_US,    RefGameState("kickoff_us_formation_strategy",      "keeper_formation_tactic",      DEFAULT_RULESET)},
         {RefCommand::PREPARE_KICKOFF_THEM,  RefGameState("kickoff_them_formation_strategy",    "keeper_default_tactic",        DEFAULT_RULESET)},
         {RefCommand::PREPARE_PENALTY_US,    RefGameState("penalty_us_prepare_strategy",        "keeper_formation_tactic",      DEFAULT_RULESET)},
@@ -59,7 +55,6 @@ private:
         {RefCommand::DEFEND_PENALTY,        RefGameState("penalty_them_strategy",              "keeper_penalty_tactic",        DEFAULT_RULESET)},
         {RefCommand::DO_PENALTY,            RefGameState("penalty_us_shoot_strategy",           "keeper_default_tactic",       DEFAULT_RULESET)}
     };
-
 };
 
 } // ai
