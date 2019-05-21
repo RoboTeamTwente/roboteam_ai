eb4cba9c91b1997267c7302f38032d56bb4e9694
ThijsLuttikhuis <t.luttikhuis@student.utwente.nl>
2019-05-21 14:58:44 +0200

    fix hasclearshot

--- roboteam_ai/src/conditions/HasClearShot.cpp
+++ roboteam_ai/src/conditions/HasClearShot.cpp
@@ -31,12 +31,10 @@ HasClearShot::Status HasClearShot::onUpdate() {
 
     // return success if there is a clear line to their goal 
     auto world = world::world->getWorld();
-    int id=robot->id;
-    world.us.erase(std::remove_if(world.us.begin(),world.us.end(), [id](Robot bot){return bot.id==id;}));
-    if (world::field->getPercentageOfGoalVisibleFromPoint(false, ball->pos,world) > minViewAtGoal * 100) {
-        return Status::Success;
-    }
-    return Status::Failure;
+    bool hasClearShot = world::field->getPercentageOfGoalVisibleFromPoint(false, ball->pos, world, robot->id, true) >
+            minViewAtGoal * 100;
+
+    return hasClearShot ? Status::Success : Status::Failure;
 }
 
 } // ai
