f05b46221189f8bb45a8f37b8a221ae86784989c
rolfvdhulst <rolfvdhulst@gmail.com>
2019-05-22 16:55:28 +0200

    tests!

--- roboteam_ai/test/CoachTests/DefensiveCoachTest.cpp
+++ roboteam_ai/test/CoachTests/DefensiveCoachTest.cpp
@@ -2,22 +2,56 @@
 //
 
 #include <gtest/gtest.h>
-#include "roboteam_ai/src/coach/defence/DefencePositionCoach.h"
+#include <roboteam_ai/test/helpers/FieldHelper.h>
+#include <roboteam_ai/test/helpers/WorldHelper.h>
 #include "roboteam_ai/src/utilities/Constants.h"
+#include "roboteam_ai/src/coach/defence/DefencePositionCoach.h"
 namespace cc=rtt::ai::coach;
-namespace w=rtt::ai;
-
-TEST(DefensiveCoachTest,it_calculates_blockPoint) {
-    rtt::Vector2 startPos(0, 3);
-    rtt::Vector2 endPos(3, 0);
-    rtt::Vector2 pointPos(0, 0);
-    auto segment = std::make_pair(startPos, endPos);
-    double colllisionRadius=w::Constants::ROBOT_RADIUS()+w::Constants::BALL_RADIUS();
-    auto blockPoint= cc::g_defensivePositionCoach.getBlockPoint(segment, pointPos,colllisionRadius);
-    ASSERT_FLOAT_EQ(blockPoint.angle(), M_PI_4);
-    ASSERT_FLOAT_EQ(blockPoint.x, colllisionRadius);
-    ASSERT_FLOAT_EQ(blockPoint.y, colllisionRadius);
-    rtt::Vector2 middlePoint(3,3),checkPoint(3,1.5);
-    auto blockPoint2 = cc::g_defensivePositionCoach.getBlockPoint(std::make_pair(middlePoint,endPos), pointPos,colllisionRadius);
-    ASSERT_FLOAT_EQ(blockPoint2.angle(),checkPoint.angle());
+namespace w=rtt::ai::world;
+namespace a= rtt::ai;
+namespace rtt{
+namespace ai{
+namespace coach{
+w::Robot createRobot(const Vector2& pos,int id, bool ourTeam){
+    w::Robot robot;
+    robot.team=ourTeam? w::Robot::Team::us : w::Robot::Team::them;
+    robot.pos = pos;
+    robot.id=id;
+    return robot;
 }
+
+TEST(defensive_coach_test,blockPoints){
+    roboteam_msgs::GeometryFieldSize field = testhelpers::FieldHelper::generateField();
+    w::field->set_field(field);
+    w::world->updateWorld(testhelpers::WorldHelper::getWorldMsg(0,8,false,field));
+    for (int j = 0; j < 10000; ++ j) {
+        w::world->updateWorld(testhelpers::WorldHelper::getWorldMsg(0,8,false,field));
+        auto testWorld=w::world->getWorld();
+        for (const auto& robot : w::world->getThem()){
+            std::pair<Vector2,Vector2> goal=w::field->getGoalSides(true);
+            Vector2 toBlockFrom=w::world->getRobotForId(robot.id,false)->pos;
+            auto lineSegment=cc::g_defensivePositionCoach.getBlockLineSegment(goal,toBlockFrom);
+            auto blockPos=cc::g_defensivePositionCoach.getBlockPoint(goal,toBlockFrom,a::Constants::ROBOT_RADIUS() + a::Constants::BALL_RADIUS());
+            testWorld.us.push_back(createRobot(blockPos,robot.id,true));
+            auto parts=w::field->getVisiblePartsOfGoal(true,toBlockFrom,testWorld);
+            EXPECT_EQ(parts.size(),0);
+
+            if (lineSegment){
+                if (w::field->pointIsInDefenceArea(blockPos,true,0.15)){
+                    EXPECT_NE(blockPos,lineSegment->second);
+                }
+                else{
+                    EXPECT_EQ(blockPos,lineSegment->second);
+                }
+                EXPECT_FALSE(w::field->pointIsInDefenceArea(lineSegment->first,true,0.15));
+                EXPECT_FALSE(w::field->pointIsInDefenceArea(lineSegment->second,true,0.15));
+            }
+
+            testWorld.us.clear();
+        }
+    }
+}
+}
+}
+}
+
