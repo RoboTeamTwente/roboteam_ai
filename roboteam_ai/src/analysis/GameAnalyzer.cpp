79de70b1f078d5c66f3ad86bd3bace61599e8dc6
mrlukasbos <mrlukasbos@gmail.com>
2019-05-27 17:56:41 +0200

    change some includes for faster compilation (i hope)

--- roboteam_ai/src/analysis/GameAnalyzer.cpp
+++ roboteam_ai/src/analysis/GameAnalyzer.cpp
@@ -124,11 +124,12 @@ std::shared_ptr<AnalysisReport> GameAnalyzer::getMostRecentReport() {
 /// Check with distanceToLineWithEnds if there are obstructions
 /// Returns all robots that can be passed to, along with the distance
 /// we return robot ids instead of robot object because the objects are incorrect (because of simulated world)
-vector<pair<int, double>> GameAnalyzer::getRobotsToPassTo(RobotPtr robot, bool ourTeam, WorldData simulatedWorld) {
+std::vector<std::pair<int, double>> GameAnalyzer::getRobotsToPassTo(RobotPtr robot, bool ourTeam, WorldData simulatedWorld) {
     auto ourRobots = ourTeam ? simulatedWorld.us : simulatedWorld.them;
     auto enemyRobots = ourTeam ? simulatedWorld.them : simulatedWorld.us;
 
-    vector<pair<int, double>> robotsToPassTo;
+
+    std::vector<std::pair<int, double>> robotsToPassTo;
     for (auto ourRobot : ourRobots) {
         bool canPassToThisRobot = true;
         for (auto theirRobot : enemyRobots) {
@@ -141,7 +142,7 @@ vector<pair<int, double>> GameAnalyzer::getRobotsToPassTo(RobotPtr robot, bool o
         }
         if (canPassToThisRobot) {
             double distToRobot = (Vector2(ourRobot->pos) - Vector2(robot->pos)).length();
-            robotsToPassTo.emplace_back(make_pair(ourRobot->id, distToRobot));
+            robotsToPassTo.emplace_back(std::make_pair(ourRobot->id, distToRobot));
         }
     }
     return robotsToPassTo;
