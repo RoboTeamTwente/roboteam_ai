e62b633087b820a550399d93946f61238cd21ae2
robzelluf <robzelluf@hotmail.com>
2019-05-29 15:20:30 +0200

    Fix comment

--- roboteam_ai/src/skills/MidFieldHarasser.cpp
+++ roboteam_ai/src/skills/MidFieldHarasser.cpp
@@ -52,10 +52,8 @@ void MidFieldHarasser::onTerminate(Skill::Status s) {
 
 Vector2 MidFieldHarasser::getHarassTarget() {
     auto harassTarget = coach::g_midFieldCoach.getTargetPosition(robot);
-    std::cout << "Robot" << robot->id << " harassing robot " << harassTarget.targetRobot << std::endl;
 
     if (!world::field->pointIsInField(harassTarget.targetPosition, 0.20)) {
-        std::cerr << "Robot " << robot->id << " harass target " << harassTarget.targetPosition << " is not in field" << std::endl;
         harassTarget.targetPosition = robot->pos;
     }
 
