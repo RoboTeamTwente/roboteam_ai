0e99a3ff1bc7c6ec1ee61cb69a5768fda5bfaf8d
mrlukasbos <mrlukasbos@gmail.com>
2019-06-07 12:18:55 +0200

    remove NaN logs

--- roboteam_ai/src/skills/Skill.cpp
+++ roboteam_ai/src/skills/Skill.cpp
@@ -22,14 +22,8 @@ void Skill::publishRobotCommand() {
     if(Constants::GRSIM() && ourSideParam=="right"){
       command=rotateRobotCommand(command);
     }
-    if (std::isnan(command.x_vel) || std::isnan(command.y_vel)) {
-        std::cout << "ERROR: x or y vel in command is NAN in Skill " << node_name().c_str() << "!" << std::endl;
-    }
-    limitRobotCommand();
 
-    if (std::isnan(command.x_vel) || std::isnan(command.y_vel)) {
-        std::cout << "ERROR: x or y vel in command is NAN in Skill " << node_name().c_str() << "!" << std::endl;
-    }
+    limitRobotCommand();
 
     // Make sure both kicker and chipper vel are set, so that it works for both GrSim and Serial
     if(command.kicker_vel > command.chipper_vel) {
