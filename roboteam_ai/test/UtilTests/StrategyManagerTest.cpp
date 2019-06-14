d57f88c9c3aa660e9f0aa40db9e371ad4ec92099
rolfvdhulst <rolfvdhulst@gmail.com>
2019-06-14 16:59:36 +0200

    Merge branch 'development' into feature/shootOutGameState
    # Conflicts:
    #	roboteam_ai/src/utilities/StrategyManager.h

--- roboteam_ai/test/UtilTests/StrategyManagerTest.cpp
+++ roboteam_ai/test/UtilTests/StrategyManagerTest.cpp
@@ -38,6 +38,23 @@ TEST(StrategyManagerTest, StrategyManagerTest) {
     strategyManager.setCurrentRefGameState(RefCommand::HALT);
     EXPECT_EQ(strategyManager.getCurrentRefGameState().strategyName, "halt_strategy");
 
+    strategyManager.setCurrentRefGameState(RefCommand::PREPARE_SHOOTOUT_US);
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().strategyName,"time_out_strategy");
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().keeperStrategyName,"shootout_prepare_tactic");
+
+    strategyManager.setCurrentRefGameState(RefCommand::NORMAL_START);
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().strategyName,"time_out_strategy");
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().keeperStrategyName,"shootout_shoot_tactic");
+
+    strategyManager.setCurrentRefGameState(RefCommand::PREPARE_SHOOTOUT_THEM);
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().strategyName,"time_out_strategy");
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().keeperStrategyName,"keeper_penalty_prepare_tactic");
+
+    strategyManager.setCurrentRefGameState(RefCommand::NORMAL_START);
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().strategyName,"time_out_strategy");
+    EXPECT_EQ(strategyManager.getCurrentRefGameState().keeperStrategyName,"keeper_default_tactic");
+
+
 
 
 }
