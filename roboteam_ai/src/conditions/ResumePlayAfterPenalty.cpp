eb11cb9ef126592cd4276ae66a95c83a724913e1
rolfvdhulst <rolfvdhulst@gmail.com>
2019-06-17 09:42:27 +0200

    remove shotAtGoal condition for resuming normal Play

--- roboteam_ai/src/conditions/ResumePlayAfterPenalty.cpp
+++ roboteam_ai/src/conditions/ResumePlayAfterPenalty.cpp
@@ -16,23 +16,10 @@ ResumePlayAfterPenalty::ResumePlayAfterPenalty(std::string name, bt::Blackboard:
 bt::Node::Status ResumePlayAfterPenalty::onUpdate() {
     bool ballHasMoved= GameStateManager::getCurrentGameState().ballPositionAtStartOfGameState.dist(ball->pos) > 0.05;
     bool ballIsLayingStill = (Vector2(ball->vel)).length() < Constants::BALL_STILL_VEL();
-
-
-    Vector2 goalCentre = world::field->get_our_goal_center();
-    double goalWidth = world::field->get_field().goal_width;
-    double margin = 0.1;
-    Vector2 lowerPost = goalCentre + Vector2(0.0, - (goalWidth/2 + margin));
-    Vector2 upperPost = goalCentre + Vector2(0.0, goalWidth/2 + margin);
-
-    // determine the ball position and predicted ball position
-    Vector2 ballPos = ball->pos;
-    Vector2 ballPredPos = Vector2(ballPos) + Vector2(ball->vel)*Constants::MAX_INTERCEPT_TIME();
-
     // Check if the extension of the velocity vector goes through the goal.
     // The line drawn for the ball is the predicted position in 1.5 seconds
-    bool shotAtGoal=control::ControlUtils::lineSegmentsIntersect(lowerPost, upperPost, ballPos, ballPredPos);
 
-    if (!ballShot&&!ballIsLayingStill&&ballHasMoved&&shotAtGoal) {
+    if (!ballShot&&!ballIsLayingStill&&ballHasMoved) {
         ballShot=true;
     }
     if (ballShot){
