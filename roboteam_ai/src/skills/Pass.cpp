73f62ff14bae1bb975a69f0caccfdda31b994204
Barış İmre <b.imre@student.utwente.nl>
2019-07-02 08:31:51 +0200

    Feature/pass free kick (#634)
    * Build FreeKickPass
    
    * Make more free kick
    
    * Improve FreeKickPass

--- roboteam_ai/src/skills/Pass.cpp
+++ roboteam_ai/src/skills/Pass.cpp
@@ -21,18 +21,20 @@ void Pass::onInitialize() {
     robotToPassToID = - 1;
     passInitialized = false;
     hasShot = false;
-    chip = false;
+    forcePass = false;
     fails = 0;
     if (properties->hasInt("failsUntilChip")) {
-        failsUntilChip = properties->getInt("failsUntilChip");
+        maxTries = properties->getInt("failsUntilChip");
     }
     else {
-        failsUntilChip = - 1;
+        maxTries = - 1;
     }
 }
 
 Pass::Status Pass::onUpdate() {
     bool closeToBall = (robot->pos - ball->pos).length() < CLOSE_ENOUGH_TO_BALL;
+
+    // Only do this if not close to the ball and the pass is not yet initialized
     if (! closeToBall && ! passInitialized) {
         RobotCommand robotCommand;
 
@@ -80,15 +82,15 @@ Pass::Status Pass::onUpdate() {
         // Not having already tried a shot
         // If this is both not the case, check if there's a clear line to the target
         // If not, either ++ fails or fail immediately
-        if (! chip && ! hasShot
+        if (! forcePass && ! hasShot
                 && ! control::ControlUtils::clearLine(ball->pos, robotToPassTo->pos, world::world->getWorld(), 1)) {
-            if (failsUntilChip == - 1) {
+            if (maxTries == - 1) {
                 return Status::Failure;
             }
             else {
                 fails ++;
-                if (fails >= failsUntilChip) {
-                    chip = true;
+                if (fails >= maxTries) {
+                    forcePass = true;
                 }
                 else {
                     coach::g_pass.resetPass(robot->id);
@@ -97,9 +99,8 @@ Pass::Status Pass::onUpdate() {
             }
         }
 
-        auto shotdata = robot->getShotController()->getRobotCommand(*robot, getKicker(), chip, control::BallSpeed::PASS,
-                false, control::ShotPrecision::HIGH);
-        command = shotdata.makeROSCommand();
+        makeCommand();
+
         if ((command.kicker == true || command.chipper == true) && ! hasShot) {
             hasShot = true;
         }
@@ -107,6 +108,12 @@ Pass::Status Pass::onUpdate() {
 
     publishRobotCommand();
     return Status::Running;
+}
+
+void Pass::makeCommand() {
+    auto shotdata = robot->getShotController()->getRobotCommand(*robot, getKicker(), forcePass, control::PASS,
+                                                                true, control::HIGH);
+    command = shotdata.makeROSCommand();
 }
 
 void Pass::onTerminate(Status s) {
