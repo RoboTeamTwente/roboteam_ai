16f3b387d3a0a65e30cf74a8401cca638d82ca2f
barisimre <b.imre@student.utwente.nl>
2019-07-04 08:06:41 +0200

    add stop formation

--- roboteam_ai/src/skills/formations/StopFormation.cpp
+++ roboteam_ai/src/skills/formations/StopFormation.cpp
@@ -16,23 +16,79 @@ StopFormation::StopFormation(std::string name, bt::Blackboard::Ptr blackboard)
 Vector2 StopFormation::getFormationPosition() {
     auto field = world::field->get_field();
 
-    // determine if we should be in the top or bottom of the field
-    bool topSideOfField = rtt::ai::interface::Output::isTimeOutAtTop();
-    int inv = topSideOfField ? 1 : -1;
-    double targetLocationY = field.field_width/2 * inv;
+    auto pp = world::field->getPenaltyPoint(true); // penalty point
 
-    // first we calculate all the positions for the defense
-    std::vector<Vector2> targetLocations;
-    std::vector<int> robotIds;
+    auto defenseAreaLineA = world::field->get_field().left_penalty_line.begin;
+    auto defenseAreaLineB = world::field->get_field().left_penalty_line.end;
 
-    for (unsigned int i = 0; i<robotsInFormation->size(); i++) {
-        double targetLocationX = - field.field_length/4 * 2*i*Constants::ROBOT_RADIUS_MAX();
-        targetLocations.emplace_back(targetLocationX, targetLocationY);
-        robotIds.push_back(robotsInFormation->at(i)->id);
+    // divide the upper and bottom lines of the defense area and store those values.
+    auto dTopY = fmax(defenseAreaLineA.y, defenseAreaLineB.y);
+    auto dBtmY = fmin(defenseAreaLineA.y, defenseAreaLineB.y);
+    auto defAreaHeight = fabs(dTopY - dBtmY);
+    double offset = 0.5;
+
+    //failsafe to prevent segfaults
+    int amountOfRobots = robotsInFormation->size();
+    if (amountOfRobots <= 0) {
+        return {};
+    } else if (amountOfRobots == 1) {
+        return {pp.x + offset, pp.y};
     }
 
+    float factor = defAreaHeight/robotsInFormation->size()-1;
+
+    // lets get us .5m from our defense area for now
+
+    double fh = field.field_width;
+    double fw = field.field_length;
+
+    std::vector<std::vector<Vector2>> targetLocations = {
+            // middle
+            {{pp.x + offset, pp.y}},
+
+            // two def area corners
+            {{pp.x + offset, dTopY}, {pp.x + offset, dBtmY}},
+
+            // middle + def area corners
+            {{pp.x + offset, pp.y}, {pp.x + offset, dTopY}, {pp.x + offset, dBtmY}},
+
+            // 4 points in front of def area        (noted from top to bottom)
+            {{pp.x + offset, dTopY}, {pp.x + offset, dTopY-factor}, {pp.x + offset,  dBtmY + factor}, {pp.x + offset, dBtmY}},
+
+             // 5 points in front of def area (top to bottom
+            {{pp.x + offset, dTopY}, {pp.x + offset, dTopY-factor}, {pp.x + offset, pp.y}, {pp.x + offset,  dBtmY + factor}, {pp.x + offset, dBtmY}},
+
+            // 6 points in front of def area (top to bottom)
+            {{pp.x, dTopY+(factor*2.0)},
+             {pp.x + offset, dTopY},
+             {pp.x + offset, dTopY-(factor*2.0)},
+             {pp.x + offset, dBtmY + (factor*2.0)},
+             {pp.x + offset, dBtmY},
+             {pp.x, dBtmY - (factor*2.0)}},
+
+            // 7 points in front of def area
+            {{pp.x + offset, dTopY+(defAreaHeight/3)},
+             {pp.x + offset, dTopY},
+             {pp.x + offset, pp.y},
+             {pp.x + offset, dBtmY},
+             {pp.x + offset, dBtmY - (defAreaHeight/3)},
+             {pp.x, dTopY+ (defAreaHeight/3)},
+             {pp.x, dBtmY - (defAreaHeight/3)}},
+
+            // 8 points in front of def area
+            {{pp.x + offset, dTopY+(defAreaHeight/3)},
+             {pp.x + offset, dTopY},
+             {pp.x + offset, dTopY-(defAreaHeight/3)},
+             {pp.x + offset, dBtmY + (defAreaHeight/3)},
+             {pp.x + offset, dBtmY},
+             {pp.x + offset, dBtmY - (defAreaHeight/3)},
+             {pp.x, dTopY+ (defAreaHeight/3)},
+             {pp.x, dBtmY - (defAreaHeight/3)}}
+    };
+
+    std::vector<int> robotIds;
     rtt::HungarianAlgorithm hungarian;
-    auto shortestDistances = hungarian.getRobotPositions(robotIds, true, targetLocations);
+    auto shortestDistances = hungarian.getRobotPositions(robotIds, true, targetLocations.at(amountOfRobots));
     return shortestDistances.at(robot->id);
 }
 
