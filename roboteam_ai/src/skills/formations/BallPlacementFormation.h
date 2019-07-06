8f30b67293591eaa7944b19b39ff05b3845ca392
ThijsLuttikhuis <t.luttikhuis@student.utwente.nl>
2019-07-06 17:32:45 +0200

    make the implementation much cleaner

--- roboteam_ai/src/skills/formations/BallPlacementFormation.h
+++ roboteam_ai/src/skills/formations/BallPlacementFormation.h
@@ -1,21 +1,19 @@
 //
 // Created by thijs on 6-7-19.
 //
-#include <roboteam_ai/src/skills/formations/Formation.h>
+#include <roboteam_ai/src/skills/formations/StopFormation.h>
 
 #ifndef ROBOTEAM_AI_BALLPLACEMENTFORMATION_H
 #define ROBOTEAM_AI_BALLPLACEMENTFORMATION_H
 
 namespace rtt {
 namespace ai {
-class BallPlacementFormation : public Formation {
+class BallPlacementFormation : public StopFormation {
     public:
         explicit BallPlacementFormation(std::string name, bt::Blackboard::Ptr blackboard = nullptr);
     private:
-        Vector2 getFormationPosition() override;
         void updateFormation() override;
-        void setFinalAngle() override;
-        std::vector<std::vector<Vector2>> getStopPositions();
+        Vector2 getFormationPosition() override;
         std::shared_ptr<std::vector<std::shared_ptr<Robot>>> robotsInFormationPtr() override;
         static std::shared_ptr<std::vector<std::shared_ptr<Robot>>> robotsInFormation;
 };
--- roboteam_ai/src/skills/formations/StopFormation.h
+++ roboteam_ai/src/skills/formations/StopFormation.h
@@ -14,11 +14,12 @@ namespace ai {
 class StopFormation : public Formation {
     public:
         explicit StopFormation(std::string name, bt::Blackboard::Ptr blackboard = nullptr);
-    private:
+    protected:
         Vector2 getFormationPosition() override;
         void updateFormation() override;
         void setFinalAngle() override;
         std::vector<std::vector<Vector2>> getStopPositions();
+    private:
         std::shared_ptr<std::vector<std::shared_ptr<Robot>>> robotsInFormationPtr() override;
         static std::shared_ptr<std::vector<std::shared_ptr<Robot>>> robotsInFormation;
 };
