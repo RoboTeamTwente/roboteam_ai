d0cc003affb921a3073c99854f5200454ab9dc75
RobotJesse <j.t.vanwerven@roboteamtwente.nl>
2019-11-29 13:12:05 +0100

    added some nice constructors for selector and sequence, provided documentation for node update fnction

--- include/roboteam_ai/bt/composites/Selector.hpp
+++ include/roboteam_ai/bt/composites/Selector.hpp
@@ -6,6 +6,7 @@ namespace bt {
 
 class Selector : public Composite {
 public:
+    Selector(std::vector<std::shared_ptr<bt::Node>> childs);
     Status update() override;
     std::string node_name() override { return "Selector"; };
 };
--- include/roboteam_ai/bt/composites/Sequence.hpp
+++ include/roboteam_ai/bt/composites/Sequence.hpp
@@ -6,6 +6,8 @@ namespace bt {
 
 class Sequence : public Composite {
 public:
+    Sequence(std::vector<std::shared_ptr<bt::Node>> children);
+
     Status update() override;
     std::string node_name() override { return "Sequence"; };
 };
