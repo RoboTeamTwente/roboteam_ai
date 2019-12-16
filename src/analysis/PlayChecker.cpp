db73dddf24cdd15e09f00dc89ea328a09a3f45f3
RobotJesse <j.t.vanwerven@roboteamtwente.nl>
2019-12-16 16:30:00 +0100

    Revert "plz work 2.0"
    This reverts commit 69d15f50a99f95723d13b5321f4abda5dcb03b18.

--- src/analysis/PlayChecker.cpp
+++ src/analysis/PlayChecker.cpp
@@ -3,39 +3,39 @@
 //
 
 #include <include/roboteam_ai/analysis/PlaysObjects/Invariants/AlwaysTrueInvariant.h>
-#include <include/roboteam_ai/analysis/PlaysObjects/Invariants/BallOnOurSideInvariant.h>
-#include <include/roboteam_ai/analysis/PlaysObjects/Invariants/BallBelongsToUsInvariant.h>
 #include "analysis/PlayChecker.h"
+#include "analysis/PlaysObjects/Invariants/BallBelongsToUsInvariant.h"
 #include "analysis/PlaysObjects/Invariants/AlwaysFalseInvariant.h"
+#include "analysis/PlaysObjects/Invariants/BallOnOurSideInvariant.h"
+#include "analysis/PlaysObjects/Play.h"
 #include "functional"
 
 
 namespace rtt::ai::analysis {
-    /**
-     * Typedef to facilitate creation of functors for invariant evaluation
-     */
-    using fun = std::function<bool(world::World *, world::Field *)>;
 
-    std::vector<fun> x = {
-            [](auto world, auto field) { return AlwaysTrueInvariant().isTrue(world, field); },
-    };
+    using fun = std::function<bool(world::World*, world::Field*)>;
 
 
-    enum invariantEnum {
-        alwaysTrueIndex,
-        alwaysFalseIndex,
-    };
-
-//    x[alwaysTrueIndex]
-
-
+    std::function<bool(world::World*, world::Field*)> isAlwaysTrue;
+    std::function<bool(world::World*, world::Field*)> isAlwaysFalse;
     void PlayChecker::constructInvariants() {
+        AlwaysTrueInvariant inv = AlwaysTrueInvariant();
 
-        isAlwaysTrue = [&](auto world, auto field) { return AlwaysTrueInvariant().isTrue(world, field); };
-        isAlwaysFalse = [&](auto world, auto field) { return AlwaysFalseInvariant().isTrue(world, field); };
-        ballOnOurSide = [&](auto world, auto field) { return BallOnOurSideInvariant().isTrue(world, field); };
-        ballBelongsToUs = [&](auto world, auto field) { return BallBelongsToUsInvariant().isTrue(world, field); };
+        /**
+         * Typedef to facilitate creation of functors for invariant evaluation
+         */
+//
+//        functo f_bla = [&](auto invar, auto world, auto field) {return invar.isTrue(world, field);};
+//        fun f_blub = [&](auto invar, auto world, auto field) {return invar.isTrue(world, field);};
+//
+        isAlwaysTrue = [&](auto world, auto field) {return AlwaysTrueInvariant().isTrue(world, field);};
+        isAlwaysFalse = [&](auto world, auto field) {return AlwaysFalseInvariant().isTrue(world, field);};
+
+
     }
+
+
+
 
 
     PlayChecker::PlayChecker() {
@@ -58,39 +58,34 @@ namespace rtt::ai::analysis {
      * @param field
      * @return true if invariants are true, false otherwise
      */
-    bool PlayChecker::checkCurrentGameInvariants(rtt::ai::world::World *world, rtt::ai::world::Field *field) {
+    bool PlayChecker::checkCurrentGameInvariants(rtt::ai::world::World* world, rtt::ai::world::Field* field) {
         std::cout << "checking if the play is still valid" << std::endl;
         std::cout << "Invariants name:";
         return true;
     }
 
 
+
     /**
      * Determines what plays are viable given the current world, ref states and invariants/preconditions, and stores them in the validPlays vector
      * TODO: add lambda here, to make it faster and cleaner
      */
-    void PlayChecker::determineNewPlays(rtt::ai::world::World *world, rtt::ai::world::Field *field) {
-        Play p{};
-
+    void PlayChecker::determineNewPlays(rtt::ai::world::World* world, rtt::ai::world::Field* field) {
         std::vector<fun> functionvector = {isAlwaysFalse, isAlwaysTrue};
-        p.setInvariants(functionvector);
-
         bool result = false;
-        for (auto const &play : allPlays) {
-            play.isValidPlay(world, field);
+        for (auto f : functionvector) {
+            result = f(world, field);
             std::cout << result << std::endl;
         }
     }
-
     /**
      * first check if the current play is still ok
      * if not, loop through every possible play and check for each play if all of its invariants hold
      * Then add these plays to the list of allowed plays
      */
-    void PlayChecker::update(rtt::ai::world::World *world, rtt::ai::world::Field *field) {
-        if (!checkCurrentGameInvariants(world, field)) {
-            determineNewPlays(world, field);
-        }
+    void PlayChecker::update(rtt::ai::world::World* world, rtt::ai::world::Field* field) {
+        checkCurrentGameInvariants(world, field);
+        determineNewPlays(world, field);
 
     }
 }
