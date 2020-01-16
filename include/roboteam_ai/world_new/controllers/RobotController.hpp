f5aea8602ebb7e533b712e5f07516de3bb5350bc
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-01-16 12:45:44 +0100

    resolved compiler warning and added header + sourcefile for controllers

---
+++ include/roboteam_ai/world_new/controllers/RobotController.hpp
@@ -0,0 +1,16 @@
+//
+// Created by john on 1/16/20.
+//
+
+#ifndef RTT_ROBOTCONTROLLER_HPP
+#define RTT_ROBOTCONTROLLER_HPP
+
+namespace rtt::world_new::controller {
+
+    class RobotController {
+
+    };
+
+}
+
+#endif //RTT_ROBOTCONTROLLER_HPP
---
+++ src/world_new/controllers/RobotController.cpp
@@ -0,0 +1,9 @@
+//
+// Created by john on 1/16/20.
+//
+
+#include "include/roboteam_ai/world_new/controllers/RobotController.hpp"
+
+namespace rtt::world_new::controller {
+
+}
