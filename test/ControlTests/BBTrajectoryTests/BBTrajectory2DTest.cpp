e05b8bb6ee8edba3b8a1dc5d93f25c39c7312e67
rolfvdhulst <rolfvdhulst@gmail.com>
2020-01-17 23:39:37 +0100

    update test

--- test/ControlTests/BBTrajectoryTests/BBTrajectory2DTest.cpp
+++ test/ControlTests/BBTrajectoryTests/BBTrajectory2DTest.cpp
@@ -8,25 +8,24 @@
 #include <roboteam_utils/Timer.h>
 using namespace rtt;
 TEST(BBTrajectories, BBTrajectory2D) {
-    {
-        BBTrajectory2D<float> trajectory;
-        proto::GeometryFieldSize field;
-        field.set_field_width(9.0);
-        field.set_field_length(12.0);
-        std::chrono::nanoseconds totalTime = std::chrono::nanoseconds(0);
-        for (int j = 0; j < 6000; ++ j) {
-            Vector2 startPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
-            Vector2 initialVel = testhelpers::WorldHelper::getRandomVelocity();
-            Vector2 endPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
-            float maxVel = testhelpers::WorldHelper::getRandomValue(0, 8.0);
-            float maxAcc = testhelpers::WorldHelper::getRandomValue(0, 4.0);
-            std::chrono::nanoseconds start = std::chrono::system_clock::now().time_since_epoch();
-            trajectory.generateSyncedTrajectory(startPos, initialVel, endPos, maxVel, maxAcc);
-            std::chrono::nanoseconds end = std::chrono::system_clock::now().time_since_epoch();
-            totalTime += (end - start);
-        }
-        std::cout << totalTime.count()/6000.0 << std::endl;
+
+    BBTrajectory2D<float> trajectory;
+    proto::GeometryFieldSize field;
+    field.set_field_width(9.0);
+    field.set_field_length(12.0);
+    std::chrono::nanoseconds totalTime = std::chrono::nanoseconds(0);
+    for (int j = 0; j < 6000; ++ j) {
+        Vector2 startPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
+        Vector2 initialVel = testhelpers::WorldHelper::getRandomVelocity();
+        Vector2 endPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
+        float maxVel = testhelpers::WorldHelper::getRandomValue(0, 8.0);
+        float maxAcc = testhelpers::WorldHelper::getRandomValue(0, 4.0);
+        std::chrono::nanoseconds start = std::chrono::system_clock::now().time_since_epoch();
+        trajectory.generateSyncedTrajectory(startPos, initialVel, endPos, maxVel, maxAcc);
+        std::chrono::nanoseconds end = std::chrono::system_clock::now().time_since_epoch();
+        totalTime += (end - start);
     }
+    std::cout << totalTime.count()/6000.0 << std::endl;
 }
 TEST(BBTrajectories, BBTrajectory2D2) {
 
@@ -34,18 +33,18 @@ TEST(BBTrajectories, BBTrajectory2D2) {
     proto::GeometryFieldSize field;
     field.set_field_width(9.0);
     field.set_field_length(12.0);
-    std::chrono::nanoseconds totalTime=std::chrono::nanoseconds(0);
+    std::chrono::nanoseconds totalTime = std::chrono::nanoseconds(0);
     for (int j = 0; j < 6000; ++ j) {
         Vector2 startPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
         Vector2 initialVel = testhelpers::WorldHelper::getRandomVelocity();
         Vector2 endPos = testhelpers::WorldHelper::getRandomFieldPosition(field);
         float maxVel = testhelpers::WorldHelper::getRandomValue(0, 8.0);
         float maxAcc = testhelpers::WorldHelper::getRandomValue(0, 4.0);
-        float alpha = testhelpers::WorldHelper::getRandomValue(0,M_PI_2);
-        std::chrono::nanoseconds start=std::chrono::system_clock::now().time_since_epoch();
-        trajectory.generateTrajectory(startPos, initialVel, endPos, maxVel, maxAcc,alpha);
-        std::chrono::nanoseconds end=std::chrono::system_clock::now().time_since_epoch();
+        float alpha = testhelpers::WorldHelper::getRandomValue(0, M_PI_2);
+        std::chrono::nanoseconds start = std::chrono::system_clock::now().time_since_epoch();
+        trajectory.generateTrajectory(startPos, initialVel, endPos, maxVel, maxAcc, alpha);
+        std::chrono::nanoseconds end = std::chrono::system_clock::now().time_since_epoch();
         totalTime+=(end-start);
     }
-    std::cout<<totalTime.count()/6000.0<<std::endl;
+    std::cout << totalTime.count()/6000.0 << std::endl;
 }
