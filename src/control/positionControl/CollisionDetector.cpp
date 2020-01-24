810f1024b6935dbe055e179adde3c817614f3144
Ratoone <vacariu.paul@gmail.com>
2020-01-24 11:58:42 +0100

    Checking for null field in CollisionDetector

--- src/control/positionControl/CollisionDetector.cpp
+++ src/control/positionControl/CollisionDetector.cpp
@@ -9,9 +9,9 @@ namespace rtt::ai::control{
 CollisionDetector::CollisionDetector(const std::vector<rtt::world_new::robot::Robot> &robots): robots(robots){}
 
 bool CollisionDetector::canFollowPoint(const Vector2& initialPoint, const Vector2& nextPoint){
-    return isRobotCollisionBetweenPoints(initialPoint, nextPoint) ||
-            isPointInsideField(nextPoint) ||
-            !isPointInDefenseArea(nextPoint);
+    bool isFieldSafeFromColliding = field ? isPointInsideField(nextPoint) || !isPointInDefenseArea(nextPoint) : true;
+
+    return isRobotCollisionBetweenPoints(initialPoint, nextPoint) || isFieldSafeFromColliding;
 }
 
 bool CollisionDetector::isPointInsideField(const Vector2 &point){
