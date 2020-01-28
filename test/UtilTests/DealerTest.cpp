c6291c7d0637286d9ea1110418f19e93cf057fe8
mrlukasbos <mrlukasbos@gmail.com>
2020-01-28 16:51:56 +0100

    fix warnings and setup gmock for linux

--- test/UtilTests/DealerTest.cpp
+++ test/UtilTests/DealerTest.cpp
@@ -18,14 +18,27 @@ class MockDealer : public rtt::ai::Dealer {
 };
 
 TEST(DealerTest, it_properly_distributes_robots) {
-    // create a dealer whose 'getDefaultFlagScores' always returns 1;
+
+    // create a dealer whose 'getDefaultFlagScores' method always returns 1;
     MockDealer dealer;
     ON_CALL(dealer, getDefaultFlagScores(_,_,_)).WillByDefault(Return(1));
 
+
+}
+
+/*
+ * Check if the values of the priorities make sense.
+ * High priority should give the highest value, Medium the medium etc.
+ */
+TEST(DealerTest, the_score_factor_increases_with_priority) {
+    MockDealer dealer;
     Dealer::DealerFlag highPriorityFlag(DealerFlagTitle::ROBOT_TYPE_50W, DealerFlagPriority::HIGH_PRIORITY);
-    EXPECT_EQ(dealer.getFactorForPriority(highPriorityFlag), 3.0);
     Dealer::DealerFlag mediumPriorityFlag(DealerFlagTitle::ROBOT_TYPE_50W, DealerFlagPriority::MEDIUM_PRIORITY);
-    EXPECT_EQ(dealer.getFactorForPriority(mediumPriorityFlag), 2.0);
     Dealer::DealerFlag lowPriorityFlag(DealerFlagTitle::ROBOT_TYPE_50W, DealerFlagPriority::LOW_PRIORITY);
-    EXPECT_EQ(dealer.getFactorForPriority(lowPriorityFlag), 1.0);
+
+    auto priority1 = dealer.getFactorForPriority(lowPriorityFlag);
+    auto priority2 = dealer.getFactorForPriority(mediumPriorityFlag);
+    auto priority3 = dealer.getFactorForPriority(highPriorityFlag);
+    EXPECT_LT(priority1, priority2);
+    EXPECT_LT(priority2, priority3);
 }
