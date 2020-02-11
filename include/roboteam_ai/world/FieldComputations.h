6d0e6ce25de30568a5c82299310c0b74d52d3105
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-02-11 10:25:10 +0100

    Fixed part of the FieldComputations documentation

--- include/roboteam_ai/world/FieldComputations.h
+++ include/roboteam_ai/world/FieldComputations.h
@@ -24,15 +24,31 @@ class FieldComputations {
     public:
         /**
          * Determines whether a given point is in our/their defence area
-         * @param field
-         * @param point
-         * @param isOurDefenceArea
-         * @param margin
-         * @param includeOutsideField
-         * @return
+         * @param field The field class which is used to determine the position of the defence areas.
+         * @param point The point for which it is checked whether it is in our/their defence area.
+         * @param isOurDefenceArea True if it is checked whether the point is in our defence area, false if it is checked whether the point is in the opponents defence area (if
+         * this parameter is not set then it checks whether the point is in our defence area).
+         * @param margin The outwards margin with which the defence area will be expanded/shrinked for determining whether the point is in our defence area. A positive value means
+         * that the rectangular defence area will be expanded in both y directions and towards the center of the field, a negative value means that the rectangular defence area
+         * will be shrinked in both y directions and towards the goal of that defence area (if this paremeter is not set then the defence area will not be expanded/shrinked).
+         * @param includeOutsideField If set to true then the defence area will also be expanded/shrinked at the goal side (boundary side) of the field, if set to false then it
+         * will not be expanded/shrinked at the goal side (boundary side) of the field (if this parameter is not set then the defence area will not be expanded/shrinked at the goal
+         * side (boundary side) of the field).
+         * @return True if the point is in the given defence area, false if the point is not in the given defence area.
          */
         static bool pointIsInDefenceArea(const Field &field, const Vector2& point, bool isOurDefenceArea = true, double margin = 0.0, bool includeOutsideField = false);
+
+        /**
+         * Check whether a given point is in the field.
+         * @param field The field class which is used to determine the boundaries of the field.
+         * @param point The point for which it is checked whether it is in the field or not.
+         * @param margin The outwards margin in which the rectangular field area will get expanded in all directions. A positive value means that the field will be expanded with
+         * the absolute margin value in all directions. A negative value means that the field will be shrinked with the absolute margin value in all directions.
+         * @return True if the point is in the field, false if the point is not in the field.
+         */
         static bool pointIsInField(const Field &field, const Vector2& point, double margin = 0.0);
+
+
         static double getPercentageOfGoalVisibleFromPoint(const Field &field, bool ourGoal, const Vector2& point, const world::WorldData &world, int id = -1, bool ourTeam = false);
         static std::vector<Line> getBlockadesMappedToGoal(const Field &field, bool ourGoal, const Vector2& point, const world::WorldData &world, int id = -1, bool ourTeam = false);
         static std::vector<Line> mergeBlockades(std::vector<Line> blockades);
