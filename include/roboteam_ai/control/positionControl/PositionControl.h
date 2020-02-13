fa0940d13a1eac107b934a9d9480775168550519
Ratoone <vacariu.paul@gmail.com>
2020-02-13 17:01:29 +0100

    Replaced unique_ptr's with objects & fixed the robot vector passing

--- include/roboteam_ai/control/positionControl/PositionControl.h
+++ include/roboteam_ai/control/positionControl/PositionControl.h
@@ -20,22 +20,13 @@ namespace rtt::ai::control {
  */
 class PositionControl {
 private:
-    std::unique_ptr<NumTreesPlanning> pathPlanningAlgorithm;
-    std::unique_ptr<NumTreesTracking> pathTrackingAlgorithm;
-    std::unique_ptr<CollisionDetector> collisionDetector;
-
-    const std::vector<world_new::view::RobotView> &robots;
+    CollisionDetector collisionDetector;
+    NumTreesPlanning pathPlanningAlgorithm = NumTreesPlanning(collisionDetector);
+    NumTreesTracking pathTrackingAlgorithm;
 
     std::unordered_map<int, std::vector<Vector2>> computedPaths;
 
 public:
-    /**
-     * Specify in the constructor the algorithms (path tracking and planning) in
-     * case another one is needed.
-     * @param robots
-     */
-    explicit PositionControl(const std::vector<world_new::view::RobotView> &robots);
-
     /**
      * Generates a path according to the selected planning algorithm,
      * and tracks it using the selected tracking algorithm. In the case a collision
@@ -49,6 +40,12 @@ public:
      */
     RobotCommand computeAndTrackPath(const world::Field &field, int robotId, const Vector2 &currentPosition,
                                      const Vector2 &currentVelocity, const Vector2 &targetPosition);
+
+    /**
+     * Updates the robot view vector
+     * @param robots the RobotView vector of robots
+     */
+    void setRobotVector(const std::vector<world_new::view::RobotView>& robots);
 
     /**
      * The computed path should be recalculated if: <br>
