81bb68e6985bc28f6df2f2eb10d6145075e201b7
Timovdk <timo_kuil@hotmail.com>
2020-02-19 14:51:39 +0100

    BallKickedToOurGoal uses world_new now

--- src/conditions/BallKickedToOurGoal.cpp
+++ src/conditions/BallKickedToOurGoal.cpp
@@ -2,8 +2,6 @@
  * returns SUCCESS if the ball is kicked to the goal. Otherwise FAILURE.
  */
 #include "conditions/BallKickedToOurGoal.h"
-#include <world/Ball.h>
-#include <world/FieldComputations.h>
 #include "control/ControlUtils.h"
 
 namespace rtt::ai {
@@ -12,7 +10,7 @@ BallKickedToOurGoal::BallKickedToOurGoal(std::string name, bt::Blackboard::Ptr b
 
 bt::Node::Status BallKickedToOurGoal::onUpdate() {
     // Check if the ball is moving at all
-    bool ballIsLayingStill = (Vector2(ball->getVel())).length() < Constants::BALL_STILL_VEL();
+    bool ballIsLayingStill = (Vector2(ball->get()->getVelocity())).length() < Constants::BALL_STILL_VEL();
     if (ballIsLayingStill) {
         return Status::Failure;
     }
@@ -25,8 +23,8 @@ bt::Node::Status BallKickedToOurGoal::onUpdate() {
     Vector2 upperPost = goalCentre + Vector2(0.0, goalWidth / 2 + margin);
 
     // determine the ball position and predicted ball position
-    Vector2 ballPos = ball->getPos();
-    Vector2 ballPredPos = Vector2(ballPos) + Vector2(ball->getVel()) * BALL_TO_GOAL_TIME;
+    Vector2 ballPos = ball->get()->getPos();
+    Vector2 ballPredPos = Vector2(ballPos) + Vector2(ball->get()->getVelocity()) * BALL_TO_GOAL_TIME;
 
     // Check if the extension of the velocity vector goes through the goal.
     // The line drawn for the ball is the predicted position in 1.5 seconds
