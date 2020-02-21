7e98851079e56fd0620f28eec206a2c48cc9ef3e
RobotJesse <j.t.vanwerven@roboteamtwente.nl>
2020-02-21 14:16:48 +0100

    made new skill class in new_skills folder

---
+++ include/roboteam_ai/new_skills/NewSkill.h
@@ -0,0 +1,14 @@
+//
+// Created by jessevw on 21.02.20.
+//
+
+#ifndef RTT_NEWSKILL_H
+#define RTT_NEWSKILL_H
+
+namespace rtt::ai {
+    class NewSkill {
+
+    };
+
+}
+#endif //RTT_NEWSKILL_H
---
+++ src/new_skills/NewSkill.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jessevw on 21.02.20.
+//
+
+#include "include/roboteam_ai/new_skills/NewSkill.h"
