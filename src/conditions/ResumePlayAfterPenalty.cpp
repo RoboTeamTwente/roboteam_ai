c7a57ad00263536056451f82f49dad3337ad8d7c
Timovdk <timo_kuil@hotmail.com>
2020-02-21 10:32:53 +0100

    ResumePlayAfterPenalty uses world_new now

--- src/conditions/ResumePlayAfterPenalty.cpp
+++ src/conditions/ResumePlayAfterPenalty.cpp
@@ -1,19 +1,16 @@
-//
-// Created by rolf on 15-6-19.
-//
+/*
+ * returns SUCCESS if ball is shot and wait is done. Otherwise FAILURE.
+ */
 
 #include "conditions/ResumePlayAfterPenalty.h"
-#include <world/FieldComputations.h>
 #include <utilities/GameStateManager.hpp>
-#include "control/ControlUtils.h"
-#include "world/Ball.h"
 
 namespace rtt::ai {
 ResumePlayAfterPenalty::ResumePlayAfterPenalty(std::string name, bt::Blackboard::Ptr blackboard) : Condition(std::move(name), std::move(blackboard)) {}
 
 bt::Node::Status ResumePlayAfterPenalty::onUpdate() {
-    bool ballHasMoved = GameStateManager::getCurrentGameState().ballPositionAtStartOfGameState.dist(ball->getPos()) > 0.05;
-    bool ballIsLayingStill = (Vector2(ball->getVel())).length() < Constants::BALL_STILL_VEL();
+    bool ballHasMoved = GameStateManager::getCurrentGameState().ballPositionAtStartOfGameState.dist(ball->get()->getPos()) > 0.05;
+    bool ballIsLayingStill = (Vector2(ball->get()->getVelocity())).length() < Constants::BALL_STILL_VEL();
 
     if (!ballShot && !ballIsLayingStill && ballHasMoved) {
         ballShot = true;
