3e56fa8248552929c7597ffc0465d4d141c3fe9e
Timovdk <timo_kuil@hotmail.com>
2020-02-25 17:00:52 +0100

    ChipForward uses world_new now

--- src/skills/ChipForward.cpp
+++ src/skills/ChipForward.cpp
@@ -2,21 +2,21 @@
 // Created by robzelluf on 7/5/19.
 //
 
-#include "skills/ChipForward.h"
-#include <control/shot-controllers/ShotController.h>
-#include <world/FieldComputations.h>
+#include <skills/ChipForward.h>
+
+#include <world_new/FieldComputations.hpp>
 
 namespace rtt::ai {
 
 ChipForward::ChipForward(std::string name, bt::Blackboard::Ptr blackboard) : Skill(std::move(name), std::move(blackboard)) {}
 
 void ChipForward::onInitialize() {
-    aimPoint = FieldComputations::getPenaltyPoint(*field, false);
+    aimPoint = world_new::FieldComputations::getPenaltyPoint(*field, false);
     hasChipped = false;
 }
 
 ChipForward::Status ChipForward::onUpdate() {
-    auto shotData = robot->getShotController()->getRobotCommand(*field, *robot, aimPoint, true, control::BallSpeed::MAX_SPEED, false, control::ShotPrecision::LOW);
+    auto shotData = robot->getControllers().getShotController()->getRobotCommand(*field, *robot, aimPoint, true, control::BallSpeed::MAX_SPEED, false, control::ShotPrecision::LOW);
     command = shotData.makeROSCommand();
     if (!hasChipped && command.chipper()) {
         if (command.chip_kick_forced() || robot->hasBall()) hasChipped = true;
@@ -24,7 +24,7 @@ ChipForward::Status ChipForward::onUpdate() {
 
     publishRobotCommand();
 
-    if (hasChipped && (ball->getPos() - robot->pos).length() > 0.5) {
+    if (hasChipped && (ball->get()->getPos() - robot->get()->getPos()).length() > 0.5) {
         return Status::Success;
     }
 
