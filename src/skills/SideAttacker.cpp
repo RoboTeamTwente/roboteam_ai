5d28e8df532adec6d8c1081d4d7bf67a11ad06ca
Timovdk <timo_kuil@hotmail.com>
2020-02-25 19:10:01 +0100

    SideAttacker uses world_new now

--- src/skills/SideAttacker.cpp
+++ src/skills/SideAttacker.cpp
@@ -2,18 +2,19 @@
 // Created by thijs on 17-12-18.
 //
 
-#include "skills/SideAttacker.h"
+#include <skills/SideAttacker.h>
+#include <coach/OffensiveCoach.h>
 
 namespace rtt::ai {
 
 SideAttacker::SideAttacker(std::string name, bt::Blackboard::Ptr blackboard) : Skill(std::move(name), std::move(blackboard)) {}
 
-void SideAttacker::onInitialize() { coach::g_offensiveCoach.addSideAttacker(*field, robot); }
+void SideAttacker::onInitialize() { coach::g_offensiveCoach.addSideAttacker(*field, *robot); }
 
 /// Get an update on the skill
 bt::Node::Status SideAttacker::onUpdate() {
     targetPos = getOffensivePosition(*field);
-    auto newPosition = robot->getNumtreePosControl()->getRobotCommand(world, field, robot, targetPos);
+    auto newPosition = robot->getControllers().getNumTreePosController()->getRobotCommand(world, field, *robot, targetPos);
     Vector2 velocity = newPosition.vel;
     command.mutable_vel()->set_x(velocity.x);
     command.mutable_vel()->set_y(velocity.y);
@@ -25,13 +26,13 @@ bt::Node::Status SideAttacker::onUpdate() {
     return status;
 }
 
-Vector2 SideAttacker::getOffensivePosition(const Field &field) { return coach::g_offensiveCoach.getPositionForRobotID(field, robot->id); }
+Vector2 SideAttacker::getOffensivePosition(const Field &field) { return coach::g_offensiveCoach.getPositionForRobotID(field, robot->get()->getId()); }
 
 void SideAttacker::onTerminate(Status s) {
-    command.set_w(robot->angle);
+    command.set_w(robot->get()->getAngle());
     command.mutable_vel()->set_x(0);
     command.mutable_vel()->set_y(0);
-    coach::g_offensiveCoach.removeSideAttacker(robot);
+    coach::g_offensiveCoach.removeSideAttacker(*robot);
     publishRobotCommand();
 }
 
