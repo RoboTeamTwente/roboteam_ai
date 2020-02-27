578468e782d5310d0febfee52d753e91a3920de3
Timovdk <timo_kuil@hotmail.com>
2020-02-27 10:26:31 +0100

    FreeKickFormation uses world_new now

--- src/skills/formations/FreeKickFormation.cpp
+++ src/skills/formations/FreeKickFormation.cpp
@@ -1,21 +1,21 @@
-#include "skills/formations/FreeKickFormation.h"
 #include <control/PositionUtils.h>
+#include <skills/formations/FreeKickFormation.h>
 
 namespace rtt::ai {
 
 std::vector<Vector2> FreeKickFormation::posses;
-std::shared_ptr<std::vector<bt::Leaf::RobotPtr>> rtt::ai::FreeKickFormation::robotsInFormation = nullptr;
+std::vector<world_new::view::RobotView> rtt::ai::FreeKickFormation::robotsInFormation{};
 
 Vector2 FreeKickFormation::getFormationPosition() {
     update = true;
-    posses = rtt::ai::control::PositionUtils::getFreeKickPositions(*field, robotsInFormation->size());
-    return getOptimalPosition(robot->id, *robotsInFormation, posses);
+    posses = rtt::ai::control::PositionUtils::getFreeKickPositions(*field, robotsInFormation.size());
+    return getOptimalPosition(robot->get()->getId(), robotsInFormation, posses);
 }
 
-std::shared_ptr<std::vector<bt::Leaf::RobotPtr>> FreeKickFormation::robotsInFormationPtr() { return robotsInFormation; }
+std::vector<world_new::view::RobotView> FreeKickFormation::robotsInFormationPtr() { return robotsInFormation; }
 
 FreeKickFormation::FreeKickFormation(std::string name, bt::Blackboard::Ptr blackboard) : Formation(name, blackboard) {
-    robotsInFormation = std::make_shared<std::vector<bt::Leaf::RobotPtr>>();
+    robotsInFormation = std::vector<world_new::view::RobotView>();
 };
 
 void FreeKickFormation::onTerminate(Skill::Status s) {
