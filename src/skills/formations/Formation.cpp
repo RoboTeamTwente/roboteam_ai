a1e82c4fbebe35a75ab1183e7772b6a0a23efc20
Timovdk <timo_kuil@hotmail.com>
2020-02-27 10:24:43 +0100

    Formation uses world_new now

--- src/skills/formations/Formation.cpp
+++ src/skills/formations/Formation.cpp
@@ -2,16 +2,8 @@
 // Created by mrlukasbos on 23-1-19.
 //
 
-#include "skills/formations/Formation.h"
-#include <analysis/DecisionMaker.h>
-#include <analysis/GameAnalyzer.h>
-
-#include <analysis/DecisionMaker.h>
-#include "control/ControlUtils.h"
-#include "roboteam_utils/Hungarian.h"
-#include "skills/formations/Formation.h"
-#include "world/Field.h"
-#include "world/FieldComputations.h"
+#include <roboteam_utils/Hungarian.h>
+#include <skills/formations/Formation.h>
 
 namespace rtt::ai {
 
@@ -45,7 +37,7 @@ bt::Node::Status Formation::onUpdate() {
 // determine the angle where the robot should point to (in position)
 void Formation::setFinalAngle() {
     Vector2 targetToLookAtLocation = (*field).getTheirGoalCenter();
-    command.set_w(static_cast<float>((targetToLookAtLocation - robot->pos).angle()));
+    command.set_w(static_cast<float>((targetToLookAtLocation - robot->get()->getPos()).angle()));
 }
 
 void Formation::terminate(Status s) { onTerminate(s); }
@@ -59,14 +51,14 @@ void Formation::onTerminate(bt::Node::Status s) {
 // loop through all formationrobots to see if our robot is there.
 // if not, add it.
 void Formation::addRobotToFormation() {
-    if (!robotIsInFormation()) robotsInFormationPtr()->push_back(robot);
+    if (!robotIsInFormation()) robotsInFormationPtr().push_back(robot.value());
 }
 
 // remove robot from formation
 void Formation::removeRobotFromFormation() {
-    for (unsigned int i = 0; i < robotsInFormationPtr()->size(); i++) {
-        if (robotsInFormationPtr()->at(i) && robotsInFormationPtr()->at(i)->id == robot->id) {
-            robotsInFormationPtr()->erase(robotsInFormationPtr()->begin() + i);
+    for (unsigned int i = 0; i < robotsInFormationPtr().size(); i++) {
+        if (robotsInFormationPtr().at(i) && robotsInFormationPtr().at(i)->getId() == robot->get()->getId()) {
+            robotsInFormationPtr().erase(robotsInFormationPtr().begin() + i);
         }
     }
 }
@@ -76,8 +68,8 @@ void Formation::removeRobotFromFormation() {
 // in that case, use robotIsInPosition()
 bool Formation::robotIsInFormation() {
     bool isIn = false;
-    for (auto const& bot : *robotsInFormationPtr()) {
-        if (bot->id == robot->id) {
+    for (auto const& bot : robotsInFormationPtr()) {
+        if (bot->getId() == robot->get()->getId()) {
             isIn = true;
         }
     }
@@ -85,28 +77,28 @@ bool Formation::robotIsInFormation() {
 }
 
 // return true if the number of robots in the formation changed.
-bool Formation::formationHasChanged() { return robotsInFormationMemory != static_cast<int>(robotsInFormationPtr()->size()); }
+bool Formation::formationHasChanged() { return robotsInFormationMemory != static_cast<int>(robotsInFormationPtr().size()); }
 
 // adapt to the change of robot amount in formation
 void Formation::updateFormation() {
     if (formationHasChanged() || updateCounter()) {
         targetLocation = getFormationPosition();
-        robotsInFormationMemory = robotsInFormationPtr()->size();
+        robotsInFormationMemory = robotsInFormationPtr().size();
     }
 }
 
 // return true if a robot is at the desired position
 bool Formation::robotIsInPosition() {
-    auto robotPos = rtt::Vector2(robot->pos);
+    auto robotPos = rtt::Vector2(robot->get()->getPos());
     return robotPos.dist(targetLocation) < errorMargin;
 }
 
 // set up the command such that a robot moves towards the targetLocation
 void Formation::moveToTarget() {
-    auto velocities = robot->getNumtreePosControl()->getRobotCommand(world, field, robot, targetLocation);
+    auto velocities = robot->getControllers().getNumTreePosController()->getRobotCommand(world, field, *robot, targetLocation);
     command.mutable_vel()->set_x(velocities.vel.x);
     command.mutable_vel()->set_y(velocities.vel.y);
-    command.set_w(static_cast<float>((targetLocation - robot->pos).angle()));
+    command.set_w(static_cast<float>((targetLocation - robot->get()->getPos()).angle()));
 }
 
 bool Formation::updateCounter() {
@@ -114,11 +106,11 @@ bool Formation::updateCounter() {
     return (++updateCount % 200) == 0;
 }
 
-Vector2 Formation::getOptimalPosition(int robotId, const std::vector<RobotPtr>& robots, std::vector<Vector2> targetLocations) {
+Vector2 Formation::getOptimalPosition(int robotId, const std::vector<world_new::view::RobotView>& robots, std::vector<Vector2> targetLocations) {
     std::unordered_map<int, Vector2> robotLocations;
 
     for (auto formationRobot : robots) {
-        robotLocations.insert({formationRobot->id, formationRobot->pos});
+        robotLocations.insert({formationRobot->getId(), formationRobot->getPos()});
     }
 
     auto shortestDistances = rtt::Hungarian::getOptimalPairsIdentified(robotLocations, std::move(targetLocations));
