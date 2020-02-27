ce78372f1e337165a0c713fb925e1d33ac503bbd
Ratoone <vacariu.paul@gmail.com>
2020-02-27 13:33:36 +0100

    Added documentation to PathPointNode

--- include/roboteam_ai/control/positionControl/PathPointNode.h
+++ include/roboteam_ai/control/positionControl/PathPointNode.h
@@ -8,19 +8,28 @@
 #include <roboteam_utils/Vector2.h>
 
 namespace rtt::ai::control{
+/**
+ * A node in the path tree generation. Has a position and a parent
+ */
 class PathPointNode {
 private:
     Vector2 position;
-    double time = 0;
     PathPointNode *parent = nullptr;
 public:
+    /**
+     * Create a new tree node with a null parent (this will usually be the root of the tree)
+     * @param position the position represented by the node
+     */
     explicit PathPointNode(const Vector2 &position);
 
+    /**
+     * Create a new tree node with the specified parent
+     * @param position the position represented by the node
+     * @param parent the parent node of the current node
+     */
     PathPointNode(const Vector2 &position, PathPointNode &parent);
 
     [[nodiscard]] const Vector2 &getPosition() const;
-
-    [[nodiscard]] double getTime() const;
 
     [[nodiscard]] PathPointNode *getParent() const;
 
