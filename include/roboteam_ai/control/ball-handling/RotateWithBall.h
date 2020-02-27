1366ef0c4f9636ef46a28f03edaf9fb87285a966
Timovdk <timo_kuil@hotmail.com>
2020-02-27 13:14:02 +0100

    RotateWithBall uses world_new now

--- include/roboteam_ai/control/ball-handling/RotateWithBall.h
+++ include/roboteam_ai/control/ball-handling/RotateWithBall.h
@@ -2,14 +2,15 @@
 #define ROBOTEAM_AI_ROTATEAROUNDROBOT_H
 
 #include <include/roboteam_ai/world/Robot.h>
-#include "control/RobotCommand.h"
+#include <control/RobotCommand.h>
+#include <world_new/World.hpp>
 
 namespace rtt::ai::control {
 
 class RotateWithBall {
    public:
     explicit RotateWithBall() = default;
-    RobotCommand getRobotCommand(const std::shared_ptr<world::Robot> &r, const Vector2 &targetP, const Angle &targetA);
+    RobotCommand getRobotCommand(const world_new::view::RobotView r, const Vector2 &targetP, const Angle &targetA);
 };
 
 }  // namespace rtt::ai::control
