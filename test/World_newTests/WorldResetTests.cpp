dc479c9f55012e25f4f54629f3f5844bfa5e430c
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-02 18:57:55 +0100

    [  PASSED  ]

--- test/World_newTests/WorldResetTests.cpp
+++ test/World_newTests/WorldResetTests.cpp
@@ -8,6 +8,14 @@
 
 TEST(World_newTest, ResetWorldTest) {
     namespace w_n = rtt::world_new;
-    auto _ball = w_n::World::instance()->getBall();
-    _ball-
+    proto::GeometryFieldSize size {};
+    size.set_field_length(250);
+    auto msg = testhelpers::WorldHelper::getWorldMsg(5, 7, true, size);
+    auto second = msg;
+    w_n::World::instance()->updateWorld(msg);
+    w_n::World::instance()->updateWorld(msg);
+    ASSERT_TRUE(w_n::World::instance()->getWorld().has_value());
+    ASSERT_EQ(w_n::World::instance()->getWorld()->getUs().size(), 5);
+    auto world = w_n::World::instance(true);
+    ASSERT_FALSE(world->getWorld().has_value());
 }
