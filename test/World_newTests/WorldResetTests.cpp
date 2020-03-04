bd5acd8b4caef77a98fdf89bf98504a09678ade2
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-04 21:21:39 +0100

    Split up tests

--- test/World_newTests/WorldResetTests.cpp
+++ test/World_newTests/WorldResetTests.cpp
@@ -7,6 +7,37 @@
 #include <include/roboteam_ai/world_new/World.hpp>
 #include <test/helpers/WorldHelper.h>
 
+TEST(World_newTest, GenericWorldRemoval) {
+    namespace w_n = rtt::world_new;
+    proto::GeometryFieldSize size {};
+    size.set_field_length(250);
+    auto msg = testhelpers::WorldHelper::getWorldMsg(5, 7, true, size);
+    auto second = msg;
+    w_n::World::instance()->reset();
+    w_n::World::instance()->updateWorld(msg);
+    w_n::World::instance()->updateWorld(second);
+    ASSERT_TRUE(w_n::World::instance()->getWorld().has_value());
+
+    auto world = w_n::World::instance();
+    world->reset();
+    ASSERT_FALSE(world->getWorld().has_value());
+}
+
+TEST(World_newTest, HistorySizeTest) {
+    namespace w_n = rtt::world_new;
+    proto::GeometryFieldSize size {};
+    size.set_field_length(250);
+    auto msg = testhelpers::WorldHelper::getWorldMsg(5, 7, true, size);
+    auto second = msg;
+    w_n::World::instance()->reset();
+    w_n::World::instance()->updateWorld(msg);
+    w_n::World::instance()->updateWorld(second);
+    ASSERT_EQ(w_n::World::instance()->getHistorySize(), 1);
+
+    auto world = w_n::World::instance();
+    world->reset();
+    ASSERT_EQ(world->getHistorySize(), 0);
+}
 
 TEST(World_newTest, ResetWorldTest) {
     namespace w_n = rtt::world_new;
@@ -17,8 +48,6 @@ TEST(World_newTest, ResetWorldTest) {
     w_n::World::instance()->reset();
     w_n::World::instance()->updateWorld(msg);
     w_n::World::instance()->updateWorld(second);
-    ASSERT_TRUE(w_n::World::instance()->getWorld().has_value());
-    ASSERT_EQ(w_n::World::instance()->getHistorySize(), 1);
     ASSERT_EQ(w_n::World::instance()->getWorld()->getUs().size(), 5);
     ASSERT_EQ(w_n::World::instance()->getWorld()->getThem().size(), 7);
     ASSERT_TRUE(w_n::World::instance()->getWorld()->getBall().has_value());
@@ -27,5 +56,4 @@ TEST(World_newTest, ResetWorldTest) {
     auto world = w_n::World::instance();
     world->reset();
     ASSERT_FALSE(world->getWorld().has_value());
-    ASSERT_EQ(world->getHistorySize(), 0);
 }
