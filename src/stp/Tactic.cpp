5e02836e241960605b55965104d91c1cc00ba56d
RobotJesse <j.t.vanwerven@roboteamtwente.nl>
2020-03-06 15:59:10 +0100

    removed some bad code

--- src/stp/Tactic.cpp
+++ src/stp/Tactic.cpp
@@ -4,17 +4,12 @@
 
 #include "include/roboteam_ai/stp/Tactic.h"
 #include "stp/Skill.h"
+#include <roboteam_utils/containers/state_machine.hpp>
 namespace rtt::ai::stp {
 
     void Tactic::updateActiveSkill(TacticInfo tacticInfo) {
         auto activeSkill = std::find_if(skills.begin(), skills.end(), [] (const Skill& s) {return s.getStatus() == Status::Running;});
-        auto skillInfo = SkillInfo();
-        auto tacticInfo = TacticInfo();
-        tacticInfo.areasToAvoid = bla;
-        tacticInfo.numberOfCandy = 5;
-        tacticInfo.areasToAvoid
-        tacticInfo.numberOfCandy == 9;
-        activeSkill->update(skillInfo);
+
     }
 
     void Tactic::calculateInfoForSkill() {
