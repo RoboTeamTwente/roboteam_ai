0d819975716eb8cc5e7282bae16ff4d5c631891a
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-09 14:45:13 +0100

    Moved playchecker

--- include/roboteam_ai/analysis/play-utilities/PlayChecker.h
+++ include/roboteam_ai/analysis/play-utilities/PlayChecker.h
@@ -5,19 +5,19 @@
 #ifndef RTT_PLAYCHECKER_H
 #define RTT_PLAYCHECKER_H
 
-#include "Play.h"
+#include "analysis/play-utilities/Play.h"
 
 namespace rtt::ai::analysis {
+using namespace rtt::ai::world;
 
 class PlayChecker {
    public:
     PlayChecker() = default;
-
     /**
      * Updates the PlayChecker. When this function is called, we check if the play is still valid for the current gamestate,
      * and if it is not new plays are calculated that are valid for the gamestate.
      */
-    bool update(world::World *world, const rtt::ai::world::Field &field);
+    void update(world::World *world, const Field *field);
 
    private:
     /**
@@ -26,31 +26,27 @@ class PlayChecker {
      * @param field the current field
      * @return true if invariants of the play being executed are true, false otherwise
      */
-    bool checkCurrentGameInvariants(rtt::ai::world::World *world, const rtt::ai::world::Field &field);
+    bool checkCurrentGameInvariants(rtt::ai::world::World *world, const Field *field);
 
     /**
      * Vector of all plays (before pruning)
      */
-    std::vector<std::unique_ptr<Play>> allPlays;
+    std::vector<Play> allPlays;
 
     /**
      * Vector of all plays that are valid for the current world and field state
      */
-    std::vector<Play *> validPlays;
+    std::vector<Play> validPlays;
 
-   public:
-    const std::vector<Play *> getValidPlays() const;
-
-   private:
     /// TODO: implement this function. Not a priority right now
     bool checkStrategyPreconditions();
 
     /**
      * Determines which plays are valid by cycling through the allplays vector and seeing which plays' isValid() methods return true
-     * @param world the current world
-     * @param field the current field
+     * @param world
+     * @param field
      */
-    void determineNewPlays(world::World *world, const world::Field &field);
+    void determineNewPlays(world::World *world, const Field *field);
 };
 }  // namespace rtt::ai::analysis
 
--- src/analysis/play-utilities/PlayChecker.cpp
+++ src/analysis/play-utilities/PlayChecker.cpp
@@ -13,36 +13,28 @@
 #include "functional"
 
 namespace rtt::ai::analysis {
+using namespace rtt::ai::world;
 
-bool PlayChecker::checkCurrentGameInvariants(rtt::ai::world::World *world, const rtt::ai::world::Field &field) { return true; }
+bool PlayChecker::checkCurrentGameInvariants(World *world, const Field *field) { return true; }
 
 /**
  * Determines what plays are viable given the current world, ref states and invariants/preconditions, and stores them in the validPlays vector
  * TODO: add lambda here, to make it faster and cleaner
  */
-void PlayChecker::determineNewPlays(rtt::ai::world::World *world, const rtt::ai::world::Field &field) {
+void PlayChecker::determineNewPlays(rtt::ai::world::World *world, const Field *field) {
     validPlays.clear();
-    for (auto const &play : allPlays) {
-        if (play->isValidPlay(world, field)) {
-            validPlays.push_back(play.get());
+    for (auto play : allPlays) {
+        if (play.isValidPlay(world, field)) {
+            validPlays.push_back(play);
         }
     }
 }
 
-bool PlayChecker::update(rtt::ai::world::World *world, const rtt::ai::world::Field &field) {
-    if (checkCurrentGameInvariants(world, field)) {
-        //        std::cout << "current play is still valid" << std::endl;
-        return true;
-    }
-    // Otherwise we select new plays for the playdecider by updating the validPlays vector of this object. This can then be accessed by the playdecider.
-    else {
-        std::cout << "current play is not valid anymore, calculating new valid plays" << std::endl;
+void PlayChecker::update(World *world, const Field *field) {
+    if (!checkCurrentGameInvariants(world, field)) {
         determineNewPlays(world, field);
-        return false;
+        /// TODO: there should be a call to PlayDecider here, but this is not implemented yet.
     }
 }
 
-const std::vector<Play *> PlayChecker::getValidPlays() const { return validPlays; }
-
 }  // namespace rtt::ai::analysis
-// namespace rtt::ai::analysis
