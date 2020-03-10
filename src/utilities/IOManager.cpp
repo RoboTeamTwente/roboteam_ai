463b7e80a3dd0f09a8b4aea0ef1d14a2d6a994bc
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-10 16:32:53 +0100

    Codefactor

--- src/utilities/IOManager.cpp
+++ src/utilities/IOManager.cpp
@@ -104,14 +104,12 @@ void IOManager::publishRobotCommand(proto::RobotCommand cmd) {
         // the geneva cannot be received from world, so we set it when it gets sent.
         auto robot = world_new::World::instance()->getWorld()->getRobotForId(cmd.id(), true);
         if (robot) {
-
             if (cmd.kicker()) {
                 interface::Input::drawData(interface::Visual::SHOTLINES, {robot->get()->getPos()}, Qt::green, robot->get()->getId(), interface::Drawing::CIRCLES, 36, 36, 8);
             }
             if (cmd.chip_kick_forced()) {
                 interface::Input::drawData(interface::Visual::SHOTLINES, {robot->get()->getPos()}, Qt::green, robot->get()->getId(), interface::Drawing::DOTS, 36, 36, 8);
             }
-
             if (cmd.chipper()) {
                 interface::Input::drawData(interface::Visual::SHOTLINES, {robot->get()->getPos()}, Qt::yellow, robot->get()->getId(), interface::Drawing::CIRCLES, 36, 36, 8);
             }
