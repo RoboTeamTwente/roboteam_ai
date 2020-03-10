5bb14f4e6eb906e1c06bb18a6e670b16725ed2d5
Ratoone <vacariu.paul@gmail.com>
2020-03-10 16:40:25 +0100

    Stub for GetBall

---
+++ include/roboteam_ai/stp/new_tactics/GetBall.h
@@ -0,0 +1,14 @@
+//
+// Created by ratoone on 10-03-20.
+//
+
+#ifndef RTT_GETBALL_H
+#define RTT_GETBALL_H
+
+
+class GetBall {
+
+};
+
+
+#endif //RTT_GETBALL_H
---
+++ src/stp/new_tactics/GetBall.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ratoone on 10-03-20.
+//
+
+#include "GetBall.h"
