b00577574a7f72c4ace1792ca43f2281f7afa9e8
Ratoone <vacariu.paul@gmail.com>
2020-03-10 18:03:40 +0100

    Basic logic for positioning to get the ball

--- src/stp/new_tactics/GetBall.cpp
+++ src/stp/new_tactics/GetBall.cpp
@@ -4,11 +4,12 @@
 
 #include "stp/new_tactics/GetBall.h"
 #include <roboteam_utils/Print.h>
+#include "stp/new_skills/Rotate.h"
 #include "stp/new_skills/GoToPos.h"
 
 namespace rtt::ai::stp {
 void GetBall::onInitialize() noexcept {
-    skills = collections::state_machine<Skill, Status, SkillInfo>{GoToPos()};
+    skills = collections::state_machine<Skill, Status, SkillInfo>{GoToPos(), Rotate()};
 
     skills.initialize();
 }
@@ -24,10 +25,18 @@ SkillInfo GetBall::calculateInfoForSkill(TacticInfo const &info) noexcept {
     }
 
     SkillInfo skillInfo = SkillInfo();
-
-//    Vector2 newRobotPosition = skillInfo.setRobot();
+    Vector2 robotPosition;
+    Vector2 ballPosition = info.ball.value()->getPos();
+    // the robot will go to the position of the ball
+    double ballDistance = (ballPosition - robotPosition).length();
+    Vector2 newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - Constants::ROBOT_RADIUS());
+    if (ballDistance < 3 * Constants::ROBOT_RADIUS()){
+        skillInfo.setAngle((ballPosition - robotPosition).angle());
+        // TODO: what is the dribbler speed? why is the dribbler speed?
+        skillInfo.setDribblerSpeed(100);
+    }
 
 //    skillInfo.getTacticInfo().setPosition();
-    return {};
+    return skillInfo;
 }
 }  // namespace rtt::ai::stp
