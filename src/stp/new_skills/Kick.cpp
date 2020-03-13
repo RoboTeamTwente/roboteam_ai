ec9e681695d395962a1aac192d50b2d81fa28088
jordiluong <55586199+jordiluong@users.noreply.github.com>
2020-03-13 01:59:10 +0100

    Update skills structure (#886)
    * Clamp commands instead of returning Failure
    
    * Changed type of angle from float to double

--- src/stp/new_skills/Kick.cpp
+++ src/stp/new_skills/Kick.cpp
@@ -4,24 +4,20 @@
 
 #include "include/roboteam_ai/stp/new_skills/Kick.h"
 
-namespace rtt::ai::stp {
+namespace rtt::ai::stp::skill {
 
 void Kick::onInitialize() noexcept {}
 
 Status Kick::onUpdate(const StpInfo &info) noexcept {
-    double kickVelocity = info.getKickChipVelocity();
-
-    // Check if kick velocity is in range
-    if (kickVelocity < 0.0 || kickVelocity > Constants::MAX_KICK_POWER()) {
-        return Status::Failure;
-    }
+    // Clamp and set kick velocity
+    double kickVelocity = std::clamp(info.getKickChipVelocity(), 0.0, Constants::MAX_KICK_POWER());
 
     // Set kick command
     command.set_kicker(true);
     command.set_chip_kick_vel(kickVelocity);
 
     // Set angle command
-    command.set_w(info.getRobot().value()->getAngle());
+    command.set_w(info.getRobot().value()->getAngle().getAngle());
 
     publishRobotCommand();
 
@@ -30,4 +26,4 @@ Status Kick::onUpdate(const StpInfo &info) noexcept {
 
 void Kick::onTerminate() noexcept {}
 
-}  // namespace rtt::ai::stp
+}  // namespace rtt::ai::stp::skill
