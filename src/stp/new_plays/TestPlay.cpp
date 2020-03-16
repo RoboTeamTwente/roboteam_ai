f40091afc33e0b7a3cd835c307d225ed7a4bb8d9
RobotJesse <j.t.vanwerven@roboteamtwente.nl>
2020-03-16 12:16:25 +0100

    robots follow ball now

--- src/stp/new_plays/TestPlay.cpp
+++ src/stp/new_plays/TestPlay.cpp
@@ -51,7 +51,6 @@ void TestPlay::assignRoles() noexcept {
             stpInfos[roleName].setField(*world->getField());
 
             // TODO calculate additional info
-            stpInfos[roleName].setTargetPos({MOVETARGET, {robot->getId()*0.5, robot->getId()*0.5}});
         }
     }
 }
@@ -59,14 +58,14 @@ void TestPlay::assignRoles() noexcept {
     void TestPlay::calculateInfoForPlay() noexcept {
         for (auto & role : roles) {
             auto roleName{role->getName()};
-
+            if (stpInfos.find(roleName) != stpInfos.end()) {
                 stpInfos.emplace(roleName, StpInfo{});
                 stpInfos[roleName].setField(*world->getField());
                 auto robot = stpInfos[roleName].getRobot().value();
 
                 // TODO calculate additional info
-                stpInfos[roleName].setTargetPos({MOVETARGET, {robot->getId()*0.5, robot->getId()*0.5}});
-
+                stpInfos[roleName].setTargetPos({MOVETARGET, world->getWorld()->getBall()->get()->getPos()});
+            }
         }
     }
 
