efc35013f472b99d706fb2121f353d494013180b
RobotJesse <55085110+RobotJesse@users.noreply.github.com>
2020-03-17 17:17:24 +0100

    Apply suggestions from code review

--- src/stp/new_plays/TestPlay.cpp
+++ src/stp/new_plays/TestPlay.cpp
@@ -49,7 +49,6 @@ void TestPlay::assignRoles() noexcept {
             stpInfos.emplace(roleName, StpInfo{});
             stpInfos[roleName].setRobot(robot);
             stpInfos[roleName].setField(*world->getField());
-
         }
     }
 }
