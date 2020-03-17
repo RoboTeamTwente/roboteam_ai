29cd823796ba851c3fde8fbd31a0d0f2575336fb
Paul Vacariu <vacariu.paul@gmail.com>
2020-03-17 13:43:46 +0100

    Feature/get ball tactic (#900)
    * Stub for GetBall
    
    * Base stub for GetBall
    
    * Basic logic for positioning to get the ball
    
    * Set logic for GetBall
    
    * Added fail and reset conditions + whether the tactic is an end tactic or not
    
    * Fail conditions implemented in TestTactic. For for, it will never fail
    
    * Added the tactic info as a param for the failing conditions
    
    * Added failing conditions to GetBall
    
    * Reduced target changed radius for recalculating position control
    
    * Fixed code factor warning
    
    * Added extra documentation to calculate skill info
    
    * Angle provided in double
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Naming of position type
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Updated dribbler value
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Moved the check for empty ball and robot to tactic base class
    
    * Fixed build
    
    Co-authored-by: jordiluong <55586199+jordiluong@users.noreply.github.com>

--- src/stp/Play.cpp
+++ src/stp/Play.cpp
@@ -30,12 +30,10 @@ Status Play::update() noexcept {
     for (auto& each : roles) {
         auto roleName{each->getName()};
         if(stpInfos.find(roleName) != stpInfos.end()) {
-            // TODO refresh robots in a neater way than is done now.
-            // This is necessary to prevent the robotview from going out of scope
-            stpInfos.find(roleName)->second.setRobot(world->getWorld()->getRobotForId(stpInfos.find(roleName)->second.getRobot()->get()->getId()));
-            stpInfos.find(roleName)->second.setField(world->getField());
-            stpInfos.find(roleName)->second.setBall(world->getWorld()->getBall());
-
+            // TODO refresh robots
+            stpInfos[roleName].setRobot(world->getWorld()->getRobotForId(stpInfos.find(roleName)->second.getRobot()->get()->getId()));
+            stpInfos[roleName].setBall(world->getWorld()->getBall());
+            stpInfos[roleName].setField(world->getField());
 
             auto index = static_cast<size_t>(each->update(stpInfos.find(each->getName())->second));
             count[index] += 1;
