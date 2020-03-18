e54d6c7b80ae0004e31fdb180d31b0e481d2f5ab
Jordi <jordiluong@gmail.com>
2020-03-18 17:13:30 +0100

    Implemented methods for Attacker

--- src/stp/new_roles/Attacker.cpp
+++ src/stp/new_roles/Attacker.cpp
@@ -11,8 +11,28 @@ namespace rtt::ai::stp::role {
 
 Attacker::Attacker(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::GetBall(), tactic::DriveWithBall(), tactic::KickAtPos()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::GetBall()/*, tactic::DriveWithBall()*/, tactic::KickAtPos()};
     robotTactics.initialize();
+}
+
+StpInfo Attacker::calculateInfoForTactic(StpInfo const &info) noexcept {
+    StpInfo tacticInfo = info;
+
+    // Set info about the shot
+    tacticInfo.setPosition({SHOOT_TO_POSITION, calculateKickPosition(tacticInfo)});
+    tacticInfo.setKickChipType(MAX_SPEED);
+
+    return tacticInfo;
+}
+
+bool Attacker::shouldRoleReset(const StpInfo &info) noexcept {
+    // Reset Role when robot lost the ball
+    return !info.getRobot().value().hasBall();
+}
+
+Vector2 Attacker::calculateKickPosition(StpInfo const info) noexcept {
+    // Calculate the position of the goal to kick to
+    return info.getField()->getTheirGoalCenter();
 }
 
 } // namespace rtt::ai::stp::role
