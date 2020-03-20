b1ee402c98e3913d1c7020026d41bd9018040b1c
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-03-20 11:31:16 +0100

    Setup the FieldComputation tests

---
+++ test/WorldTests/FieldComputationTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Haico Dorenbos on 20-03-2020
+//
+
+#include <gtest/gtest.h>
+
+namespace rtt {
+
+// Checks whether the margins of the functions are all outwards
+TEST(FieldComputationTest, outwards_margin) {
+
+}
+
+}  // namespace rtt
