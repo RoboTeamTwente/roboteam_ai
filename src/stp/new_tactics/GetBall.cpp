3764bd39f2ada6789b55acdbf4d49d841ccb640c
Timovdk <timo_kuil@hotmail.com>
2020-03-20 15:33:45 +0100

    Tweaked the targetPosition to get the ball

--- src/stp/new_tactics/GetBall.cpp
+++ src/stp/new_tactics/GetBall.cpp
@@ -27,7 +27,8 @@ namespace rtt::ai::stp::tactic {
 
         // the robot will go to the position of the ball
         double ballDistance = (ballPosition - robotPosition).length();
-        Vector2 newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - Constants::CENTRE_TO_FRONT() - Constants::BALL_RADIUS());
+        // TODO: Tweak this distance, maybe only GRSIM needs the 1.2 factor?
+        Vector2 newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - Constants::CENTRE_TO_FRONT() - Constants::BALL_RADIUS() * 1.2);
         if (ballDistance < 3 * Constants::ROBOT_RADIUS()){
             skillInfo.setAngle((ballPosition - robotPosition).angle());
             skillInfo.setDribblerSpeed(100);
