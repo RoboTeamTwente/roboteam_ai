0c8015f11299ec779402b3ab8859687ce4f8c3d3
Paul Vacariu <vacariu.paul@gmail.com>
2020-03-23 09:27:42 +0100

    Tactic Pipeline Refactoring (#926)
    * Refactored tactic update flow according to diagram
    
    * Fixed tactic test considering the refactored pipeline
    
    * Added comment to the repeating of the last skill in tactic
    
    Co-Authored-By: timovdk <timo_kuil@hotmail.com>
    
    Co-authored-by: timovdk <timo_kuil@hotmail.com>

--- src/stp/Tactic.cpp
+++ src/stp/Tactic.cpp
@@ -2,7 +2,7 @@
 // Created by jessevw on 03.03.20.
 //
 
-#include "include/roboteam_ai/stp/Tactic.h"
+#include "stp/Tactic.h"
 
 #include <roboteam_utils/Print.h>
 
@@ -18,15 +18,26 @@ Status Tactic::update(StpInfo const &info) noexcept {
         return Status::Failure;
     }
 
-    // Check if the skills are all finished
-    if (skills.finished() && !isEndTactic()) {
-        // Tactic tests don't have robotID, so make sure it does not segfault here
-        if (info.getRobot().value()) RTT_INFO("TACTIC SUCCESSFUL for ", info.getRobot()->get()->getId())
-        return Status::Success;
-    }
-
     // Update skill info
     auto skill_info = calculateInfoForSkill(info);
+
+    // Update the current skill with the new SkillInfo
+    auto status = skills.update(skill_info);
+
+    // Call onUpdate on a skill for specific behaviour
+    onUpdate(status);
+    RTT_DEBUG("ID AFTER UPDATE: ", skills.current_num(), " Called on robot: ", info.getRobot()->get()->getId());
+
+    // Check if the skills are all finished
+    if (skills.finished()) {
+        RTT_INFO("TACTIC SUCCESSFUL for ", info.getRobot()->get()->getId())
+        if (!isEndTactic()) {
+            return Status::Success;
+        }
+        // Make sure we keep executing the last tactic since it is an end tactic
+        skills.skip_n(-1);
+        return Status::Waiting;
+    }
 
     // if the failing condition is true, the current tactic will fail
     if (isTacticFailing(skill_info)) {
@@ -34,18 +45,11 @@ Status Tactic::update(StpInfo const &info) noexcept {
         return Status::Failure;
     }
 
-    // the tactic will not be reset if it's the first skill; it will be reset as well if the
-    // state machine is finished
-    if (skills.finished() || (skills.current_num() != 0 && shouldTacticReset(skill_info))) {
+    // the tactic will not be reset if it's the first skill
+    if((skills.current_num() != 0 && shouldTacticReset(skill_info))){
         RTT_INFO("State Machine reset for current tactic for ID = ", info.getRobot()->get()->getId())
         reset();
     }
-
-    // Update the current skill with the new SkillInfo
-    auto status = skills.update(skill_info);
-
-    // Call onUpdate on a skill for specific behaviour
-    onUpdate(status);
 
     return Status::Running;
 }
