634f6234c1ec4f9167cf689fbed86ed79601383d
timovdk <timo_kuil@hotmail.com>
2020-03-23 09:28:39 +0100

    Refactor of role (#928)

--- src/stp/Role.cpp
+++ src/stp/Role.cpp
@@ -6,6 +6,7 @@
 
 namespace rtt::ai::stp {
 Status Role::update(StpInfo const& info) noexcept {
+    // Failure if the required data is not present
     if (!info.getBall() || !info.getRobot() || !info.getField()) {
         RTT_WARNING("Required information missing in the tactic info");
         return Status::Failure;
@@ -14,23 +15,33 @@ Status Role::update(StpInfo const& info) noexcept {
     // Update the current tactic with the new tacticInfo
     auto status = robotTactics.update(info);
 
-    // Check if the skills are all finished
-    if (robotTactics.finished()) {
+    // Success if the tactic returned success and if all tactics are done
+    if (status == Status::Success && robotTactics.finished()) {
         RTT_INFO("ROLE SUCCESSFUL for ", info.getRobot()->get()->getId())
         return Status::Success;
     }
 
-    // Reset the role
-    if (robotTactics.current_num() != 0 && status == Status::Failure) {
+    // Reset the tactic state machine if a tactic failed
+    if (status == Status::Failure) {
         RTT_INFO("State Machine reset for current role for ID = ", info.getRobot()->get()->getId())
-        // Reset all the Tactics state machines
+        // Reset all the Skills state machines
         for (auto& tactic : robotTactics) {
             tactic->reset();
         }
-        // Reset Role state machine
+        // Reset Tactics state machine
         robotTactics.reset();
     }
 
+    // Success if waiting and tactics are finished
+    // Waiting if waiting but not finished
+    if (status == Status::Waiting) {
+        if (robotTactics.finished()) {
+            return Status::Success;
+        }
+        return Status::Waiting;
+    }
+
+    // Return running by default
     return Status::Running;
 }
 
