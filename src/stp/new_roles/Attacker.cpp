b4d711c80d590a86c4a005d10bd837ed89534c4c
jordiluong <55586199+jordiluong@users.noreply.github.com>
2020-03-27 14:22:29 +0100

    Removed state machine initialization
    Co-Authored-By: timovdk <timo_kuil@hotmail.com>

--- src/stp/new_roles/Attacker.cpp
+++ src/stp/new_roles/Attacker.cpp
@@ -12,7 +12,6 @@ namespace rtt::ai::stp::role {
 Attacker::Attacker(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
     robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::GetBall()/*, tactic::DriveWithBall()*/, tactic::KickAtPos()};
-    robotTactics.initialize();
 }
 
 } // namespace rtt::ai::stp::role
