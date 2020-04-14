3a91f269fd4fd5980f458fbb23f4984837d9b7c8
Timovdk <timo_kuil@hotmail.com>
2020-04-14 12:34:12 +0200

    Introduced a bug oops

--- src/stp/invariants/BallCloseToUsInvariant.cpp
+++ src/stp/invariants/BallCloseToUsInvariant.cpp
@@ -9,6 +9,6 @@ bool BallCloseToUsInvariant::checkInvariant(world_new::view::WorldDataView world
     auto& us = world.getUs();
     auto ballPos = world.getBall()->get()->getPos();
     // If the distance between ballpos and robotpos is smaller than BALL_IS_CLOSE constant return true
-    return std::any_of(us.begin(), us.end(), [&ballPos](world_new::view::RobotView& robot) { return robot.get()->getPos().dist(ballPos) < control_constants::BALL_IS_CLOSE; });
+    return std::any_of(us.begin(), us.end(), [&ballPos](auto& robot) { return robot.get()->getPos().dist(ballPos) < control_constants::BALL_IS_CLOSE; });
 }
 }  // namespace rtt::ai::stp::invariant
