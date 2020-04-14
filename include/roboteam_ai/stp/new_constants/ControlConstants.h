43a8d994c08a9a6ffb5d267b9bad0103aa1947bf
Timovdk <timo_kuil@hotmail.com>
2020-04-14 12:25:38 +0200

    Implemented invariant and added constant

--- include/roboteam_ai/stp/new_constants/ControlConstants.h
+++ include/roboteam_ai/stp/new_constants/ControlConstants.h
@@ -23,6 +23,7 @@ inline constexpr size_t MAX_ROBOT_COUNT = 11;
 extern const double BALL_STILL_VEL;
 extern const double BALL_IS_MOVING_VEL;
 extern const double BALL_IS_MOVING_FAST_LIMIT;
+extern const double BALL_IS_CLOSE;
 extern const double BALL_RADIUS;
 extern const double HAS_KICKED_ERROR_MARGIN;
 extern const double HAS_CHIPPED_ERROR_MARGIN;
