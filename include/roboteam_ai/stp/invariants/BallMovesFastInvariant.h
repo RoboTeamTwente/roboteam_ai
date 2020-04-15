1c487855857f6134b9cd3d10c9206f3d10294c19
Timovdk <timo_kuil@hotmail.com>
2020-04-15 16:50:27 +0200

    Comments

--- include/roboteam_ai/stp/invariants/BallMovesFastInvariant.h
+++ include/roboteam_ai/stp/invariants/BallMovesFastInvariant.h
@@ -10,8 +10,14 @@
 namespace rtt::ai::stp::invariant {
 class BallMovesFastInvariant : public BaseInvariant {
    public:
-    [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const Field *field) const noexcept override;
+    [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const Field* field) const noexcept override;
+
    private:
+    /**
+     * Calculates the actual metric value using a piecewise linear function
+     * @param x the x of the function
+     * @return metric value between 0-255
+     */
     [[nodiscard]] uint8_t calculateMetric(const double& x) const noexcept;
 };
 }  // namespace rtt::ai::stp::invariant
--- include/roboteam_ai/stp/invariants/BaseInvariant.h
+++ include/roboteam_ai/stp/invariants/BaseInvariant.h
@@ -10,8 +10,26 @@
 namespace rtt::ai::stp::invariant {
 class BaseInvariant {
    public:
+    /**
+     * Converts the fuzzy return to a boolean return
+     * @param world the world
+     * @param field the field
+     * @param conversionMargin the cutoff number
+     * @return returns true when greater than cutoff and false when smaller than cutoff
+     */
     [[nodiscard]] bool checkInvariant(world_new::view::WorldDataView world, const Field *field, const uint8_t conversionMargin = 127) const noexcept;
+
+    /**
+     * Calculates the 'true-ness' of the invariant between 0 and 255. 0 == false, 255 == true
+     * @param world the world
+     * @param field the field
+     * @return the 'true-ness' of this invariant during this tick
+     */
     [[nodiscard]] virtual uint8_t metricCheck(world_new::view::WorldDataView world, const Field *field) const noexcept = 0;
+
+    /**
+     * dtor
+     */
     virtual ~BaseInvariant() = default;
 };
 }  // namespace rtt::ai::stp::invariant
