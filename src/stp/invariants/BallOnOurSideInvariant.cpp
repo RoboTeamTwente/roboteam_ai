01dbfe37d402e8ddd8e34d63e0c4b6ca23b5c5f1
timovdk <timo_kuil@hotmail.com>
2020-04-21 10:22:55 +0200

    Ball on our side invariant (#1022)
    * Implemented invariant
    
    * Added BallOnOurSide invariant

---
+++ src/stp/invariants/BallOnOurSideInvariant.cpp
@@ -0,0 +1,13 @@
+//
+// Created by timovdk on 4/14/20.
+//
+
+#include "stp/invariants/BallOnOurSideInvariant.h"
+
+#include <utilities/GameStateManager.hpp>
+
+namespace rtt::ai::stp::invariant {
+uint8_t BallOnOurSideInvariant::metricCheck(world_new::view::WorldDataView world, const Field *field) const noexcept {
+    return world.getBall().value()->getPos().x < 0 ? control_constants::FUZZY_TRUE : control_constants::FUZZY_FALSE;
+}
+}  // namespace rtt::ai::stp::invariant
