bce4ba272f1a091b9138b54cee75e7590d1aa5fe
timovdk <timo_kuil@hotmail.com>
2020-04-23 12:13:36 +0200

    Ball is close to us invariant (#1023)
    * Implemented invariant and added constant
    
    * Introduced a bug oops
    
    * Implemented ball close invariant with metrics. Still need to add piecewise linear function
    
    * Implemented ball close invariant with metrics
    
    * Fixed namespace
    
    * Optimization
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: jordiluong <55586199+jordiluong@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: RobotJesse <55085110+RobotJesse@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-Authored-By: RobotJesse <55085110+RobotJesse@users.noreply.github.com>
    
    * Update src/stp/invariants/BallCloseToUsInvariant.cpp
    
    Co-authored-by: RobotJesse <55085110+RobotJesse@users.noreply.github.com>
    Co-authored-by: jordiluong <55586199+jordiluong@users.noreply.github.com>

--- src/stp/new_constants/ControlConstants.cpp
+++ src/stp/new_constants/ControlConstants.cpp
@@ -22,7 +22,7 @@ const double TURN_ON_DRIBBLER_DISTANCE = 3 * ROBOT_RADIUS;
 // Ball constants
 constexpr double BALL_STILL_VEL = 0.1;
 constexpr double BALL_IS_MOVING_VEL = 0.5;
-constexpr double BALL_GOT_SHOT_LIMIT = 1.5;
+constexpr double BALL_GOT_SHOT_LIMIT = 1.3;
 constexpr double BALL_IS_MOVING_SLOW_LIMIT = 0.5;
 constexpr double BALL_IS_CLOSE = 0.5;
 constexpr double BALL_RADIUS = 0.0215;
