e29f08ec9ff00ed017f56f2ad16879eb59d3f27f
Jordi <jordiluong@gmail.com>
2020-04-23 11:44:00 +0200

    Naming style matches other invariants

--- src/stp/invariants/FreedomOfRobotsInvariant.cpp
+++ src/stp/invariants/FreedomOfRobotsInvariant.cpp
@@ -17,11 +17,11 @@ FreedomOfRobotsInvariant::FreedomOfRobotsInvariant() noexcept {
      *   (0,0)  |XXXXXX---------
      *              (Distance to closest enemy robot)
      */
-    piecewiseLinearFunction = nativeformat::param::createParam(stp::control_constants::FUZZY_FALSE, stp::control_constants::FUZZY_TRUE, stp::control_constants::FUZZY_FALSE, "distance to closest enemy robot");
-    piecewiseLinearFunction->setYAtX(stp::control_constants::FUZZY_FALSE, 0.0);
-    piecewiseLinearFunction->setYAtX(stp::control_constants::FUZZY_FALSE, stp::control_constants::DISTANCE_TO_ROBOT_CLOSE + stp::control_constants::FUZZY_MARGIN);
-    piecewiseLinearFunction->linearRampToYAtX(stp::control_constants::FUZZY_TRUE, stp::control_constants::DISTANCE_TO_ROBOT_FAR - stp::control_constants::FUZZY_MARGIN);
-    piecewiseLinearFunction->setYAtX(stp::control_constants::FUZZY_TRUE, stp::control_constants::DISTANCE_TO_ROBOT_FAR - stp::control_constants::FUZZY_MARGIN);
+    piecewiseLinearFunction = nativeformat::param::createParam(control_constants::FUZZY_FALSE, control_constants::FUZZY_TRUE, control_constants::FUZZY_FALSE, "distanceToClosestEnemyRobot");
+    piecewiseLinearFunction->setYAtX(control_constants::FUZZY_FALSE, 0.0);
+    piecewiseLinearFunction->setYAtX(control_constants::FUZZY_FALSE, stp::control_constants::DISTANCE_TO_ROBOT_CLOSE + stp::control_constants::FUZZY_MARGIN);
+    piecewiseLinearFunction->linearRampToYAtX(control_constants::FUZZY_TRUE, stp::control_constants::DISTANCE_TO_ROBOT_FAR - stp::control_constants::FUZZY_MARGIN);
+    piecewiseLinearFunction->setYAtX(control_constants::FUZZY_TRUE, stp::control_constants::DISTANCE_TO_ROBOT_FAR - stp::control_constants::FUZZY_MARGIN);
 }
 
 uint8_t FreedomOfRobotsInvariant::metricCheck(world_new::view::WorldDataView world, const Field* field) const noexcept {
