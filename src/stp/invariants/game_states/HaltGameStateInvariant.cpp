9526792d1ca441acf0cb1249a4c6d5a93e820fe2
Jordi <jordiluong@gmail.com>
2020-04-28 15:42:53 +0200

    Changed ball placement and halt invariants

--- src/stp/invariants/game_states/HaltGameStateInvariant.cpp
+++ src/stp/invariants/game_states/HaltGameStateInvariant.cpp
@@ -7,6 +7,6 @@
 
 namespace rtt::ai::stp::invariant {
 uint8_t HaltGameStateInvariant::metricCheck(world_new::view::WorldDataView world, const world::Field *field) const noexcept {
-    return GameStateManager::getCurrentGameState().getRuleSet().title == "halt" ? stp::control_constants::FUZZY_TRUE : stp::control_constants::FUZZY_FALSE;
+    return GameStateManager::getCurrentGameState().getStrategyName() == "halt_strategy" ? stp::control_constants::FUZZY_TRUE : stp::control_constants::FUZZY_FALSE;
 }
 }  // namespace rtt::ai::stp::invariant
