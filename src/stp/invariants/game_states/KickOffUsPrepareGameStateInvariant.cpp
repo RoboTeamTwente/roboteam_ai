7c20786675278bea17bc9711dc241c0b8f081b02
Jordi <jordiluong@gmail.com>
2020-04-28 16:02:38 +0200

    Invariants for kick off prepare

---
+++ src/stp/invariants/game_states/KickOffUsPrepareGameStateInvariant.cpp
@@ -0,0 +1,14 @@
+//
+// Created by jordi on 28-04-20.
+//
+
+#include "stp/invariants/game_states/KickOffUsPrepareGameStateInvariant.h"
+#include <utilities/GameStateManager.hpp>
+
+namespace rtt::ai::stp::invariant {
+
+uint8_t KickOffUsPrepareGameStateInvariant::metricCheck(world_new::view::WorldDataView, const world::Field *) const noexcept {
+    return GameStateManager::getCurrentGameState().getStrategyName() == "kickoff_us_prepare" ? stp::control_constants::FUZZY_TRUE : stp::control_constants::FUZZY_FALSE;
+}
+
+}  // namespace rtt::ai::stp::invariant
