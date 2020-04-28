b91806ce5d960c615d3a8293d8b761aa037f3ed2
Timovdk <timo_kuil@hotmail.com>
2020-04-28 13:57:52 +0200

    Implemented invariant

---
+++ include/roboteam_ai/stp/invariants/GoalVisionInvariant.h
@@ -0,0 +1,36 @@
+//
+// Created by timovdk on 4/24/20.
+//
+
+#ifndef RTT_GOALVISIONINVARIANT_H
+#define RTT_GOALVISIONINVARIANT_H
+
+#include <NFParam/Param.h>
+
+#include "BaseInvariant.h"
+
+namespace rtt::ai::stp::invariant {
+
+class GoalVisionInvariant : public BaseInvariant {
+   public:
+    GoalVisionInvariant() noexcept;
+
+    [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const world::Field* field) const noexcept override;
+
+   private:
+    /**
+     * Calculates the actual metric value using the piecewise linear function member
+     * @param x the x of the function
+     * @return metric value between 0-255
+     */
+    [[nodiscard]] uint8_t calculateMetric(const double& x) const noexcept;
+
+    /**
+     * Unique pointer to the piecewise linear function that calculates the fuzzy value
+     */
+    std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction;
+};
+
+}  // namespace rtt::ai::stp::invariant
+
+#endif  // RTT_GOALVISIONINVARIANT_H
