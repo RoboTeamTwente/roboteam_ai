2b825391a16e19580ae061e9612d25e0801110be
Jordi <jordiluong@gmail.com>
2020-05-01 15:08:12 +0200

    Implemented time out play

--- src/ApplicationManager.cpp
+++ src/ApplicationManager.cpp
@@ -16,6 +16,7 @@
 #include "stp/new_plays/BallPlacement.h"
 #include "stp/new_plays/DefensiveFormation.h"
 #include "stp/new_plays/AggressiveFormation.h"
+#include "stp/new_plays/TimeOut.h"
 
 namespace io = rtt::ai::io;
 namespace ai = rtt::ai;
@@ -39,6 +40,7 @@ void ApplicationManager::start() {
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::DefensiveFormation>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::AggressiveFormation>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::BallPlacement>());
+    plays.emplace_back(std::make_unique<rtt::ai::stp::play::TimeOut>());
     playChecker.setPlays(plays);
 
     int amountOfCycles = 0;
