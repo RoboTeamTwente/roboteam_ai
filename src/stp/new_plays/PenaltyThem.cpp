fb5a16bb8fff27dcd51a69c00fa0863ce10b5873
Timovdk <timo_kuil@hotmail.com>
2020-05-01 11:36:39 +0200

    Added a new role for a penalty keeper

--- src/stp/new_plays/PenaltyThem.cpp
+++ src/stp/new_plays/PenaltyThem.cpp
@@ -5,8 +5,8 @@
 #include <stp/invariants/BallCloseToUsInvariant.h>
 #include <stp/invariants/BallMovesSlowInvariant.h>
 #include <stp/new_plays/PenaltyThem.h>
-#include <stp/new_roles/Formation.h>
-#include <stp/new_roles/Keeper.h>
+#include <stp/new_roles/Halt.h>
+#include <stp/new_roles/PenaltyKeeper.h>
 
 namespace rtt::ai::stp::play {
 
@@ -19,53 +19,44 @@ PenaltyThem::PenaltyThem() : Play() {
     keepPlayInvariants.clear();
     keepPlayInvariants.emplace_back(std::make_unique<invariant::BallMovesSlowInvariant>());
 
-    roles = std::array<std::unique_ptr<Role>, stp::control_constants::MAX_ROBOT_COUNT>{std::make_unique<role::Keeper>(role::Keeper("keeper")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_0")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_1")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_2")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_3")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_4")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_5")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_6")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_7")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_8")),
-                                                                                       std::make_unique<role::Formation>(role::Formation("formation_9"))};
+    roles = std::array<std::unique_ptr<Role>, stp::control_constants::MAX_ROBOT_COUNT>{std::make_unique<role::PenaltyKeeper>(role::PenaltyKeeper("keeper")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_0")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_1")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_2")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_3")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_4")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_5")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_6")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_7")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_8")),
+                                                                                       std::make_unique<role::Halt>(role::Halt("halt_9"))};
 }
 
 uint8_t PenaltyThem::score(world_new::World *world) noexcept { return 111; }
 
 Dealer::FlagMap PenaltyThem::decideRoleFlags() const noexcept {
     Dealer::FlagMap flagMap;
-    Dealer::DealerFlag closeToBallFlag(DealerFlagTitle::CLOSE_TO_BALL, DealerFlagPriority::HIGH_PRIORITY);
-    Dealer::DealerFlag closeToTheirGoalFlag(DealerFlagTitle::CLOSE_TO_THEIR_GOAL, DealerFlagPriority::MEDIUM_PRIORITY);
+    Dealer::DealerFlag keeperFlag(DealerFlagTitle::KEEPER, DealerFlagPriority::REQUIRED);
 
-    flagMap.insert({"keeper", {closeToBallFlag}});
-    flagMap.insert({"formation_0", {closeToTheirGoalFlag}});
-    flagMap.insert({"formation_1", {closeToTheirGoalFlag, closeToBallFlag}});
-    flagMap.insert({"formation_2", {closeToBallFlag}});
-    flagMap.insert({"formation_3", {closeToTheirGoalFlag}});
-    flagMap.insert({"formation_4", {closeToTheirGoalFlag, closeToBallFlag}});
-    flagMap.insert({"formation_5", {closeToBallFlag}});
-    flagMap.insert({"formation_6", {closeToTheirGoalFlag}});
-    flagMap.insert({"formation_7", {closeToTheirGoalFlag, closeToBallFlag}});
-    flagMap.insert({"formation_8", {closeToBallFlag}});
-    flagMap.insert({"formation_9", {closeToTheirGoalFlag}});
+    flagMap.insert({"keeper", {keeperFlag}});
+    flagMap.insert({"formation_0", {}});
+    flagMap.insert({"formation_1", {}});
+    flagMap.insert({"formation_2", {}});
+    flagMap.insert({"formation_3", {}});
+    flagMap.insert({"formation_4", {}});
+    flagMap.insert({"formation_5", {}});
+    flagMap.insert({"formation_6", {}});
+    flagMap.insert({"formation_7", {}});
+    flagMap.insert({"formation_8", {}});
+    flagMap.insert({"formation_9", {}});
 
     return flagMap;
 }
 
 void PenaltyThem::calculateInfoForRoles() noexcept {
-    stpInfos["keeper"].setEnemyRobot(world->getWorld()->getRobotClosestToBall(world_new::them));
-    stpInfos["formation_0"].setPositionToMoveTo(Vector2{-2, -4});
-    stpInfos["formation_1"].setPositionToMoveTo(Vector2{-2, -3});
-    stpInfos["formation_2"].setPositionToMoveTo(Vector2{-2, -2});
-    stpInfos["formation_3"].setPositionToMoveTo(Vector2{-2, -1});
-    stpInfos["formation_4"].setPositionToMoveTo(Vector2{-2, 0});
-    stpInfos["formation_5"].setPositionToMoveTo(Vector2{-2, 1});
-    stpInfos["formation_6"].setPositionToMoveTo(Vector2{-2, 2});
-    stpInfos["formation_7"].setPositionToMoveTo(Vector2{-2, 3});
-    stpInfos["formation_8"].setPositionToMoveTo(Vector2{-2, 4});
-    stpInfos["formation_9"].setPositionToMoveTo(Vector2{-1.5, 0});
+    // TODO: these positions might need to change in the future
+    stpInfos["keeper"].setPositionToMoveTo(field.getOurGoalCenter());
+    stpInfos["keeper"].setPositionToShootAt(Vector2{-2, -4});
 }
 
 bool PenaltyThem::shouldRoleSkipEndTactic() { return false; }
