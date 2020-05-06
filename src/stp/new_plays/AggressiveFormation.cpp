4075c99612ff9ff117fb0541a3929e8b559c1924
Timovdk <timo_kuil@hotmail.com>
2020-05-06 10:53:38 +0200

    Formation is during stop game state for now, probably will change

--- src/stp/new_plays/AggressiveFormation.cpp
+++ src/stp/new_plays/AggressiveFormation.cpp
@@ -4,19 +4,17 @@
 
 #include "stp/new_plays/AggressiveFormation.h"
 
-#include "stp/invariants/BallGotShotInvariant.h"
+#include "stp/invariants/game_states/StopGameStateInvariant.h"
 #include "stp/new_roles/Formation.h"
 
 namespace rtt::ai::stp::play {
 
 AggressiveFormation::AggressiveFormation() : Play() {
-    // TODO: decide start invariants
     startPlayInvariants.clear();
-    startPlayInvariants.emplace_back(std::make_unique<invariant::BallGotShotInvariant>());
+    startPlayInvariants.emplace_back(std::make_unique<invariant::StopGameStateInvariant>());
 
-    // TODO: decide keep invariants
-/*    keepPlayInvariants.clear();
-    keepPlayInvariants.emplace_back(std::make_unique<invariant::WeHaveBallInvariant>());*/
+    keepPlayInvariants.clear();
+    keepPlayInvariants.emplace_back(std::make_unique<invariant::StopGameStateInvariant>());
 
     roles = std::array<std::unique_ptr<Role>, rtt::ai::Constants::ROBOT_COUNT()>{
         std::make_unique<role::Formation>(role::Formation("keeper")),      std::make_unique<role::Formation>(role::Formation("defender_0")),
@@ -31,43 +29,39 @@ uint8_t AggressiveFormation::score(world_new::World* world) noexcept { return 2;
 
 void AggressiveFormation::calculateInfoForRoles() noexcept {
     // TODO: TUNE these positions could probably be a bit better once we decide how we want to play
-    if (stpInfos.find("keeper") != stpInfos.end()) stpInfos["keeper"].setPositionToMoveTo(Vector2{-4.5, 0});
-    if (stpInfos.find("defender_0") != stpInfos.end()) stpInfos["defender_0"].setPositionToMoveTo(Vector2{-3, 3});
-    if (stpInfos.find("defender_1") != stpInfos.end()) stpInfos["defender_1"].setPositionToMoveTo(Vector2{-3, 0});
-    if (stpInfos.find("defender_2") != stpInfos.end()) stpInfos["defender_2"].setPositionToMoveTo(Vector2{-3, -3});
-    if (stpInfos.find("mid_field_0") != stpInfos.end()) stpInfos["mid_field_0"].setPositionToMoveTo(Vector2{-2, 3});
-    if (stpInfos.find("mid_field_1") != stpInfos.end()) stpInfos["mid_field_1"].setPositionToMoveTo(Vector2{-2, 0});
-    if (stpInfos.find("mid_field_2") != stpInfos.end()) stpInfos["mid_field_2"].setPositionToMoveTo(Vector2{-2, -3});
-    if (stpInfos.find("offender_0") != stpInfos.end()) stpInfos["offender_0"].setPositionToMoveTo(Vector2{-1, 4});
-    if (stpInfos.find("offender_1") != stpInfos.end()) stpInfos["offender_1"].setPositionToMoveTo(Vector2{-1, 1.5});
-    if (stpInfos.find("offender_2") != stpInfos.end()) stpInfos["offender_2"].setPositionToMoveTo(Vector2{-1, -1.5});
-    if (stpInfos.find("offender_3") != stpInfos.end()) stpInfos["offender_3"].setPositionToMoveTo(Vector2{-1, -4});
+    stpInfos["keeper"].setPositionToMoveTo(Vector2{-4.5, 0});
+    stpInfos["defender_0"].setPositionToMoveTo(Vector2{-3, 3});
+    stpInfos["defender_1"].setPositionToMoveTo(Vector2{-3, 0});
+    stpInfos["defender_2"].setPositionToMoveTo(Vector2{-3, -3});
+    stpInfos["mid_field_0"].setPositionToMoveTo(Vector2{-2, 3});
+    stpInfos["mid_field_1"].setPositionToMoveTo(Vector2{-2, 0});
+    stpInfos["mid_field_2"].setPositionToMoveTo(Vector2{-2, -3});
+    stpInfos["offender_0"].setPositionToMoveTo(Vector2{-1, 4});
+    stpInfos["offender_1"].setPositionToMoveTo(Vector2{-1, 1.5});
+    stpInfos["offender_2"].setPositionToMoveTo(Vector2{-1, -1.5});
+    stpInfos["offender_3"].setPositionToMoveTo(Vector2{-1, -4});
 }
 
 bool AggressiveFormation::shouldRoleSkipEndTactic() { return false; }
 
 Dealer::FlagMap AggressiveFormation::decideRoleFlags() const noexcept {
     Dealer::FlagMap flagMap;
-    Dealer::DealerFlag closeToBallFlag(DealerFlagTitle::CLOSE_TO_BALL, DealerFlagPriority::HIGH_PRIORITY);
-    Dealer::DealerFlag closeToTheirGoalFlag(DealerFlagTitle::CLOSE_TO_THEIR_GOAL, DealerFlagPriority::MEDIUM_PRIORITY);
-    Dealer::DealerFlag notImportant(DealerFlagTitle::CLOSE_TO_OUR_GOAL, DealerFlagPriority::LOW_PRIORITY);
+    Dealer::DealerFlag keeperFlag(DealerFlagTitle::KEEPER, DealerFlagPriority::REQUIRED);
 
-    flagMap.insert({"keeper", {closeToBallFlag}});
-    flagMap.insert({"defender_0", {closeToTheirGoalFlag}});
-    flagMap.insert({"defender_1", {notImportant}});
-    flagMap.insert({"defender_2", {closeToTheirGoalFlag}});
-    flagMap.insert({"mid_field_0", {closeToTheirGoalFlag, closeToBallFlag}});
-    flagMap.insert({"mid_field_1", {closeToBallFlag}});
-    flagMap.insert({"mid_field_2", {closeToTheirGoalFlag}});
-    flagMap.insert({"offender_0", {closeToTheirGoalFlag, closeToBallFlag}});
-    flagMap.insert({"offender_1", {closeToBallFlag}});
-    flagMap.insert({"offender_2", {closeToTheirGoalFlag}});
-    flagMap.insert({"offender_3", {closeToTheirGoalFlag}});
+    flagMap.insert({"keeper", {keeperFlag}});
+    flagMap.insert({"defender_0", {}});
+    flagMap.insert({"defender_1", {}});
+    flagMap.insert({"defender_2", {}});
+    flagMap.insert({"mid_field_0", {}});
+    flagMap.insert({"mid_field_1", {}});
+    flagMap.insert({"mid_field_2", {}});
+    flagMap.insert({"offender_0", {}});
+    flagMap.insert({"offender_1", {}});
+    flagMap.insert({"offender_2", {}});
+    flagMap.insert({"offender_3", {}});
 
     return flagMap;
 }
 
-const char *AggressiveFormation::getName() {
-    return "Aggressive Formation";
-}
+const char* AggressiveFormation::getName() { return "Aggressive Formation"; }
 }  // namespace rtt::ai::stp::play
