815f9a693dee20656f9446d461ae90d2248ea32a
timovdk <timo_kuil@hotmail.com>
2020-05-06 17:31:39 +0200

    Feature/dealer positions refactor (#1075)
    * Dealer now uses positions to deal when available
    
    * Formatting
    
    * Added case for if a robot has an enemy robot
    
    * Added comments
    
    * Added comments
    
    * Update src/utilities/Dealer.cpp
    
    Co-authored-by: RobotJesse <55085110+RobotJesse@users.noreply.github.com>

--- include/roboteam_ai/utilities/Dealer.h
+++ include/roboteam_ai/utilities/Dealer.h
@@ -5,15 +5,19 @@
  * The purpose of Dealer is to go from pairs of (name -> dealerFlags) to pairs of (name -> robot id).
  * In other words, there is a requirement for each role in the tree, and Dealer maps the robot that fits those
  * requirements best to that role in the tree.
+ * The lower the score, the better.
  */
 
-#include <vector>
-#include <map>
+#include <stp/StpInfo.h>
+
 #include <iostream>
+#include <map>
+#include <vector>
+
 #include "gtest/gtest_prod.h"
+#include "world/Field.h"
 #include "world_new/views/RobotView.hpp"
 #include "world_new/views/WorldDataView.hpp"
-#include "world/Field.h"
 
 namespace rtt::ai {
 
@@ -21,58 +25,111 @@ namespace v = rtt::world_new::view;
 
 // Set up a struct for dealerflags. Set up a struct for dealerflags.
 enum class DealerFlagTitle {
-  CLOSE_TO_THEIR_GOAL,
-  CLOSE_TO_OUR_GOAL,
-  CLOSE_TO_BALL,
-  WITH_WORKING_BALL_SENSOR,
-  WITH_WORKING_DRIBBLER,
-  ROBOT_TYPE_50W,
-  ROBOT_TYPE_30W,
-  READY_TO_INTERCEPT_GOAL_SHOT,
-  KEEPER
+    CLOSE_TO_THEIR_GOAL,
+    CLOSE_TO_OUR_GOAL,
+    CLOSE_TO_BALL,
+    WITH_WORKING_BALL_SENSOR,
+    WITH_WORKING_DRIBBLER,
+    ROBOT_TYPE_50W,
+    ROBOT_TYPE_30W,
+    READY_TO_INTERCEPT_GOAL_SHOT,
+    KEEPER
 };
 
-enum class DealerFlagPriority {
-  LOW_PRIORITY,
-  MEDIUM_PRIORITY,
-  HIGH_PRIORITY,
-  REQUIRED
-};
+enum class DealerFlagPriority { LOW_PRIORITY, MEDIUM_PRIORITY, HIGH_PRIORITY, REQUIRED };
 
 class Dealer {
-  FRIEND_TEST(DealerTest, it_properly_distributes_robots);
-  FRIEND_TEST(DealerTest, the_score_factor_increases_with_priority);
+    FRIEND_TEST(DealerTest, it_properly_distributes_robots);
+    FRIEND_TEST(DealerTest, the_score_factor_increases_with_priority);
 
- public:
-  struct DealerFlag {
-    DealerFlagTitle title;
-    DealerFlagPriority priority;
-    explicit DealerFlag(DealerFlagTitle title, DealerFlagPriority priority);
-  };
-  using FlagMap = std::map<std::string, std::vector<DealerFlag>>;
-  Dealer(v::WorldDataView world, world::Field * field);
-  virtual ~Dealer() = default; // needed for test
-  // Create a distribution of robots
-  std::unordered_map<std::string, v::RobotView> distribute(const std::vector<v::RobotView> &allRobots, const FlagMap &flagMap);
+   public:
+    struct DealerFlag {
+        DealerFlagTitle title;
+        DealerFlagPriority priority;
+        explicit DealerFlag(DealerFlagTitle title, DealerFlagPriority priority);
+    };
+    using FlagMap = std::map<std::string, std::vector<DealerFlag>>;
+    Dealer(v::WorldDataView world, world::Field *field);
+    virtual ~Dealer() = default;  // needed for test
+    // Create a distribution of robots
+    std::unordered_map<std::string, v::RobotView> distribute(const std::vector<v::RobotView> &allRobots, const FlagMap &flagMap,
+                                                             const std::unordered_map<std::string, stp::StpInfo> &stpInfoMap);
 
- protected:
-  // This function is virtual such that it can be mocked in the tests.
-  // the performance hit is minimal (in the scope of nanoseconds)
-  virtual double getDefaultFlagScores(const v::RobotView &robot, const DealerFlag &flag);
+   protected:
+    // This function is virtual such that it can be mocked in the tests.
+    // the performance hit is minimal (in the scope of nanoseconds)
+    virtual double getDefaultFlagScores(const v::RobotView &robot, const DealerFlag &flag);
 
- private:
-  v::WorldDataView world;
-  world::Field * field;
+   private:
+    v::WorldDataView world;
+    world::Field *field;
 
-  double getScoreForFlag(v::RobotView robot, DealerFlag flag);
-  std::vector<std::vector<double>> getScoreMatrix(const std::vector<v::RobotView> &allRobots, const FlagMap &flagMap);
-  static double getFactorForPriority(const DealerFlag &flag);
-  static double costForDistance(double distance, double fieldWidth, double fieldHeight);
-  static double costForProperty(bool property);
-  double scoreForFlags(const std::vector<Dealer::DealerFlag> &dealerFlags, const v::RobotView &robot);
-  std::unordered_map<std::string, v::RobotView> mapFromAssignments(const std::vector<v::RobotView> &allRobots,
-                                                            const FlagMap &flagMap,
-                                                            const std::vector<int> &assignment) const;
+    /**
+     * Calculates the score for a flag by multiplying the factor and score
+     * The factor is based on the priority and the score is based on the trueness of a property
+     * @param robot
+     * @param flag
+     * @return Score for a flag
+     */
+    double getScoreForFlag(v::RobotView robot, DealerFlag flag);
+
+    /**
+     * Calculates the score for a distance between a point and a robot
+     * @param stpInfo
+     * @param robot
+     * @return Score for distance
+     */
+    double getScoreForDistance(const stp::StpInfo &stpInfo, const v::RobotView &robot);
+
+    /**
+     * Populates the matrix of robots and roles with scores based on flags and distance
+     * @param allRobots
+     * @param flagMap
+     * @param stpInfoMap
+     * @return The score matrix
+     */
+    std::vector<std::vector<double>> getScoreMatrix(const std::vector<v::RobotView> &allRobots, const FlagMap &flagMap,
+                                                    const std::unordered_map<std::string, stp::StpInfo> &stpInfoMap);
+
+    /**
+     * Translates a priority into a double
+     * @param flag
+     * @return priority factor
+     */
+    static double getFactorForPriority(const DealerFlag &flag);
+
+    /**
+     * Calculates the cost of travelling a certain distance
+     * @param distance  the distance to travel
+     * @param fieldWidth
+     * @param fieldHeight
+     * @return cost of travelling that distance
+     */
+    static double costForDistance(double distance, double fieldWidth, double fieldHeight);
+
+    /**
+     * Calculates the cost of a property (think of isWorkingDribbler())
+     * @param property
+     * @return cost of a property
+     */
+    static double costForProperty(bool property);
+
+    /**
+     * Calculates the score for all flags for a role, for one robot (so will be called 11 times for each role)
+     * @param dealerFlags
+     * @param robot
+     * @return the score of all flags combined
+     */
+    double scoreForFlags(const std::vector<Dealer::DealerFlag> &dealerFlags, const v::RobotView &robot);
+
+    /**
+     * Makes a mapping from roleName to a robot using the result of the hungarian
+     * @param allRobots
+     * @param flagMap
+     * @param assignment
+     * @return Returns the mapping
+     */
+    std::unordered_map<std::string, v::RobotView> mapFromAssignments(const std::vector<v::RobotView> &allRobots, const FlagMap &flagMap, const std::vector<int> &assignment) const;
 };
 }  // namespace rtt::ai
 #endif  // RTT_ROBOTEAM_AI_SRC_UTILITIES_DEALER_H_
