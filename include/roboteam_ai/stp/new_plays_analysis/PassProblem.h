f31c9b1191aff37fe59bf9e910a5505ac4cfb899
RobotJesse <55085110+RobotJesse@users.noreply.github.com>
2020-05-08 09:14:23 +0200

    Apply suggestions from code review

--- include/roboteam_ai/stp/new_plays_analysis/PassProblem.h
+++ include/roboteam_ai/stp/new_plays_analysis/PassProblem.h
@@ -25,6 +25,7 @@ namespace rtt::ai::stp {
         world::Field problemField;
 
         static double shootSuccesReward(Vector2 point, world_new::view::WorldDataView world, const world::Field& field);
+        
     public:
         /// Pagmo bounds function, only used for pagmo, don't change the signature as Pagmo algo's expect this exact function
         std::pair<vector_double, vector_double> get_bounds() const;
@@ -37,9 +38,7 @@ namespace rtt::ai::stp {
 
         // TODO: when this function is finished, refactor so it only uses those components of world it actually needs
         void updateInfoForProblem(world_new::WorldData problemWorld, world::Field& field);
-
         PassProblem() = default;
-
     };
 }
 #endif //RTT_PASSPROBLEM_H
