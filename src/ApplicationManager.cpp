77a8aa51ca88eb1a546c82d7887e836ff67b40f4
Jordi <jordiluong@gmail.com>
2020-05-11 15:12:23 +0200

    Add stub for get ball possession play

--- src/ApplicationManager.cpp
+++ src/ApplicationManager.cpp
@@ -26,6 +26,7 @@
 #include "stp/new_plays/KickOffThemPrepare.h"
 #include "stp/new_plays/KickOffUs.h"
 #include "stp/new_plays/KickOffThem.h"
+#include "stp/new_plays/GetBallPossession.h"
 
 namespace io = rtt::ai::io;
 namespace ai = rtt::ai;
@@ -59,6 +60,7 @@ void ApplicationManager::start() {
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffThemPrepare>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffUs>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffThem>());
+    plays.emplace_back(std::make_unique<rtt::ai::stp::play::GetBallPossession>());
     playChecker.setPlays(plays);
 
     int amountOfCycles = 0;
