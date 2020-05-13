c12d98a817b702069519078088ab9da21581fb29
Jordi <jordiluong@gmail.com>
2020-05-13 11:54:25 +0200

    Add TODO

--- src/stp/invariants/FirstArrivalToBallInvariant.cpp
+++ src/stp/invariants/FirstArrivalToBallInvariant.cpp
@@ -7,6 +7,7 @@
 namespace rtt::ai::stp::invariant {
 
 uint8_t FirstArrivalToBallInvariant::metricCheck(world_new::view::WorldDataView world, const world::Field *field) const noexcept {
+    // TODO: Improve by taking into account velocity and direction of the robots to calculate ETA
     const auto closestTeam = world.getRobotClosestToBall()->getTeam();
     return closestTeam == world_new::us ? control_constants::FUZZY_TRUE : control_constants::FUZZY_FALSE;
 }
