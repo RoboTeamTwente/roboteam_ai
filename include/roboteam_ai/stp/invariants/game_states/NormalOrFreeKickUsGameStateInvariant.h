8f668a4065a1b60db0d7407c5651d60d1b009447
Ratoone <vacariu.paul@gmail.com>
2020-05-15 14:17:34 +0200

    Modified Attack, Defend and GetBall to include the FreeKickUs game state

---
+++ include/roboteam_ai/stp/invariants/game_states/NormalOrFreeKickUsGameStateInvariant.h
@@ -0,0 +1,18 @@
+//
+// Created by ratoone on 15-05-20.
+//
+
+#ifndef RTT_NORMALORFREEKICKUSGAMESTATEINVARIANT_H
+#define RTT_NORMALORFREEKICKUSGAMESTATEINVARIANT_H
+
+#include "stp/invariants/BaseInvariant.h"
+
+namespace rtt::ai::stp::invariant {
+
+class NormalOrFreeKickUsGameStateInvariant : public BaseInvariant {
+public:
+    [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const world::Field *field) const noexcept override;
+};
+}
+
+#endif //RTT_NORMALORFREEKICKUSGAMESTATEINVARIANT_H
