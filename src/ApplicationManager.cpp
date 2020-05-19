b865d16d2d4a890fd2415691b418e14bb671f9e0
timovdk <timo_kuil@hotmail.com>
2020-05-19 10:13:11 +0200

    Feature/risky get ball (#1116)
    * Added get ball risky
    
    * Improvements
    
    * Update src/stp/new_plays/GetBallRisky.cpp
    
    * Update src/stp/new_plays/GetBallRisky.cpp

--- src/ApplicationManager.cpp
+++ src/ApplicationManager.cpp
@@ -28,6 +28,7 @@
 #include "stp/new_plays/KickOffUs.h"
 #include "stp/new_plays/KickOffThem.h"
 #include "stp/new_plays/GetBallPossession.h"
+#include "stp/new_plays/GetBallRisky.h"
 
 namespace io = rtt::ai::io;
 namespace ai = rtt::ai;
@@ -63,6 +64,7 @@ void ApplicationManager::start() {
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffUs>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffThem>());
     plays.emplace_back(std::make_unique<rtt::ai::stp::play::GetBallPossession>());
+    plays.emplace_back(std::make_unique<rtt::ai::stp::play::GetBallRisky>());
     playChecker.setPlays(plays);
 
     int amountOfCycles = 0;
