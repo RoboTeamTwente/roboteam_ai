1f79c19b96f364fa51f4ba41846d1e45669ab299
Timovdk <timo_kuil@hotmail.com>
2020-05-20 10:16:48 +0200

    Initial commit

---
+++ src/stp/new_plays/GenericPass.cpp
@@ -0,0 +1,51 @@
+//
+// Created by timovdk on 5/20/20.
+//
+
+#include "stp/invariants/game_states/HaltGameStateInvariant.h"
+#include "stp/new_plays/GenericPass.h"
+#include "stp/new_roles/Halt.h"
+
+namespace rtt::ai::stp::play {
+
+GenericPass::GenericPass() : Play() {
+    startPlayInvariants.clear();
+    startPlayInvariants.emplace_back(std::make_unique<invariant::HaltGameStateInvariant>());
+
+    keepPlayInvariants.clear();
+    keepPlayInvariants.emplace_back(std::make_unique<invariant::HaltGameStateInvariant>());
+
+    roles = std::array<std::unique_ptr<Role>, rtt::ai::Constants::ROBOT_COUNT()>{
+        std::make_unique<role::Halt>(role::Halt("halt_0")), std::make_unique<role::Halt>(role::Halt("halt_1")), std::make_unique<role::Halt>(role::Halt("halt_2")),
+        std::make_unique<role::Halt>(role::Halt("halt_3")), std::make_unique<role::Halt>(role::Halt("halt_4")), std::make_unique<role::Halt>(role::Halt("halt_5")),
+        std::make_unique<role::Halt>(role::Halt("halt_6")), std::make_unique<role::Halt>(role::Halt("halt_7")), std::make_unique<role::Halt>(role::Halt("halt_8")),
+        std::make_unique<role::Halt>(role::Halt("halt_9")), std::make_unique<role::Halt>(role::Halt("halt_10"))};
+}
+
+uint8_t GenericPass::score(world_new::World* world) noexcept { return 50; }
+
+void GenericPass::calculateInfoForRoles() noexcept {}
+
+bool GenericPass::shouldRoleSkipEndTactic() { return false; }
+
+Dealer::FlagMap GenericPass::decideRoleFlags() const noexcept {
+    Dealer::FlagMap flagMap;
+    Dealer::DealerFlag not_important(DealerFlagTitle::ROBOT_TYPE_50W, DealerFlagPriority::LOW_PRIORITY);
+
+    flagMap.insert({"halt_0", {not_important}});
+    flagMap.insert({"halt_1", {not_important}});
+    flagMap.insert({"halt_2", {not_important}});
+    flagMap.insert({"halt_3", {not_important}});
+    flagMap.insert({"halt_4", {not_important}});
+    flagMap.insert({"halt_5", {not_important}});
+    flagMap.insert({"halt_6", {not_important}});
+    flagMap.insert({"halt_7", {not_important}});
+    flagMap.insert({"halt_8", {not_important}});
+    flagMap.insert({"halt_9", {not_important}});
+    flagMap.insert({"halt_10", {not_important}});
+    return flagMap;
+}
+
+const char* GenericPass::getName() { return "Generic Pass"; }
+
+}  // namespace rtt::ai::stp::play
