f7fcdf1037cf8f65333c733169390a753b7b39e4
Timovdk <timo_kuil@hotmail.com>
2020-07-07 15:46:48 +0200

    Fixed BallClosestToUsInvariant

--- include/roboteam_ai/stp/invariants/BallClosestToUsInvariant.h
+++ include/roboteam_ai/stp/invariants/BallClosestToUsInvariant.h
@@ -5,37 +5,15 @@
 #ifndef RTT_BALLCLOSESTTOUSINVARIANT_H
 #define RTT_BALLCLOSESTTOUSINVARIANT_H
 
-
-#include <NFParam/Param.h>
 #include "BaseInvariant.h"
 
 namespace rtt::ai::stp::invariant {
 class BallClosestToUsInvariant : public BaseInvariant {
-    public:
-        BallClosestToUsInvariant() noexcept;
+   public:
+    [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const world::Field* field) const noexcept override;
 
-        [[nodiscard]] uint8_t metricCheck(world_new::view::WorldDataView world, const world::Field* field) const noexcept override;
-
-        const char* getName() override
-        {
-            return "BallCloseToUs";
-        }
-
-    private:
-        /**
-         * Calculates the actual metric value using the piecewise linear function member
-         * @param x the x of the function
-         * @return metric value between 0-255
-         */
-        [[nodiscard]] uint8_t calculateMetric(const double& x) const noexcept;
-
-        /**
-         * Unique pointer to the piecewise linear function that calculates the fuzzy value
-         */
-        std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction;
+    const char* getName() override { return "BallClosestToUs"; }
 };
 }  // namespace rtt::ai::stp::invariant
 
-
-#endif //RTT_BALLCLOSESTTOUSINVARIANT_H
-
+#endif  // RTT_BALLCLOSESTTOUSINVARIANT_H
