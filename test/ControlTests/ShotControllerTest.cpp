a097d68d2e0d8d18626fc0813994ab1d841b7e98
Emiel Steerneman <emielsteerneman@gmail.com>
2020-10-22 20:14:05 +0200

    Removed all occurences of geneva

--- test/ControlTests/ShotControllerTest.cpp
+++ test/ControlTests/ShotControllerTest.cpp
@@ -36,7 +36,6 @@ TEST(ShotControllerTest, it_generates_robotcommands) {
     EXPECT_EQ(command.kicker_forced, sd.kicker);
     EXPECT_EQ(command.chipper_forced, sd.kicker);
     EXPECT_DOUBLE_EQ(command.kicker_vel, sd.kickerVel);
-    EXPECT_EQ(command.geneva_state, sd.geneva);
 }
 
 TEST(ShotControllerTest, it_calculates_kickforce) {
@@ -85,33 +84,6 @@ TEST(ShotControllerTest, it_locates_robots_properly) {
     rtt::world::world->updateWorld(world);
     ShotController shotController;
 
-    // check if the correct positions behind the ball are found
-    // the robot is at -1, 0 and the ball is at 0,0 and the shottarget is 1,0
-    Vector2 loc = shotController.getPlaceBehindBallForGenevaState(world::Robot(robot), {1, 0}, 1);
-    Vector2 ballPos = ball.pos;
-    EXPECT_FLOAT_EQ(loc.length(), 2.0 * Constants::ROBOT_RADIUS() + Constants::BALL_RADIUS());
-    EXPECT_FLOAT_EQ((ballPos - loc).angle(), toRadians(20));
-
-    loc = shotController.getPlaceBehindBallForGenevaState(world::Robot(robot), {1, 0}, 2);
-    EXPECT_FLOAT_EQ(loc.length(), 2.0 * Constants::ROBOT_RADIUS() + Constants::BALL_RADIUS());
-    EXPECT_FLOAT_EQ((ballPos - loc).angle(), toRadians(10));
-
-    loc = shotController.getPlaceBehindBallForGenevaState(world::Robot(robot), {1, 0}, 3);
-    EXPECT_FLOAT_EQ(loc.length(), 2.0 * Constants::ROBOT_RADIUS() + Constants::BALL_RADIUS());
-    EXPECT_FLOAT_EQ((ballPos - loc).angle(), toRadians(0));
-
-    loc = shotController.getPlaceBehindBallForGenevaState(world::Robot(robot), {1, 0}, 4);
-    EXPECT_FLOAT_EQ(loc.length(), 2.0 * Constants::ROBOT_RADIUS() + Constants::BALL_RADIUS());
-    EXPECT_FLOAT_EQ((ballPos - loc).angle(), toRadians(-10));
-
-    loc = shotController.getPlaceBehindBallForGenevaState(world::Robot(robot), {1, 0}, 5);
-    EXPECT_FLOAT_EQ(loc.length(), 2.0 * Constants::ROBOT_RADIUS() + Constants::BALL_RADIUS());
-    EXPECT_FLOAT_EQ((ballPos - loc).angle(), toRadians(-20));
-
-    // the shotcontroller can determine a proper geneva state
-    EXPECT_EQ(shotController.determineOptimalGenevaState(world::Robot(robot), {1, 0}), 3);
-    EXPECT_EQ(shotController.determineOptimalGenevaState(world::Robot(robot), {-1, 1}), 4);
-    EXPECT_EQ(shotController.determineOptimalGenevaState(world::Robot(robot), {-1, -1}), 2);
 }
 
 TEST(ShotControllerTest, it_sends_proper_shoot_commands) {
@@ -164,9 +136,6 @@ TEST(ShotControllerTest, getshotdata_test) {
     auto worldRobotPair = testhelpers::WorldHelper::getWorldMsgWhereRobotHasBall(1, 0, true, field);
     int robotWithBallId = worldRobotPair.second;
     auto robotWithBall = world::world->getRobotForId(robotWithBallId, true);
-    robotWithBall->setGenevaState(3);
-    robotWithBall->setWorkingGeneva(true);
-    robotWithBall->setTimeToChangeOneGenevaState(-1);
     rtt::world::world->updateWorld(worldRobotPair.first);
     Vector2 shotVector = world::world->getBall()->pos - robotWithBall->pos;
     Vector2 simulatedShotTarget = world::world->getBall()->pos + shotVector.stretchToLength(1.0);
@@ -180,82 +149,13 @@ TEST(ShotControllerTest, getshotdata_test) {
     EXPECT_TRUE(shotdata.kicker);
     EXPECT_FALSE(shotdata.chipper);
     EXPECT_FLOAT_EQ(shotdata.angle, (world::world->getBall()->pos - robotWithBall->pos).toAngle());
-    EXPECT_FLOAT_EQ(shotdata.geneva, 3);
 
     // chip test
     shotdata = shotController.getRobotCommand(*robotWithBall, simulatedShotTarget, true, BallSpeed::MAX_SPEED, false, ShotPrecision::LOW);
     EXPECT_FALSE(shotdata.kicker);
     EXPECT_TRUE(shotdata.chipper);
     EXPECT_FLOAT_EQ(shotdata.angle, (world::world->getBall()->pos - robotWithBall->pos).toAngle());
-    EXPECT_FLOAT_EQ(shotdata.geneva, 3);
-
-    /*
-     *
-     * When the autogeneva is on we should test some things a lot -> in a loop
-     * when we chip the geneva from the outgoing command should ALWAYS be 3
-     * We alternate the target position to see the difference in behaviour
-     */
-    for (int i = 0; i < 100; i++) {
-        auto world = testhelpers::WorldHelper::getWorldMsg(1, 0, true, field);
-        rtt::world::world->updateWorld(world);
-        auto robot = world::world->getUs().at(0);
-        robot->setGenevaState(3);
-        robot->setWorkingGeneva(true);
-        robot->setTimeToChangeOneGenevaState(-1);
-
-        shotdata = shotController.getRobotCommand(*robot, testhelpers::WorldHelper::getRandomFieldPosition(field), true, BallSpeed::MAX_SPEED, true, ShotPrecision::HIGH);
-        EXPECT_FALSE(shotdata.kicker);
-        EXPECT_FLOAT_EQ(shotdata.geneva, 3);
-    }
-
-    /*
-     * we use autogeneva and kick
-     * The geneva state should always stay between 0 and 5
-     */
-    for (int i = 0; i < 100; i++) {
-        auto world = testhelpers::WorldHelper::getWorldMsg(1, 0, true, field);
-        rtt::world::world->updateWorld(world);
-        auto robot = world::world->getUs().at(0);
-        robot->setGenevaState(3);
-        robot->setWorkingGeneva(true);
-        robot->setTimeToChangeOneGenevaState(-1);
-
-        shotdata = shotController.getRobotCommand(*robot, testhelpers::WorldHelper::getRandomFieldPosition(field), false, BallSpeed::MAX_SPEED, true, ShotPrecision::HIGH);
-        EXPECT_GE(shotdata.geneva, 0);
-        EXPECT_LE(shotdata.geneva, 5);
-    }
-
-    /*
-     * If the robot has no geneva it should ALWAYS keep its initial state
-     */
-    for (int i = 0; i < 100; i++) {
-        auto world = testhelpers::WorldHelper::getWorldMsg(1, 0, true, field);
-        rtt::world::world->updateWorld(world);
-        auto robot = world::world->getUs().at(0);
-        robot->setGenevaState(4);
-        robot->setWorkingGeneva(false);
-        robot->setTimeToChangeOneGenevaState(-1);
-
-        shotdata = shotController.getRobotCommand(*robot, testhelpers::WorldHelper::getRandomFieldPosition(field), false, BallSpeed::MAX_SPEED, true, ShotPrecision::HIGH);
-        EXPECT_EQ(shotdata.geneva, 4);
-    }
-}
-
-TEST(ShotControllerTest, geneva_turning) {
-    //    ShotController shotController;
-    //    shotController.setGenevaDelay(2);
-    //    EXPECT_FLOAT_EQ(shotController.secondsToTurnGeneva, 0.8);
-    //    EXPECT_TRUE(shotController.genevaIsTurning);
-    //
-    //    ShotController shotController2;
-    //    shotController2.setGenevaDelay(0);
-    //    EXPECT_FLOAT_EQ(shotController2.secondsToTurnGeneva, 0);
-    //    EXPECT_FALSE(shotController2.genevaIsTurning);
-    //
-    //    ShotController shotController3;
-    //    shotController3.setGenevaDelay(4);
-    //    EXPECT_FLOAT_EQ(shotController3.secondsToTurnGeneva, 1.6);
-    //    EXPECT_TRUE(shotController3.genevaIsTurning);
+-
 }
 
 }  // namespace control
