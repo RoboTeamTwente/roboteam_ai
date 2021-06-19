504cb307c3d5847cbf18c119fc1a13f2a24e75fe
rolfvdhulst <rolfvdhulst@gmail.com>
2021-06-19 15:23:35 +0200

    Fix robots driving into defense area before kickoff

--- src/stp/plays/referee_specific/KickOffThemPrepare.cpp
+++ src/stp/plays/referee_specific/KickOffThemPrepare.cpp
@@ -44,17 +44,18 @@ namespace rtt::ai::stp::play {
         stpInfos["keeper"].setPositionToMoveTo(Vector2(field.getOurGoalCenter() + Vector2(0.5, 0.0)));
         stpInfos["keeper"].setEnemyRobot(world->getWorld()->getRobotClosestToBall(world::them));
 
+        double defense_line_x = field.getLeftPenaltyX()+0.15;
         // regular bots
         stpInfos["formation_0"].setPositionToMoveTo(Vector2(-length / 4, width / 8));
         stpInfos["formation_1"].setPositionToMoveTo(Vector2(-length / 4, -width / 8));
         stpInfos["formation_2"].setPositionToMoveTo(Vector2(-length / 8, width / 4));
         stpInfos["formation_3"].setPositionToMoveTo(Vector2(-length / 8, -width / 4));
-        stpInfos["formation_4"].setPositionToMoveTo(Vector2(-length * 3 / 8, 0.0));
-        stpInfos["formation_5"].setPositionToMoveTo(Vector2(-length * 3 / 8, width / 5));
-        stpInfos["formation_6"].setPositionToMoveTo(Vector2(-length * 3 / 8, -width / 5));
+        stpInfos["formation_4"].setPositionToMoveTo(Vector2(defense_line_x, 0.0));
+        stpInfos["formation_5"].setPositionToMoveTo(Vector2(defense_line_x, width / 5));
+        stpInfos["formation_6"].setPositionToMoveTo(Vector2(defense_line_x, -width / 5));
         stpInfos["formation_7"].setPositionToMoveTo(Vector2(-length / 4, width / 3));
         stpInfos["formation_8"].setPositionToMoveTo(Vector2(-length / 4, -width / 3));
-        stpInfos["formation_9"].setPositionToMoveTo(Vector2(-length / 4, -width / 3));
+        stpInfos["formation_9"].setPositionToMoveTo(Vector2(-length * 3.0 / 16.0, 0.0);
     }
 
     Dealer::FlagMap KickOffThemPrepare::decideRoleFlags() const noexcept {
