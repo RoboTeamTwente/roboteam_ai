13d2978757d6f0da4f261f21cfedd20f0f6ea30e
j.a.kuiken@student.utwente.nl <05SwC4cgVrI7kT9tavp1>
2021-06-20 22:23:47 +0200

    Change max velocity for RoboCup

--- src/stp/constants/ControlConstants.cpp
+++ src/stp/constants/ControlConstants.cpp
@@ -38,7 +38,7 @@ constexpr double ENEMY_CLOSE_TO_BALL_DISTANCE = 1.0;
 
 /// RobotCommand limits
 // TODO: for testing, this is set to 1.89!
-constexpr double MAX_VEL_CMD = 1.891;
+constexpr double MAX_VEL_CMD = 8;
 constexpr double MAX_DRIBBLER_CMD = 31;
 // Angle increment per tick
 constexpr double ANGLE_RATE = 0.1 * M_PI;
