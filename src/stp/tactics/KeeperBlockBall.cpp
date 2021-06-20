3a196e108e5279e3d29e3b52e9aa463b04b35343
j.a.kuiken@student.utwente.nl <05SwC4cgVrI7kT9tavp1>
2021-06-20 17:32:12 +0200

    Fix getWallPosition segfault & let keeper wait longer until it intercepts the ball when it moves towards the goal

--- src/stp/tactics/KeeperBlockBall.cpp
+++ src/stp/tactics/KeeperBlockBall.cpp
@@ -58,7 +58,7 @@ std::pair<Vector2, stp::PIDType> KeeperBlockBall::calculateTargetPosition(const 
         // Intercept ball when it is moving towards the goal
         if (ball->getVelocity().length() > control_constants::BALL_STILL_VEL) {
             auto start = ball->getPos();
-            auto end = start + ball->getVelocity().stretchToLength(field.getFieldLength() * 0.5);
+            auto end = start + ball->getVelocity().stretchToLength(field.getFieldLength() * 0.2);
             auto startGoal = field.getOurTopGoalSide() + Vector2(control_constants::DISTANCE_FROM_GOAL_CLOSE, 0);
             auto endGoal = field.getOurBottomGoalSide() + Vector2(control_constants::DISTANCE_FROM_GOAL_CLOSE, 0);
 
