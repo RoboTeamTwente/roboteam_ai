f547d0625df30adc68120facee8a897b92d0d996
rtt-vision <management@roboteamtwente.nl>
2021-10-20 11:17:52 +0200

    Change kicked condition to !hasBall instead of speed

--- src/stp/skills/Kick.cpp
+++ src/stp/skills/Kick.cpp
@@ -35,7 +35,8 @@ Status Kick::onUpdate(const StpInfo &info) noexcept {
     // forward the generated command to the ControlModule, for checking and limiting
     forwardRobotCommand(info.getCurrentWorld());
 
-    if (info.getBall()->get()->getVelocity().length() > stp::control_constants::HAS_KICKED_ERROR_MARGIN) {
+    if (!info.getRobot()->hasBall()) {
+        RTT_DEBUG("Kicked");
         kickAttempts = 0;
         return Status::Success;
     }
