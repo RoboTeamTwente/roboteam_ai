227bb460647a953adac2a833c934d4f38aa1d52c
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2021-10-27 15:27:02 +0200

    Comment out TestPlay in application manager and note this in TestPlay
    TestPlay should not be available as a play during regular play. To avoid having this happen, this play is commented out in the application manager and can not be selected. A comment in application manager and testPlay itself point to this to make sure people know what to do to enable the testplay for testing.

--- src/stp/plays/TestPlay.cpp
+++ src/stp/plays/TestPlay.cpp
@@ -2,6 +2,8 @@
 // Created by timovdk on 3/10/20.
 //
 
+///This play is for testing purposes, to use it, be sure to uncomment in in ApplicationManager.cpp's start function!
+
 #include "stp/plays/TestPlay.h"
 
 #include "stp/roles/TestRole.h"
