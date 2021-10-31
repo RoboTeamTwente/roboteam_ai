0d8f22c9effa873bbd358695b501cd81a16aefe6
Florisheinen1 <Florisheinen1@users.noreply.github.com>
2021-10-31 10:41:26 +0000

    Merge pull request #1259 from RoboTeamTwente/fix/remove-ci-config
    Remove Travis CI configuration file 65353c537be03aa324ad1377f33259afbe89a143

--- include/roboteam_ai/stp/tactics/TestTactic.h
+++ include/roboteam_ai/stp/tactics/TestTactic.h
@@ -40,8 +40,6 @@ class TestTactic : public Tactic {
      */
     bool shouldTacticReset(const StpInfo &info) noexcept override;
 
-    bool forceTacticSuccess(const StpInfo &info) noexcept override;;
-
     /**
      * Is this tactic an end tactic?
      * @return This will always return false, since it is NOT an endTactic
@@ -53,7 +51,6 @@ class TestTactic : public Tactic {
      * @return The name of this tactic
      */
     const char *getName() override;
-
 };
 }  // namespace rtt::ai::stp
 
