df866df1049354ce9dabdce2f812f000bb2d4efe
GagataS <a.sowa@roboteamtwente.nl>
2021-12-22 13:43:23 +0100

    Reverted changes in TestPlay and TestRole

--- src/stp/roles/TestRole.cpp
+++ src/stp/roles/TestRole.cpp
@@ -3,14 +3,12 @@
 //
 
 #include "stp/roles/TestRole.h"
-
 #include "stp/tactics/TestTactic.h"
-#include "stp/tactics/passive/BlockRobot.h"
 
 namespace rtt::ai::stp {
 
 TestRole::TestRole(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::BlockRobot()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{TestTactic()};
 }
 }  // namespace rtt::ai::stp
