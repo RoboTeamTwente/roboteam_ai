623bec93a8a3b8713d364e821dac2a55cd326552
Florisheinen1 <Florisheinen1@users.noreply.github.com>
2021-12-29 12:42:36 +0000

    deploy: 5d94ebc352ddac2e44b731beb2458ca029053c6a

--- src/stp/tactics/GetBehindBallInDirection.cpp
+++ src/stp/tactics/GetBehindBallInDirection.cpp
@@ -1,15 +1,18 @@
 //
 // Created by jordi on 06-04-20.
+// Rebuilt by alexander on 21-12-21
 //
 #include "stp/tactics/GetBehindBallInDirection.h"
 
 #include <roboteam_utils/LineSegment.h>
 
+#include "roboteam_utils/Circle.h"
 #include "stp/skills/GoToPos.h"
-#include "stp/skills/Rotate.h"
+#include "stp/skills/Orbit.h"
 
 namespace rtt::ai::stp::tactic {
-GetBehindBallInDirection::GetBehindBallInDirection() { skills = collections::state_machine<Skill, Status, StpInfo>{skill::GoToPos(), skill::Rotate(), skill::GoToPos()}; }
+
+GetBehindBallInDirection::GetBehindBallInDirection() { skills = collections::state_machine<Skill, Status, StpInfo>{skill::GoToPos(), skill::Orbit()}; }
 
 std::optional<StpInfo> GetBehindBallInDirection::calculateInfoForSkill(StpInfo const &info) noexcept {
     StpInfo skillStpInfo = info;
@@ -18,49 +21,47 @@ std::optional<StpInfo> GetBehindBallInDirection::calculateInfoForSkill(StpInfo c
         return std::nullopt;
     }
 
-    Vector2 robotPosition = info.getRobot().value()->getPos();
-    Vector2 ballPosition = info.getBall().value()->getPos();
-    Vector2 targetPosition = info.getPositionToShootAt().value();
+    // Look at the position to shoot at
+    skillStpInfo.setAngle((info.getPositionToShootAt().value() - info.getRobot()->get()->getPos()).angle());
 
-    /// The robot will go to the position of the ball
-    double ballDistance = (ballPosition - robotPosition).length();
-    Vector2 newRobotPosition;
-
-    /// If ball is to far away, dont get stuck on other GoToPos(), but restart and get behind the ball
-    if (skillStpInfo.getBall().value()->getPos().dist2(skillStpInfo.getRobot().value()->getPos()) > control_constants::DISTANCE_TO_ROBOT_FAR) {
-        skills.skip_to(0);
+    // If the robot is far from the ball, go to the target position
+    if ((info.getBall()->get()->getPos() - info.getRobot()->get()->getPos()).length() > 0.5) {
+        auto targetPos = GetBehindBallInDirection::calculateTargetPosition(info.getBall()->get()->getPos(), info.getRobot()->get()->getPos(), info.getPositionToShootAt().value());
+        skillStpInfo.setPositionToMoveTo(targetPos);
     }
 
-    if (skills.current_num() == 0) {
-        newRobotPosition = ballPosition + (ballPosition - targetPosition).stretchToLength(control_constants::DISTANCE_TO_ROBOT_NEAR);
-
-        /// Check if the path crosses through the ball
-        auto pathToBehindBall = LineSegment(robotPosition, newRobotPosition);
-        auto distFromPathToBall = pathToBehindBall.distanceToLine(ballPosition);
-        auto drivingDirection = robotPosition - newRobotPosition;
-        if (distFromPathToBall < control_constants::ROBOT_RADIUS * 1.2) {
-            /// Decide which side of the ball to approach on
-            auto posBallLeft = ballPosition + Vector2(drivingDirection.y, -drivingDirection.x).stretchToLength(control_constants::DISTANCE_TO_ROBOT_NEAR);
-            auto posBallRight = ballPosition + Vector2(-drivingDirection.y, drivingDirection.x).stretchToLength(control_constants::DISTANCE_TO_ROBOT_NEAR);
-            newRobotPosition = (posBallLeft.dist(robotPosition) < posBallRight.dist(robotPosition)) ? posBallLeft : posBallRight;
+    // If the robot is close to the ball, use orbit to get behind it
+    else {
+        if (skills.current_num() == 0) {
+            skills.skip_to(1);
         }
-    } else {
-        /// Second GoToPos: Go towards ball
-        newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - control_constants::CENTER_TO_FRONT + 0.035);
-        /// Rotate towards ball
-        skillStpInfo.setAngle((ballPosition - robotPosition).angle());
     }
-    /// Turn on dribbler when close to ball
-    if (ballDistance < control_constants::TURN_ON_DRIBBLER_DISTANCE) {
-        skillStpInfo.setDribblerSpeed(100);
-    }
-    skillStpInfo.setPositionToMoveTo(newRobotPosition);
+
     return skillStpInfo;
 }
 
+Vector2 GetBehindBallInDirection::calculateTargetPosition(Vector2 ballPosition, Vector2 robotPosition, Vector2 positionToShootAt) {
+    auto ballAvoidDistance = 4 * control_constants::ROBOT_RADIUS;
+    auto ballToTarget = positionToShootAt - ballPosition;
+
+    // The inital target position is behind the ball in the direction to shoot at, at a distance of ballAvoidDistance
+    auto targetPos = ballPosition - ballToTarget.stretchToLength(ballAvoidDistance);
+
+    // If the line from the robot to the target brings the robot closer to the ball than the avoid distance, adjust the target position to prevent this
+    auto robotToTarget = targetPos - robotPosition;
+    if (!Circle(ballPosition, ballAvoidDistance).intersectsCircleWithLineSegment(LineSegment(robotPosition, targetPos)).empty()) {
+        auto direction = ballToTarget.toAngle().rotateDirection(robotToTarget) ? 1.0 : -1.0;
+        auto finalPos = ballPosition + robotToTarget.rotate(M_PI_2).stretchToLength(ballAvoidDistance) * direction;
+        return finalPos;
+    } else {
+        return targetPos;
+    }
+}
 bool GetBehindBallInDirection::isTacticFailing(const StpInfo &info) noexcept { return !info.getPositionToShootAt(); }
 
-bool GetBehindBallInDirection::shouldTacticReset(const StpInfo &info) noexcept { return false; }
+bool GetBehindBallInDirection::shouldTacticReset(const StpInfo &info) noexcept {
+    return ((info.getBall()->get()->getPos() - info.getRobot()->get()->getPos()).length() > 0.5) && (skills.current_num() == 1);
+}
 
 bool GetBehindBallInDirection::isEndTactic() noexcept {
     // This is not an end tactic
