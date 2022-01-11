bdf43c9f1c9114fb7e81547cae6b03456f713a76
Tijmen Westeneng <t.r.m.westeneng@gmail.com>
2022-01-11 16:14:14 +0100

    Removed speed limiter

--- src/stp/tactics/TestTactic.cpp
+++ src/stp/tactics/TestTactic.cpp
@@ -8,21 +8,26 @@
 
 #include "stp/skills/GoToPos.h"
 
+#include "stp/skills/Kick.h"
+
 #include "roboteam_utils/Print.h"
 
 namespace rtt::ai::stp {
 
 TestTactic::TestTactic() {
     // Create state machine of skills and initialize first skill
-    skills = rtt::collections::state_machine<Skill, Status, StpInfo>{skill::GoToPos()};
+    skills = rtt::collections::state_machine<Skill, Status, StpInfo>{skill::GoToPos(), skill::Kick()};
 }
 
 std::optional<StpInfo> TestTactic::calculateInfoForSkill(StpInfo const &info) noexcept {
     StpInfo skillStpInfo = info;
     if (!skillStpInfo.getField()) return std::nullopt;
 
-    skillStpInfo.setPositionToMoveTo(skillStpInfo.getBall()->get()->getPos());
+    skillStpInfo.setPositionToMoveTo(skillStpInfo.getBall()->get()->getPos() - ((info.getBall()->get()->getPos() - info.getRobot()->get()->getPos()).stretchToLength(0.022)));
     skillStpInfo.setAngle((skillStpInfo.getBall()->get()->getPos() - info.getRobot()->get()->getPos()).angle());
+
+    skillStpInfo.setKickChipVelocity(6.5);
+
     return skillStpInfo;
 }
 
