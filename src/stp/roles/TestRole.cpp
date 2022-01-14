676a9b186f5c601548f662a2c3de289076272024
GagataS <a.sowa@roboteamtwente.nl>
2022-01-14 09:44:57 +0100

    For testing block robot

--- src/stp/roles/TestRole.cpp
+++ src/stp/roles/TestRole.cpp
@@ -4,11 +4,12 @@
 
 #include "stp/roles/TestRole.h"
 #include "stp/tactics/TestTactic.h"
+#include "stp/tactics/passive/BlockRobot.h"
 
 namespace rtt::ai::stp {
 
 TestRole::TestRole(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{TestTactic()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::BlockRobot()};
 }
 }  // namespace rtt::ai::stp
