60ae0a805119382fd9df1318e489b4f62ebefa70
GagataS <a.sowa@roboteamtwente.nl>
2022-01-16 17:48:30 +0100

    Improved BlockRobot for handling situations close to our defense area

--- src/stp/tactics/passive/BlockRobot.cpp
+++ src/stp/tactics/passive/BlockRobot.cpp
@@ -27,33 +27,38 @@ std::optional<StpInfo> BlockRobot::calculateInfoForSkill(StpInfo const &info) no
     // Minimum distance from the defense area
     auto margin = 2.5 * control_constants::ROBOT_RADIUS;
 
-    // Distance of the enemy robot to our defense zone
-    auto enemyDistanceToDefenseZone =
-        FieldComputations::getDistanceToDefenseZone(info.getField().value(), true, info.getEnemyRobot()->get()->getPos(), margin, margin);
+    auto posMargin = 0;
 
-    auto desiredRobotPosition =
-        calculateDesiredRobotPosition(info.getBlockDistance(), info.getEnemyRobot().value(), info.getPositionToDefend().value(), false, enemyDistanceToDefenseZone);
+    Vector2 desiredRobotPosition;
+
+    //
+    auto enemyDistanceToDefenseZone = 10000;
 
     // Project the target position outside the defense area if it is in it (within the margin)
-    if (FieldComputations::pointIsInDefenseArea(info.getField().value(), desiredRobotPosition, margin)) {
+    if (FieldComputations::pointIsInDefenseArea(info.getField().value(), info.getPositionToDefend().value(), posMargin, margin)) {
 
-        auto projectedDesiredRobotPosition = PositionComputations::ProjectPositionOutsideDefenseAreaOnLine(
-            info.getField().value(), desiredRobotPosition, info.getPositionToDefend().value(), info.getEnemyRobot()->get()->getPos(), margin);
+        auto projectedRobotPosition = PositionComputations::ProjectPositionOutsideDefenseAreaOnLine(
+            info.getField().value(), info.getPositionToDefend().value(), info.getPositionToDefend().value(), info.getEnemyRobot()->get()->getPos(), posMargin);
+
+        auto enemyDistanceToProjectedPos = distanceFromPointToPoint(projectedRobotPosition, info.getEnemyRobot()->get()->getPos());
 
         bool isBelowPenalty = false;
-        RTT_DEBUG("Position projected")
+        RTT_DEBUG("Position projected ", projectedRobotPosition)
 
-        // check if the enemy robot is below our penalty line (closer to our goal than the penalty line)
-        if (enemyDistanceToDefenseZone < FieldComputations::isBelowPenaltyLine(info.getField().value(), true, info.getEnemyRobot()->get()->getPos(), margin, margin)) {
+        if (FieldComputations::isBelowPenaltyLine(info.getField().value(), true, info.getEnemyRobot()->get()->getPos(), posMargin, posMargin)) {
             isBelowPenalty = true;
             RTT_DEBUG("Is below penalty")
         }
 
         desiredRobotPosition =
-            calculateDesiredRobotPosition(info.getBlockDistance(), info.getEnemyRobot().value(), projectedDesiredRobotPosition, isBelowPenalty, enemyDistanceToDefenseZone);
+            calculateDesiredRobotPosition(info.getBlockDistance(), info.getEnemyRobot().value(), projectedRobotPosition, isBelowPenalty, enemyDistanceToProjectedPos);
+
+    } else {
+        desiredRobotPosition =
+            calculateDesiredRobotPosition(info.getBlockDistance(), info.getEnemyRobot().value(), info.getPositionToDefend().value(), false, enemyDistanceToDefenseZone);
     }
 
-    RTT_DEBUG("Desired robot psoition ", desiredRobotPosition);
+    RTT_DEBUG("Desired robot position ", desiredRobotPosition);
 
     skillStpInfo.setPositionToMoveTo(desiredRobotPosition);
 
@@ -82,19 +87,23 @@ Vector2 BlockRobot::calculateDesiredRobotPosition(BlockDistance blockDistance, c
             distance = lineEnemyToTarget.length()-2*control_constants::ROBOT_RADIUS;
             break;
     }
-//|| enemyDistance < 3 * control_constants::ROBOT_RADIUS
-    if (distance < 4 * control_constants::ROBOT_RADIUS ) {
-        distance = 4 * control_constants::ROBOT_RADIUS;
-        RTT_DEBUG("To close", enemyDistance);
+
+    if (distance < 4 * control_constants::ROBOT_RADIUS || enemyDistance < distance) {
+        distance = lineEnemyToTarget.length()/2;
+        RTT_DEBUG("To close ", enemyDistance);
     }
 
-    // check if enemy is too close to the defense zone and under the penalty line
-    if (enemyDistance < 2 * control_constants::ROBOT_RADIUS && isBelowPenalty) {
-        RTT_DEBUG("HERE", enemy->getPos());
+
+    if(enemyDistance < 3 * control_constants::ROBOT_RADIUS){
+        distance = 4 * control_constants::ROBOT_RADIUS;
         auto movePosition = lineEnemyToTarget.stretchToLength(distance);
-        auto finalPos = Vector2(enemy->getPos().x - movePosition.x, enemy->getPos().y);
-        RTT_DEBUG("MOVE" ,enemy->getPos() - movePosition, "   ", movePosition, "   ", finalPos)
-        return finalPos;
+        if(isBelowPenalty){
+            RTT_DEBUG("BELOW");
+            return Vector2(enemy->getPos().x - movePosition.x, enemy->getPos().y);
+        } else {
+            RTT_DEBUG("ABOVE")
+            return Vector2(enemy->getPos().x, enemy->getPos().y + movePosition.y);
+        }
     }
 
     auto movePosition = lineEnemyToTarget.stretchToLength(distance);
--- src/world/FieldComputations.cpp
+++ src/world/FieldComputations.cpp
@@ -116,6 +116,7 @@ Vector2 FieldComputations::getPenaltyPoint(const rtt_world::Field &field, bool o
     }
 }
 
+//Rewrite this for only segment not the whole line from segment
 double FieldComputations::getDistanceToDefenseZone(const rtt_world::Field &field, bool ourDefenseArea, const Vector2 &point, double margin, double backMargin) {
     auto polygon = getDefenseArea(field, ourDefenseArea, margin, backMargin);
     double distance;
@@ -163,6 +164,8 @@ Polygon FieldComputations::getDefenseArea(const rtt_world::Field &field, bool ou
     Vector2 bottomPenalty = ourDefenseArea ? field.getLeftPenaltyLineBottom() + Vector2(margin, -margin) : field.getRightPenaltyLineBottom() + Vector2(-margin, -margin);
     Vector2 topPenalty = ourDefenseArea ? field.getLeftPenaltyLineTop() + Vector2(margin, margin) : field.getRightPenaltyLineTop() + Vector2(-margin, margin);
 
+    //for vision :      std::vector<Vector2> defenseArea = {bottomPenalty, topPenalty, belowGoal, aboveGoal};
+    //for simulator:     std::vector<Vector2> defenseArea = {bottomPenalty, topPenalty, aboveGoal, belowGoal};
     std::vector<Vector2> defenseArea = {bottomPenalty, topPenalty, aboveGoal, belowGoal};
     interface::Input::drawDebugData(defenseArea);
     return Polygon(defenseArea);
