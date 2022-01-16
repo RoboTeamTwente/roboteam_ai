5b43243f018829d17bad859378bcea2a044cfdcb
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2022-01-16 17:28:34 +0100

    Reduce no. roles in ballPlacementUs to 4

--- src/stp/plays/referee_specific/BallPlacementUs.cpp
+++ src/stp/plays/referee_specific/BallPlacementUs.cpp
@@ -18,12 +18,8 @@ BallPlacementUs::BallPlacementUs() : Play() {
     keepPlayEvaluation.emplace_back(eval::BallPlacementUsGameState);
 
     roles = std::array<std::unique_ptr<Role>, rtt::ai::Constants::ROBOT_COUNT()>{
-        std::make_unique<role::BallPlacer>(role::BallPlacer("ball_placer")),      std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_1")),
-        std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_2")), std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_3")),
-        std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_4")), std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_5")),
-        std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_6")), std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_7")),
-        std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_8")), std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_9")),
-        std::make_unique<role::BallAvoider>(role::BallAvoider("keeper"))};
+        std::make_unique<role::BallPlacer>(role::BallPlacer("ball_placer")), std::make_unique<role::BallAvoider>(role::BallAvoider("keeper")),
+        std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_0")), std::make_unique<role::BallAvoider>(role::BallAvoider("ball_avoider_1"))};
 }
 
 uint8_t BallPlacementUs::score(PlayEvaluator &playEvaluator) noexcept {
@@ -43,19 +39,6 @@ void BallPlacementUs::calculateInfoForRoles() noexcept {
         stpInfos["ball_placer"].setDribblerSpeed(0);
         stpInfos["ball_placer"].setPositionToMoveTo(stpInfos["ball_placer"].getRobot()->get()->getPos());
     }
-
-    auto length = field.getFieldLength();
-    auto width = field.getFieldWidth();
-
-    stpInfos["ball_avoider_1"].setPositionToMoveTo(Vector2{-length / 5, 0.0});
-    stpInfos["ball_avoider_2"].setPositionToMoveTo(Vector2{-length / 5, width / 6});
-    stpInfos["ball_avoider_3"].setPositionToMoveTo(Vector2{length / 5, -width / 6});
-    stpInfos["ball_avoider_4"].setPositionToMoveTo(Vector2{-length / 8, 0.0});
-    stpInfos["ball_avoider_5"].setPositionToMoveTo(Vector2{-length / 9, -width / 4});
-    stpInfos["ball_avoider_6"].setPositionToMoveTo(Vector2{length / 9, width / 4});
-    stpInfos["ball_avoider_7"].setPositionToMoveTo(Vector2{length / 4, 0.0});
-    stpInfos["ball_avoider_8"].setPositionToMoveTo(Vector2{length / 4, width / 4});
-    stpInfos["ball_avoider_9"].setPositionToMoveTo(Vector2{length / 4, -width / 4});
 }
 
 Dealer::FlagMap BallPlacementUs::decideRoleFlags() const noexcept {
@@ -66,13 +49,6 @@ Dealer::FlagMap BallPlacementUs::decideRoleFlags() const noexcept {
     flagMap.insert({"ball_placer", {DealerFlagPriority::REQUIRED, {ballPlacement}}});
     flagMap.insert({"ball_avoider_0", {DealerFlagPriority::LOW_PRIORITY, {}}});
     flagMap.insert({"ball_avoider_1", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_2", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_3", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_4", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_5", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_6", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_7", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"ball_avoider_8", {DealerFlagPriority::LOW_PRIORITY, {}}});
 
     return flagMap;
 }
