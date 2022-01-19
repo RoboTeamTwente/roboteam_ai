e266834d0f21a21598462427b1e8a247247ab44a
GagataS <a.sowa@roboteamtwente.nl>
2022-01-19 13:20:56 +0100

    Switched BlockRobot tactic to BlockBall for role Defender and BlockBall for BlockRobot for role Harasser

--- src/stp/roles/passive/Harasser.cpp
+++ src/stp/roles/passive/Harasser.cpp
@@ -5,12 +5,12 @@
 #include "stp/roles/passive/Harasser.h"
 
 #include "stp/tactics/Intercept.h"
-#include "stp/tactics/passive/BlockBall.h"
+#include "stp/tactics/passive/BlockRobot.h"
 
 namespace rtt::ai::stp::role {
 
 Harasser::Harasser(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::BlockBall()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::BlockRobot()};
 }
 }  // namespace rtt::ai::stp::role
