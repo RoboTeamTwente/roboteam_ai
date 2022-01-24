a2736a54eee1111ee2386a5b7a232e30a354ef63
emielsteerneman <emielsteerneman@users.noreply.github.com>
2022-01-24 10:23:08 +0000

    deploy: 3caa665fe01a7b342117340f76088d0699df2543

--- doxygen_docs/html/search/files_c.js
+++ doxygen_docs/html/search/files_c.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['nogoalvisionfromballglobalevaluation_2ecpp_2650',['NoGoalVisionFromBallGlobalEvaluation.cpp',['../NoGoalVisionFromBallGlobalEvaluation_8cpp.html',1,'']]],
-  ['nogoalvisionfromballglobalevaluation_2eh_2651',['NoGoalVisionFromBallGlobalEvaluation.h',['../NoGoalVisionFromBallGlobalEvaluation_8h.html',1,'']]],
-  ['normalize_2ecpp_2652',['normalize.cpp',['../normalize_8cpp.html',1,'']]],
-  ['normalize_2eh_2653',['normalize.h',['../normalize_8h.html',1,'']]],
-  ['normalorfreekickusgamestateevaluation_2ecpp_2654',['NormalOrFreeKickUsGameStateEvaluation.cpp',['../NormalOrFreeKickUsGameStateEvaluation_8cpp.html',1,'']]],
-  ['normalorfreekickusgamestateevaluation_2eh_2655',['NormalOrFreeKickUsGameStateEvaluation.h',['../NormalOrFreeKickUsGameStateEvaluation_8h.html',1,'']]],
-  ['normalplaygamestateevaluation_2ecpp_2656',['NormalPlayGameStateEvaluation.cpp',['../NormalPlayGameStateEvaluation_8cpp.html',1,'']]],
-  ['normalplaygamestateevaluation_2eh_2657',['NormalPlayGameStateEvaluation.h',['../NormalPlayGameStateEvaluation_8h.html',1,'']]],
-  ['numtreeposcontroltest_2ecpp_2658',['NumTreePosControlTest.cpp',['../NumTreePosControlTest_8cpp.html',1,'']]],
-  ['numtreesplanning_2ecpp_2659',['NumTreesPlanning.cpp',['../NumTreesPlanning_8cpp.html',1,'']]],
-  ['numtreesplanning_2eh_2660',['NumTreesPlanning.h',['../NumTreesPlanning_8h.html',1,'']]]
+  ['nogoalvisionfromballglobalevaluation_2ecpp_2641',['NoGoalVisionFromBallGlobalEvaluation.cpp',['../NoGoalVisionFromBallGlobalEvaluation_8cpp.html',1,'']]],
+  ['nogoalvisionfromballglobalevaluation_2eh_2642',['NoGoalVisionFromBallGlobalEvaluation.h',['../NoGoalVisionFromBallGlobalEvaluation_8h.html',1,'']]],
+  ['normalize_2ecpp_2643',['normalize.cpp',['../normalize_8cpp.html',1,'']]],
+  ['normalize_2eh_2644',['normalize.h',['../normalize_8h.html',1,'']]],
+  ['normalorfreekickusgamestateevaluation_2ecpp_2645',['NormalOrFreeKickUsGameStateEvaluation.cpp',['../NormalOrFreeKickUsGameStateEvaluation_8cpp.html',1,'']]],
+  ['normalorfreekickusgamestateevaluation_2eh_2646',['NormalOrFreeKickUsGameStateEvaluation.h',['../NormalOrFreeKickUsGameStateEvaluation_8h.html',1,'']]],
+  ['normalplaygamestateevaluation_2ecpp_2647',['NormalPlayGameStateEvaluation.cpp',['../NormalPlayGameStateEvaluation_8cpp.html',1,'']]],
+  ['normalplaygamestateevaluation_2eh_2648',['NormalPlayGameStateEvaluation.h',['../NormalPlayGameStateEvaluation_8h.html',1,'']]],
+  ['numtreeposcontroltest_2ecpp_2649',['NumTreePosControlTest.cpp',['../NumTreePosControlTest_8cpp.html',1,'']]],
+  ['numtreesplanning_2ecpp_2650',['NumTreesPlanning.cpp',['../NumTreesPlanning_8cpp.html',1,'']]],
+  ['numtreesplanning_2eh_2651',['NumTreesPlanning.h',['../NumTreesPlanning_8h.html',1,'']]]
 ];
