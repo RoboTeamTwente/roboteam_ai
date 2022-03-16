0772b4e6cf8241e52a8e51f818783e5745a72934
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2022-03-16 14:36:08 +0100

    Make constants constexpr

--- src/stp/evaluations/position/BlockingEvaluation.cpp
+++ src/stp/evaluations/position/BlockingEvaluation.cpp
@@ -9,7 +9,7 @@
 namespace rtt::ai::stp::evaluation {
 uint8_t BlockingEvaluation::metricCheck(double pointDistance, std::vector<double>& enemyDistances, std::vector<double>& enemyAngles) noexcept {
     double evalScore = 0;
-    double maxAngle = M_PI / 6;  // 30 degrees, max angle considered for blocking
+    constexpr double maxAngle = M_PI / 6;  // 30 degrees, max angle considered for blocking
     for (int i = 0; i < enemyAngles.size(); i++) {
         if (enemyDistances[i] > pointDistance && enemyAngles[i] < maxAngle) {
             evalScore += std::pow((1 / (maxAngle)) * (maxAngle - enemyAngles[i]), 2) / 2;
