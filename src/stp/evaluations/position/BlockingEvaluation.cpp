19cb99f4ddbe00f2aab9a01f86e0d69ebb729b09
alexander-de-ranitz <alexander-de-ranitz@users.noreply.github.com>
2022-03-18 10:43:18 +0000

    deploy: 5ec688f67dce644e71c2f781fb519b64040084ac

--- src/stp/evaluations/position/BlockingEvaluation.cpp
+++ src/stp/evaluations/position/BlockingEvaluation.cpp
@@ -9,7 +9,7 @@
 namespace rtt::ai::stp::evaluation {
 uint8_t BlockingEvaluation::metricCheck(double pointDistance, std::vector<double>& enemyDistances, std::vector<double>& enemyAngles) noexcept {
     double evalScore = 0;
-    const expr double maxAngle = M_PI / 6;  // 30 degrees, max angle considered for blocking
+    constexpr double maxAngle = M_PI / 6;  // 30 degrees, max angle considered for blocking
     for (size_t i = 0; i < enemyAngles.size(); i++) {
         if (enemyDistances[i] > pointDistance && enemyAngles[i] < maxAngle) {
             evalScore += std::pow((1 / (maxAngle)) * (maxAngle - enemyAngles[i]), 2) / 2;
