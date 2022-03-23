06587e40cef3884594ef88c084b8d1daa3c655e6
itsasimpleplan <s.onrust@roboteamtwente.nl>
2022-03-23 09:53:21 +0100

    Set up InterceptBall.cpp
    Added evaluations and flags

--- src/stp/plays/InterceptBall.cpp
+++ src/stp/plays/InterceptBall.cpp
@@ -6,25 +6,25 @@
 #include "stp/roles/Keeper.h"
 #include "stp/roles/passive/Defender.h"
 #include "stp/roles/passive/Harasser.h"
+#include "stp/roles/passive/Halt.h"
+#include "stp/roles/active/Intercepter.h"
 
 namespace rtt::ai::stp::play {
 
 InterceptBall::InterceptBall() : Play() {
     startPlayEvaluation.clear();
     startPlayEvaluation.emplace_back(eval::NormalPlayGameState);
-    startPlayEvaluation.emplace_back(eval::TheyHaveBall);
-    startPlayEvaluation.emplace_back(GlobalEvaluation::BallNotInOurDefenseAreaAndStill);
+    startPlayEvaluation.emplace_back(GlobalEvaluation::BallIsFree);
 
     keepPlayEvaluation.clear();
     keepPlayEvaluation.emplace_back(eval::NormalPlayGameState);
-    keepPlayEvaluation.emplace_back(eval::TheyHaveBall);
-    keepPlayEvaluation.emplace_back(GlobalEvaluation::BallNotInOurDefenseAreaAndStill);
+    keepPlayEvaluation.emplace_back(GlobalEvaluation::BallIsFree);
 
     roles = std::array<std::unique_ptr<Role>, stp::control_constants::MAX_ROBOT_COUNT>{
         std::make_unique<role::Keeper>(role::Keeper("keeper")),
-        std::make_unique<role::Defender>(role::Defender("defender_1")),
-        std::make_unique<role::Defender>(role::Defender("defender_2")),
-        std::make_unique<role::Harasser>(role::Harasser("harassing_defender")),
+        std::make_unique<role::Halt>(role::Halt("halt_1")),
+        std::make_unique<role::Halt>(role::Halt("halt_2")),
+        std::make_unique<role::Intercepter>(role::Intercepter("intercepter")),
     };
 }
 
@@ -33,8 +33,12 @@ uint8_t InterceptBall::score(PlayEvaluator &playEvaluator) noexcept {
 
 Dealer::FlagMap InterceptBall::decideRoleFlags() const noexcept {
     Dealer::FlagMap flagMap;
+    Dealer::DealerFlag closeToBallFlag(DealerFlagTitle::CLOSE_TO_BALL, DealerFlagPriority::HIGH_PRIORITY);
 
-
+    flagMap.insert({"keeper", {DealerFlagPriority::KEEPER, {}}});
+    flagMap.insert({"halt_1", {DealerFlagPriority::LOW_PRIORITY, {}}});
+    flagMap.insert({"halt_2", {DealerFlagPriority::LOW_PRIORITY, {}}});
+    flagMap.insert({"intercepter", {DealerFlagPriority::REQUIRED, {closeToBallFlag}}});
     return flagMap;
 }
 
