51032024c48b541c1130af55d126571aa7f99ae1
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2022-04-13 10:22:34 +0200

    Change PlayEvaluator to take a ptr to world as input instead of storing internally
    Previously, a ptr to world was stored in the PlayEvaluator, which was then used in several places to get access to the world. We can avoid having to set/update the world field in the PlayEvaluator and also prevent having to get the world from the PlayEvaluator in several places by just passing along world as an argument. This also allows us to make the functions in this class static.

--- include/roboteam_ai/stp/PlayEvaluator.h
+++ include/roboteam_ai/stp/PlayEvaluator.h
@@ -64,24 +64,12 @@ class PlayEvaluator {
      * @param evaluation that needs
      * @return
      */
-    uint8_t getGlobalEvaluation(GlobalEvaluation evaluation);
-
-    /**
-     * Sets this->world
-     * @param world World to update against
-     */
-    void update(rtt::world::World* world) noexcept;
-
-    /**
-     * Gets this world, used when a play is not initialised yet (in between plays)
-     * @return world
-     */
-    rtt::world::World* getWorld() noexcept;
+    static uint8_t getGlobalEvaluation(GlobalEvaluation evaluation, const world::World* world);
 
     /**
      * Make all booleans of updatedGlobal to false (they have not been updated yet this tick)
      */
-    void clearGlobalScores();
+    static void clearGlobalScores();
 
     /**
      * Checks if FUZZY-TRUE score in uint8-t of global evaluation is above the TRUE threshold
@@ -89,37 +77,27 @@ class PlayEvaluator {
      * @param cutOff Bottom bound value of true
      * @return boolean if FUZZY-TRUE is high enough
      */
-    bool checkEvaluation(GlobalEvaluation globalEvaluation, uint8_t cutOff = control_constants::FUZZY_DEFAULT_CUTOFF) noexcept;
+    static bool checkEvaluation(GlobalEvaluation globalEvaluation, const rtt::world::World* world, uint8_t cutOff = control_constants::FUZZY_DEFAULT_CUTOFF) noexcept;
 
     /**
      * Calcalute score with the given vector of scores
      * @param scoring vector withto be considered Evaluations
      * @return final score
      */
-    uint8_t calculateScore(std::vector<PlayScoring>& scoring);
+    static uint8_t calculateScore(std::vector<PlayScoring>& scoring);
 
    private:
     /**
      * Map of all loaded Global Evaluations scores
      */
-    std::unordered_map<GlobalEvaluation, uint8_t> scoresGlobal{};
+    static inline std::unordered_map<GlobalEvaluation, uint8_t> scoresGlobal{};
 
     /**
      * Updates a given Global Evaluation, should only happen if this was not done in this tick yet.
      * @param index of evaluation that needs to be updated
      * @return the score of the updated evaluation
      */
-    uint8_t updateGlobalEvaluation(GlobalEvaluation& evaluation);
-
-    /**
-     * Current world, do not use before update()
-     */
-    rtt::world::World* world{};
-
-    /**
-     *  Current field from world
-     */
-    rtt::world::Field field;
+    static uint8_t updateGlobalEvaluation(GlobalEvaluation& evaluation, const rtt::world::World* world);
 };
 
 }  // namespace rtt::ai::stp
