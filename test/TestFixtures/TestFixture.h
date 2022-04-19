9d8998bba59b8a2918d87f658ae3d680e846e80c
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2022-04-19 14:45:23 +0200

    Add Plays to testFixture
    When testing STP things such as the play deciding, it can be useful to have these plays.

--- test/TestFixtures/TestFixture.h
+++ test/TestFixtures/TestFixture.h
@@ -1,6 +1,10 @@
 //
 // Created by alexander on 28-03-22.
 //
+
+#ifndef RTT_TESTFIXTURE_H
+#define RTT_TESTFIXTURE_H
+
 #include <gtest/gtest.h>
 #include <interface/api/Output.h>
 #include <utilities/Constants.h>
@@ -15,12 +19,40 @@
 #include "utilities/Settings.h"
 #include "world/World.hpp"
 
-#ifndef RTT_TESTFIXTURE_H
-#define RTT_TESTFIXTURE_H
+/**
+ * Plays are included here
+ */
+#include "stp/plays/ReflectKick.h"
+#include "stp/plays/TestPlay.h"
+#include "stp/plays/contested/GetBallPossession.h"
+#include "stp/plays/contested/GetBallRisky.h"
+#include "stp/plays/defensive/DefendPass.h"
+#include "stp/plays/defensive/DefendShot.h"
+#include "stp/plays/defensive/KeeperKickBall.h"
+#include "stp/plays/offensive/Attack.h"
+#include "stp/plays/offensive/AttackingPass.h"
+#include "stp/plays/offensive/GenericPass.h"
+#include "stp/plays/referee_specific/AggressiveStopFormation.h"
+#include "stp/plays/referee_specific/BallPlacementThem.h"
+#include "stp/plays/referee_specific/BallPlacementUs.h"
+#include "stp/plays/referee_specific/DefensiveStopFormation.h"
+#include "stp/plays/referee_specific/FreeKickThem.h"
+#include "stp/plays/referee_specific/Halt.h"
+#include "stp/plays/referee_specific/KickOffThem.h"
+#include "stp/plays/referee_specific/KickOffThemPrepare.h"
+#include "stp/plays/referee_specific/KickOffUs.h"
+#include "stp/plays/referee_specific/KickOffUsPrepare.h"
+#include "stp/plays/referee_specific/PenaltyThem.h"
+#include "stp/plays/referee_specific/PenaltyThemPrepare.h"
+#include "stp/plays/referee_specific/PenaltyUs.h"
+#include "stp/plays/referee_specific/PenaltyUsPrepare.h"
 
 class RTT_AI_Tests : public ::testing::Test {
    protected:
     rtt::world::World* world;
+
+    // Vector of all plays
+    std::vector<std::unique_ptr<rtt::ai::stp::Play>> plays;
 
     /// This function is called before each test is run, ensuring the default environment for each test
     void SetUp() override {
@@ -41,6 +73,9 @@ class RTT_AI_Tests : public ::testing::Test {
         rtt::ai::GameStateManager::forceNewGameState(RefCommand::HALT, std::nullopt);
         rtt::ai::interface::Output::setInterfaceGameState(rtt::ai::GameState("halt_strategy", "default"));
         rtt::ai::interface::Output::setUseRefereeCommands(false);
+
+        // Make sure all plays exist as new
+        constructPlays();
     }
 
     /**
@@ -58,6 +93,50 @@ class RTT_AI_Tests : public ::testing::Test {
         world->updateWorld(protoWorld);
         world->updateField(field.value());
         return world;
+    }
+
+   private:
+    /**
+     * Creates a vector containing all plays. If this already exists, clear it before adding new plays.
+     */
+    void constructPlays() {
+        plays.clear();
+
+        /// This play is only used for testing purposes, when needed uncomment this play!
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::TestPlay>());
+
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::AttackingPass>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::Attack>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::Halt>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::DefendShot>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::DefendPass>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::KeeperKickBall>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::DefensiveStopFormation>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::AggressiveStopFormation>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::BallPlacementUs>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::BallPlacementThem>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::TimeOut>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::PenaltyThemPrepare>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::PenaltyUsPrepare>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::PenaltyThem>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::PenaltyUs>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffUsPrepare>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffThemPrepare>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::FreeKickThem>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffUs>());
+        plays.emplace_back(std::make_unique<rtt::ai::stp::play::KickOffThem>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::GetBallPossession>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::GetBallRisky>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::ReflectKick>());
+        // plays.emplace_back(std::make_unique<rtt::ai::stp::play::GenericPass>());
+
+        // Set the pointer to world for all plays
+        {
+            auto const& [_, world] = rtt::world::World::instance();
+            for (auto& play : plays) {
+                play->setWorld(world);
+            }
+        }
     }
 };
 #endif  // RTT_TESTFIXTURE_H
