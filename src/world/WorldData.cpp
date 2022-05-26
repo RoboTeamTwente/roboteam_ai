544a5a78d9c784fb93ba8cb1a8695b798e83d42b
rolfvdhulst <rolfvdhulst@gmail.com>
2022-05-26 12:20:11 +0200

    Change INFO log into a warning

--- src/world/WorldData.cpp
+++ src/world/WorldData.cpp
@@ -33,9 +33,9 @@ WorldData::WorldData(const World *data, proto::World &protoMsg, rtt::Settings co
 
     //TODO: add information from robots which were only seen on feedback but not on vision
     if (settings.isYellow() && protoMsg.yellow_unseen_robots_size() > 0){
-        RTT_INFO("Received feedback from unseen robots!")
+        RTT_WARNING("Received feedback from unseen robots!")
     }else if (!settings.isYellow() && protoMsg.blue_unseen_robots_size() > 0){
-        RTT_INFO("Received feedback from unseen robots!")
+        RTT_WARNING("Received feedback from unseen robots!")
     }
 }
 
